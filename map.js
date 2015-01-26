f70f5110dd714ef70b14ffb8f992a703226f162e
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-01-26 13:55:34 +0100

    Add Leaflet map

---
+++ map.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([0, 0], 2);
+// add an OpenStreetMap tile layer
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
