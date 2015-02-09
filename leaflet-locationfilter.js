f69b6609eb5234d731d8ac0a4f7dbcafd722726c
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-02-09 17:59:51 +0100

    update bounding box text inputs with values from map

--- leaflet-locationfilter.js
+++ leaflet-locationfilter.js
@@ -4,4 +4,19 @@
 
 var locationFilter = new L.LocationFilter({
 	enable : true
-}).addTo(map); 
+}).addTo(map);
+
+// push initial values to text inputs
+var bounds = locationFilter.getBounds()
+$('#input-north').val(bounds._northEast.lat);
+$('#input-west').val(bounds._southWest.lng);
+$('#input-east').val(bounds._northEast.lng);
+$('#input-south').val(bounds._southWest.lat);
+
+// update text input values upon change on map
+locationFilter.on("change", function(e) {
+	$('#input-north').val(e.bounds._northEast.lat);
+	$('#input-west').val(e.bounds._southWest.lng);
+	$('#input-east').val(e.bounds._northEast.lng);
+	$('#input-south').val(e.bounds._southWest.lat);
+});
