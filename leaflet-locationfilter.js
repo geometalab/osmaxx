c04c30cdacf429a44b4f2088d5417ab9a71ec337
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-02-10 09:15:21 +0100

    extracted js function for updating bbox text inputs with bounds passed as argument

--- leaflet-locationfilter.js
+++ leaflet-locationfilter.js
@@ -6,17 +6,17 @@ var locationFilter = new L.LocationFilter({
 	enable : true
 }).addTo(map);
 
+function updateBboxTextInputsWith(bounds) {
+	$('#input-north').val(bounds._northEast.lat);
+	$('#input-west').val(bounds._southWest.lng);
+	$('#input-east').val(bounds._northEast.lng);
+	$('#input-south').val(bounds._southWest.lat);
+}
+
 // push initial values to text inputs
-var bounds = locationFilter.getBounds()
-$('#input-north').val(bounds._northEast.lat);
-$('#input-west').val(bounds._southWest.lng);
-$('#input-east').val(bounds._northEast.lng);
-$('#input-south').val(bounds._southWest.lat);
+updateBboxTextInputsWith(locationFilter.getBounds());
 
 // update text input values upon change on map
 locationFilter.on("change", function(e) {
-	$('#input-north').val(e.bounds._northEast.lat);
-	$('#input-west').val(e.bounds._southWest.lng);
-	$('#input-east').val(e.bounds._northEast.lng);
-	$('#input-south').val(e.bounds._southWest.lat);
+	updateBboxTextInputsWith(e.bounds);
 });
