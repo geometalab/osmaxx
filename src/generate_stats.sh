bbef6094e4c163e52121c6c57880323cdd57ab4e
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2015-02-14 12:56:41 +0100

    Parallelize generate_stats.sh

--- src/generate_stats.sh
+++ src/generate_stats.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+parallel_options="-k --eta -j50% --noswap"
+
 if [[ $# -ne 2 ]]; then
 	echo "Usage: $0 directory/with/pbffiles/ outfile"
 fi
@@ -17,14 +19,18 @@ fi
 indir=$1
 outfile=$2
 
-for lat in {-90..89}; do
-	for lon in {-180..179}; do
-		infile=$indir/out_${lat}_${lon}.pbf
-		filesize=$(stat -c '%s' $infile)
-		stats=$(./osmconvert $infile --out-statistics)
-		nodes=$(echo "$stats"     | grep 'nodes: '     | sed -e 's!.*: \(.*\)!\1!')
-		ways=$(echo "$stats"      | grep 'ways: '      | sed -e 's!.*: \(.*\)!\1!')
-		relations=$(echo "$stats" | grep 'relations: ' | sed -e 's!.*: \(.*\)!\1!')
-		echo "$lat,$lon,$filesize,$nodes,$ways,$relations" >> $outfile
-	done
-done
+process() {
+	indir=$1
+	minlat=$2
+	minlon=$3
+	infile=$indir/out_${minlat}_${minlon}.pbf
+	filesize=$(stat -c '%s' $infile)
+	stats=$(./osmconvert $infile --out-statistics)
+	nodes=$(echo "$stats"     | grep 'nodes: '     | sed -e 's!.*: \(.*\)!\1!')
+	ways=$(echo "$stats"      | grep 'ways: '      | sed -e 's!.*: \(.*\)!\1!')
+	relations=$(echo "$stats" | grep 'relations: ' | sed -e 's!.*: \(.*\)!\1!')
+	echo "$minlat,$minlon,$filesize,$nodes,$ways,$relations"
+}
+
+export -f process
+time ./parallel $parallel_options process $indir ::: {-90..89} ::: {-180..179} > $outfile # minlat, minlon
