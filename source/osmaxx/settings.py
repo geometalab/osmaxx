46da6bff94b7ebe792739ec0df86455693169a4b
Tobias Blaser <tblaser@hsr.ch>
2015-02-24 15:14:06 +0100

    Configure database connection, fix imports for usage with apache

--- source/osmaxx/settings.py
+++ source/osmaxx/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^9@i=_zww-&mih(&_j+c8i+a%o1-7t83i$tgl#_)k9rbz#(rxj'
+SECRET_KEY = 'w%uc3xg!elspz-3!g@j7-^7@^g1-a-3%-+u39$4=vueb_p5p_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -35,7 +35,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,10 +57,14 @@ WSGI_APPLICATION = 'osmaxx.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'osmaxx',
+       'USER': 'osmaxx',
+       'PASSWORD': 'osmaxx',
+       'HOST': '127.0.0.1',
+       'PORT': '5432'
+   }
 }
 
 # Internationalization
--- source/osmaxx/wsgi.py
+++ source/osmaxx/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/var/www/eda')
+sys.path.append('/var/www/eda/projects')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "osmaxx.settings")
 
 from django.core.wsgi import get_wsgi_application
