b7def3fb65956ab74f1cad4ea66d2e9ecd58d5a2
Tobias Blaser <tblaser@hsr.ch>
2015-02-24 17:29:59 +0100

    Allow models to be edited in admin area

--- source/excerptExport/admin.py
+++ source/excerptExport/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from excerptExport.models import BoundingGeometry
+from excerptExport.models import Excerpt
+from excerptExport.models import ExtractionOrder
+from excerptExport.models import OutputFile
 
 # Register your models here.
+admin.site.register(BoundingGeometry)
+admin.site.register(Excerpt)
+admin.site.register(ExtractionOrder)
+admin.site.register(OutputFile)
