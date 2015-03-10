5ea71f949ed5f36c6ffbc728a20ec4ef9aa83332
Tobias Blaser <tobias.blaser@hsr.ch>
2015-03-10 13:38:58 +0100

    Refactoring: move models to python module. Issue #3

---
+++ source/excerptExport/models/excerpt.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+
+class Excerpt(models.Model):
+    name = models.CharField(max_length=128)
+    is_public = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    owner = models.ForeignKey(User, related_name='excerpts')
+
+    def __str__(self):
+        return self.name
