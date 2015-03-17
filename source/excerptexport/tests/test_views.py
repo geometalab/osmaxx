556042cc807f6b43c1f77a4d7487e4efdec3f0ea
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-03-17 13:39:39 +0100

    refactoring: extract POST data used in tests
    before implementing #21

--- source/excerptexport/tests/test_views.py
+++ source/excerptexport/tests/test_views.py
@@ -6,6 +6,19 @@ from django.contrib.auth.models import User
 class ExcerptExportViewTests(TestCase):
     def setUp(self):
         self.user = User.objects.create_user('user', 'user@example.com', 'pw')
+        self.new_excerpt_post_data = {
+            'form-mode': 'create_new_excerpt',
+            'new_excerpt.name': 'A very interesting region',
+            'new_excerpt.is_public': 'True',
+            'new_excerpt.boundingBox.north': '1.0',
+            'new_excerpt.boundingBox.east': '2.0',
+            'new_excerpt.boundingBox.south': '3.0',
+            'new_excerpt.boundingBox.west': '4.0'
+        }
+        self.existing_excerpt_post_data = {
+            'form-mode': 'existing_excerpt',
+            'existing_excerpt.id': 'some_id'
+        }
 
     def test_new_when_not_logged_in(self):
         """
@@ -27,15 +40,7 @@ class ExcerptExportViewTests(TestCase):
         """
         When not logged in, we get redirected.
         """
-        response = self.client.post(reverse('excerptexport:create'), {
-            'form-mode': 'create_new_excerpt',
-            'new_excerpt.name': 'A very interesting region',
-            'new_excerpt.is_public': 'True',
-            'new_excerpt.boundingBox.north': '1.0',
-            'new_excerpt.boundingBox.east': '2.0',
-            'new_excerpt.boundingBox.south': '3.0',
-            'new_excerpt.boundingBox.west': '4.0'
-        })
+        response = self.client.post(reverse('excerptexport:create'), self.new_excerpt_post_data)
         self.assertEqual(response.status_code, 302)
 
     def test_create_with_new_excerpt(self):
@@ -43,15 +48,7 @@ class ExcerptExportViewTests(TestCase):
         When logged in, POSTing an export request with a new excerpt is successful.
         """
         self.client.login(username='user', password='pw')
-        response = self.client.post(reverse('excerptexport:create'), {
-            'form-mode': 'create_new_excerpt',
-            'new_excerpt.name': 'A very interesting region',
-            'new_excerpt.is_public': 'True',
-            'new_excerpt.boundingBox.north': '1.0',
-            'new_excerpt.boundingBox.east': '2.0',
-            'new_excerpt.boundingBox.south': '3.0',
-            'new_excerpt.boundingBox.west': '4.0'
-        })
+        response = self.client.post(reverse('excerptexport:create'), self.new_excerpt_post_data)
         self.assertEqual(response.status_code, 200)
 
     def test_create_with_existing_excerpt(self):
@@ -59,8 +56,5 @@ class ExcerptExportViewTests(TestCase):
         When logged in, POSTing an export request using an existing excerpt is successful.
         """
         self.client.login(username='user', password='pw')
-        response = self.client.post(reverse('excerptexport:create'), {
-            'form-mode': 'existing_excerpt',
-            'existing_excerpt.id': 'some_id'
-        })
+        response = self.client.post(reverse('excerptexport:create'), self.existing_excerpt_post_data)
         self.assertEqual(response.status_code, 200)
