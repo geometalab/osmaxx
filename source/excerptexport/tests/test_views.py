28ad3afeb0659b1730c89937134741ce198e83ad
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-03-17 13:29:39 +0100

    test existing create functionality
    before implementing #21

--- source/excerptexport/tests/test_views.py
+++ source/excerptexport/tests/test_views.py
@@ -22,3 +22,45 @@ class ExcerptExportViewTests(TestCase):
         response = self.client.get(reverse('excerptexport:new'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Create new excerpt')
+
+    def test_create_when_not_logged_in(self):
+        """
+        When not logged in, we get redirected.
+        """
+        response = self.client.post(reverse('excerptexport:create'), {
+            'form-mode': 'create_new_excerpt',
+            'new_excerpt.name': 'A very interesting region',
+            'new_excerpt.is_public': 'True',
+            'new_excerpt.boundingBox.north': '1.0',
+            'new_excerpt.boundingBox.east': '2.0',
+            'new_excerpt.boundingBox.south': '3.0',
+            'new_excerpt.boundingBox.west': '4.0'
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_create_with_new_excerpt(self):
+        """
+        When logged in, POSTing an export request with a new excerpt is successful.
+        """
+        self.client.login(username='user', password='pw')
+        response = self.client.post(reverse('excerptexport:create'), {
+            'form-mode': 'create_new_excerpt',
+            'new_excerpt.name': 'A very interesting region',
+            'new_excerpt.is_public': 'True',
+            'new_excerpt.boundingBox.north': '1.0',
+            'new_excerpt.boundingBox.east': '2.0',
+            'new_excerpt.boundingBox.south': '3.0',
+            'new_excerpt.boundingBox.west': '4.0'
+        })
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_with_existing_excerpt(self):
+        """
+        When logged in, POSTing an export request using an existing excerpt is successful.
+        """
+        self.client.login(username='user', password='pw')
+        response = self.client.post(reverse('excerptexport:create'), {
+            'form-mode': 'existing_excerpt',
+            'existing_excerpt.id': 'some_id'
+        })
+        self.assertEqual(response.status_code, 200)
