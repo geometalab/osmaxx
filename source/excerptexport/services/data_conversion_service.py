270d1e1a92de8d3bcab2ce5e7698d6b89a046005
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-04-13 17:56:03 +0200

    refactoring: extract local variable for data directory

--- source/excerptexport/services/data_conversion_service.py
+++ source/excerptexport/services/data_conversion_service.py
@@ -46,9 +46,10 @@ def trigger_data_conversion(extraction_order, export_options):
                 extraction_order=extraction_order
             )
 
-            if not os.path.exists(settings.APPLICATION_SETTINGS['data_directory']):
-                os.makedirs(settings.APPLICATION_SETTINGS['data_directory'])
-            file_path = settings.APPLICATION_SETTINGS['data_directory'] + '/' \
+            data_directory = settings.APPLICATION_SETTINGS['data_directory']
+            if not os.path.exists(data_directory):
+                os.makedirs(data_directory)
+            file_path = data_directory + '/' \
                 + output_file.public_identifier + '.' + export_formats_configuration[format_key]['file_extension']
 
             with open(file_path, 'w') as file_reference:
