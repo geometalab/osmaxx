26dcc40c8fdaede0aa72e285ce09fc09b988e2c5
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-04-20 14:10:15 +0200

    fix and further simplify listening on all interfaces for runserver script

--- source/runDevelopmentServer.sh
+++ source/runDevelopmentServer.sh
@@ -2,7 +2,5 @@
 
 CURRENTDIR=`dirname $0`
 cd "$CURRENTDIR"
-source "../environment/bin/activate"
-LOCALIP=`hostname -I`
-echo "$LOCALIP"
-python ./manage.py runserver "$LOCALIP:8000"
+source ../environment/bin/activate
+python ./manage.py runserver 0.0.0.0:8000
