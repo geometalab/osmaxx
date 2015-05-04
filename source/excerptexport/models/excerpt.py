a5c2f78cad558c68c5b5202032a8fc979ced1ca0
Nicola Jordan <njordan@hsr.ch>
2015-05-04 15:22:20 +0200

    labels for internationalization for models

--- source/excerptexport/models/excerpt.py
+++ source/excerptexport/models/excerpt.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class Excerpt(models.Model):
-    name = models.CharField(max_length=128)
-    is_public = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    name = models.CharField(verbose_name=_('name'), max_length=128)
+    is_public = models.BooleanField(verbose_name=_('is public'), default=False)
+    is_active = models.BooleanField(verbose_name=_('is active'), default=True)
 
-    owner = models.ForeignKey(User, related_name='excerpts')
-    bounding_geometry = models.OneToOneField('BoundingGeometry')
+    owner = models.ForeignKey(User, related_name='excerpts', verbose_name=_('owner'))
+    bounding_geometry = models.OneToOneField('BoundingGeometry', verbose_name=_('bounding geometry'))
 
     def __str__(self):
         return self.name
