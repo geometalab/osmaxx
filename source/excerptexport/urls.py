0557a9b5ea016fcb6950333c754ce5144cc0bdad
Nicola Jordan <njordan@hsr.ch>
2015-05-04 12:41:36 +0200

    explicit import of urls

--- source/excerptexport/urls.py
+++ source/excerptexport/urls.py
@@ -1,21 +1,25 @@
 from django.conf.urls import patterns, url
+from excerptexport.views import index, access_denied, new_excerpt_export, show_downloads, download_file, \
+    create_excerpt_export, extraction_order_status
 
 
-urlpatterns = patterns(
-    'excerptexport.views',
-    url(r'^$', 'index', name='index'),
-    url(r'^access_denied/$', 'access_denied', name='access_denied'),
-    url(r'^new/$', 'new_excerpt_export', name='new'),
-    url(r'^downloads/$', 'show_downloads', name='downloads'),
-    url(r'^download/$', 'download_file', name='download'),
-    url(r'^create/$', 'create_excerpt_export', name='create'),
-    url(r'^orders/(?P<extraction_order_id>[0-9]+)$', 'extraction_order_status', name='status')
+except_export_urlpatterns = patterns(
+    '',
+    url(r'^$', index, name='index'),
+    url(r'^access_denied/$', access_denied, name='access_denied'),
+    url(r'^new/$', new_excerpt_export, name='new'),
+    url(r'^downloads/$', show_downloads, name='downloads'),
+    url(r'^download/$', download_file, name='download'),
+    url(r'^create/$', create_excerpt_export, name='create'),
+    url(r'^orders/(?P<extraction_order_id>[0-9]+)$', extraction_order_status, name='status')
 )
 
-urlpatterns += patterns(
+login_logout_patterns = patterns(
     '',
     url(r'^login/$', 'django.contrib.auth.views.login',
         {'template_name': 'excerptexport/templates/login.html'}, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout',
         {'template_name': 'excerptexport/templates/logout.html'}, name='logout'),
 )
+
+urlpatterns = except_export_urlpatterns + login_logout_patterns
