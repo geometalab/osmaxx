4b0682ea6f82042802e4953988939a593809d428
Nicola Jordan <njordan@hsr.ch>
2015-05-04 16:34:24 +0200

    verbose_name for model fields everywhere at the end of the file

--- source/excerptexport/models/excerpt.py
+++ source/excerptexport/models/excerpt.py
@@ -4,9 +4,9 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Excerpt(models.Model):
-    name = models.CharField(verbose_name=_('name'), max_length=128)
-    is_public = models.BooleanField(verbose_name=_('is public'), default=False)
-    is_active = models.BooleanField(verbose_name=_('is active'), default=True)
+    name = models.CharField(max_length=128, verbose_name=_('name'))
+    is_public = models.BooleanField(default=False, verbose_name=_('is public'))
+    is_active = models.BooleanField(default=True, verbose_name=_('is active'))
 
     owner = models.ForeignKey(User, related_name='excerpts', verbose_name=_('owner'))
     bounding_geometry = models.OneToOneField('BoundingGeometry', verbose_name=_('bounding geometry'))
