24535d234b91f65b20bffe38d28e647683fed8ad
Tobias Blaser <tobias.blaser@hsr.ch>
2015-05-11 09:36:08 +0200

    Create form template

--- source/excerptexport/forms/new_excerpt_form.py
+++ source/excerptexport/forms/new_excerpt_form.py
@@ -6,9 +6,9 @@ class NewExcerptForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(NewExcerptForm, self).__init__(*args, **kwargs)
 
-        self.fields['new_excerpt.name'] = forms.CharField(label=gettext_lazy('Excerpt name'))
-        self.fields['new_excerpt.boundingBox.north'] = forms.CharField(label=gettext_lazy('North'))
-        self.fields['new_excerpt.boundingBox.west'] = forms.CharField(label=gettext_lazy('West'))
-        self.fields['new_excerpt.boundingBox.east'] = forms.CharField(label=gettext_lazy('East'))
-        self.fields['new_excerpt.boundingBox.south'] = forms.CharField(label=gettext_lazy('South'))
-        self.fields['new_excerpt.is_public'] = forms.BooleanField(label=gettext_lazy('Public'))
+        self.fields['new_excerpt_name'] = forms.CharField(label=gettext_lazy('Excerpt name'))
+        self.fields['new_excerpt_bounding_box_north'] = forms.CharField(label=gettext_lazy('North'))
+        self.fields['new_excerpt_bounding_box_west'] = forms.CharField(label=gettext_lazy('West'))
+        self.fields['new_excerpt_bounding_box_east'] = forms.CharField(label=gettext_lazy('East'))
+        self.fields['new_excerpt_bounding_box_south'] = forms.CharField(label=gettext_lazy('South'))
+        self.fields['new_excerpt_is_public'] = forms.BooleanField(label=gettext_lazy('Public'))
