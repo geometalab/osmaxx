bd25d776422399f3b3791645c64980372618d085
Tobias Blaser <tobias.blaser@hsr.ch>
2015-05-12 13:49:17 +0200

    Fix duplicated file name

--- source/excerptexport/settings/application_settings.py
+++ source/excerptexport/settings/application_settings.py
@@ -1,5 +1,5 @@
 APPLICATION_SETTINGS = {
-    'download_file_name': '%(id)s-%(name)s',
+    'download_file_name': '%(name)s',
     'download_chunk_size': 8192,
     'orders_history_number_of_items': 100
 }
