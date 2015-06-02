27849e9b7d0d313280bff899c72f7da57226541c
Nicola Jordan <njordan@hsr.ch>
2015-06-02 13:42:02 +0200

    migrations updated

---
+++ osmaxx/config/celery.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+
+import environ
+
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+env = environ.Env()
+env('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+
+from django.conf import settings
+
+app = Celery('config')
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
