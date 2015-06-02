b6135e1d3b2b2cfad28560e71accd34b3f99b4fa
Nicola Jordan <njordan@hsr.ch>
2015-06-02 11:28:44 +0200

    new structure and setup for docker

--- setup-containers.sh
+++ setup-containers.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-docker-compose build database
+docker-compose build
 docker-compose up -d database
 # wait for the database cluster to come online
-sleep 10
+sleep 15
+echo "applying migrations"
 docker-compose run webapp python3 manage.py migrate
+sleep 10
+echo "create a superuser:"
 docker-compose run webapp python3 manage.py createsuperuser
 docker-compose stop
