8994251a03b7defffaa47083c2ec58557a6d08b1
Nicola Jordan <njordan@hsr.ch>
2015-06-16 09:25:08 +0200

    add whitenoise settings for delivering static files properly when using wsgi

--- osmaxx/config/wsgi.py
+++ osmaxx/config/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
