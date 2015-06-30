715d2239a6a1449ec32fce6a1c183d5c2ddfccc9
Nicola Jordan <njordan@hsr.ch>
2015-06-30 09:23:01 +0200

    dry clavid backend up a bit

--- osmaxx-py/osmaxx/social_auth/backends/clavid_backend.py
+++ osmaxx-py/osmaxx/social_auth/backends/clavid_backend.py
@@ -3,37 +3,35 @@ from social.backends.open_id import OpenIdAuth
 from social.exceptions import AuthMissingParameter
 
 
-class ClavidComOpenId(OpenIdAuth):
+class ClavidAbstractBaseOpenId(OpenIdAuth):
+    openID_url = None
+
+    def __init__(self, *args, **kwargs):
+        assert type(self) is not ClavidAbstractBaseOpenId
+        assert self.openID_url is not None
+        super().__init__(*args, **kwargs)
+
+    def get_user_details(self, response):
+        """Generate username from identity url"""
+        values = super().get_user_details(response)
+        values['username'] = values.get('username') or \
+            urlparse.urlsplit(response.identity_url).netloc.split('.', 1)[0]
+        return values
+
+    def openid_url(self):
+        """Returns clavid authentication URL"""
+        if not self.data.get('openid_user'):
+            raise AuthMissingParameter(self, 'openid_user')
+        return 'http://%s.%s' % (self.data['openid_user'], self.openID_url)
+
+
+class ClavidComOpenId(ClavidAbstractBaseOpenId):
     """clavid.com OpenID authentication backend"""
     name = 'clavid.com'
-
-    def get_user_details(self, response):
-        """Generate username from identity url"""
-        values = super().get_user_details(response)
-        values['username'] = values.get('username') or \
-            urlparse.urlsplit(response.identity_url).netloc.split('.', 1)[0]
-        return values
-
-    def openid_url(self):
-        """Returns clavid.com authentication URL"""
-        if not self.data.get('openid_user'):
-            raise AuthMissingParameter(self, 'openid_user')
-        return 'http://%s.clavid.com' % self.data['openid_user']
+    openID_url = 'clavid.com'
 
 
-class ClavidChOpenId(OpenIdAuth):
+class ClavidChOpenId(ClavidAbstractBaseOpenId):
     """clavid.ch OpenID authentication backend"""
     name = 'clavid.ch'
-
-    def get_user_details(self, response):
-        """Generate username from identity url"""
-        values = super().get_user_details(response)
-        values['username'] = values.get('username') or \
-            urlparse.urlsplit(response.identity_url).netloc.split('.', 1)[0]
-        return values
-
-    def openid_url(self):
-        """Returns clavid.ch authentication URL"""
-        if not self.data.get('openid_user'):
-            raise AuthMissingParameter(self, 'openid_user')
-        return 'http://%s.clavid.ch' % self.data['openid_user']
+    openID_url = 'clavid.ch'
