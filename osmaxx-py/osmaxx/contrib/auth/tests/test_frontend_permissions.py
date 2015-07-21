370dac353937d73798b4cd2014884b9f1aa95abf
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-07-21 10:57:13 +0200

    Test that users can access frontend when in osmaxx group

--- osmaxx-py/osmaxx/contrib/auth/tests/test_frontend_permissions.py
+++ osmaxx-py/osmaxx/contrib/auth/tests/test_frontend_permissions.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
-from osmaxx.contrib.auth.frontend_permissions import user_in_osmaxx_group
+from django.contrib.auth.models import User, Group
+from osmaxx.contrib.auth.frontend_permissions import user_in_osmaxx_group, FRONTEND_USER_GROUP
 
 
 class TestFrontendPermissions(TestCase):
     def test_superuser_can_access_frontend_even_if_not_in_osmaxx_group(self):
         an_admin = User.objects.create_superuser('A. D. Min', 'admin@example.com', 'password')
         self.assertTrue(user_in_osmaxx_group(an_admin))
+
+    def test_user_can_access_frontend_when_in_osmaxx_group(self):
+        a_user = User.objects.create_user('U. Ser', 'user@example.com', 'password')
+        a_user.groups.add(Group.objects.get(name=FRONTEND_USER_GROUP))
+        self.assertTrue(user_in_osmaxx_group(a_user))
