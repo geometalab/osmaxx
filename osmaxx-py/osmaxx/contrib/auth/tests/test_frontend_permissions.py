f54de932766482aaee50f47129e40432664302e1
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-07-21 11:00:18 +0200

    reorder tests

--- osmaxx-py/osmaxx/contrib/auth/tests/test_frontend_permissions.py
+++ osmaxx-py/osmaxx/contrib/auth/tests/test_frontend_permissions.py
@@ -8,11 +8,11 @@ class TestFrontendPermissions(TestCase):
         a_user = User.objects.create_user('U. Ser', 'user@example.com', 'password')
         self.assertFalse(user_in_osmaxx_group(a_user))
 
-    def test_superuser_can_access_frontend_even_if_not_in_osmaxx_group(self):
-        an_admin = User.objects.create_superuser('A. D. Min', 'admin@example.com', 'password')
-        self.assertTrue(user_in_osmaxx_group(an_admin))
-
     def test_user_can_access_frontend_when_in_osmaxx_group(self):
         a_user = User.objects.create_user('U. Ser', 'user@example.com', 'password')
         a_user.groups.add(Group.objects.get(name=FRONTEND_USER_GROUP))
         self.assertTrue(user_in_osmaxx_group(a_user))
+
+    def test_superuser_can_access_frontend_even_if_not_in_osmaxx_group(self):
+        an_admin = User.objects.create_superuser('A. D. Min', 'admin@example.com', 'password')
+        self.assertTrue(user_in_osmaxx_group(an_admin))
