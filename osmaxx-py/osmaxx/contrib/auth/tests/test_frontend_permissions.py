1b44849f9fac68c6ce0732baded63681cbf58ccb
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-07-21 10:58:30 +0200

    Test that users can not access frontend by default

--- osmaxx-py/osmaxx/contrib/auth/tests/test_frontend_permissions.py
+++ osmaxx-py/osmaxx/contrib/auth/tests/test_frontend_permissions.py
@@ -4,6 +4,10 @@ from osmaxx.contrib.auth.frontend_permissions import user_in_osmaxx_group, FRONT
 
 
 class TestFrontendPermissions(TestCase):
+    def test_user_can_not_access_frontend_by_default(self):
+        a_user = User.objects.create_user('U. Ser', 'user@example.com', 'password')
+        self.assertFalse(user_in_osmaxx_group(a_user))
+
     def test_superuser_can_access_frontend_even_if_not_in_osmaxx_group(self):
         an_admin = User.objects.create_superuser('A. D. Min', 'admin@example.com', 'password')
         self.assertTrue(user_in_osmaxx_group(an_admin))
