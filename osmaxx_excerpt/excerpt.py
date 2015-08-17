bb361652c403b1a2f70e1c48e2dd14d76d7f92cb
IndianCurry <dsharma@hsr.ch>
2015-08-17 09:41:06 +0200

    Removed the redundant code from excerpt.py

--- osmaxx_excerpt/excerpt.py
+++ osmaxx_excerpt/excerpt.py
@@ -3,9 +3,11 @@ import time, subprocess, os, argparse
 import math
 path = os.path.dirname(os.path.realpath(__file__))
 
+
 def name_generator():
     filename='osmaxx_excerpt_'+time.strftime("%Y-%m-%d")+'_'+time.strftime("%H%M%S")
     return filename
+
 
 def to_mercator(mercator_x_lon, mercator_y_lat):
     if math.fabs(mercator_x_lon) > 180 or math.fabs(mercator_y_lat) > 90:
@@ -19,62 +21,17 @@ def to_mercator(mercator_x_lon, mercator_y_lat):
                               (1.0 - math.sin(additional)))
     return [mercator_x_lon, mercator_y_lat]
 
+
 def get_statistics(data, name):
     statcmd = 'bash', './extract/extract_statistics', data[0], data[1], data[2], data[3], name
     subprocess.check_call(statcmd)
+
 
 def call_db(data, name, form):
     get_statistics(data, name)
     dbcmd = 'sh', './extract/extract_db.sh', data[0], data[1], data[2], data[3], name, form
     subprocess.check_call(dbcmd)
 
-#ignore for now
-def call_fgdb(data, name):
-    get_statistics(data, name)
-    gdbcmd = 'sh', './extract/extract_fgdb.sh', data[0], data[1], data[2], data[3], name
-    subprocess.check_call(gdbcmd)
-
-
-def call_shp(data, name):
-    get_statistics(data, name)
-    shpcmd = 'sh', './extract/extract_shp.sh', data[0], data[1], data[2], data[3], name
-    subprocess.check_call(shpcmd)
-
-
-def call_gpkg(data, name):
-    get_statistics(data, name)
-    gpkgcmd = 'sh', './extract/extract_gpkg.sh', data[0], data[1], data[2], data[3], name
-    subprocess.check_call(gpkgcmd)
-
-def call_spatialite(data, name):
-    get_statistics(data, name)
-    spatialitecmd = 'sh', './extract/extract_spatialite.sh', data[0], data[1], data[2], data[3], name
-    subprocess.check_call(spatialitecmd)
-
-
-def call_test():
-    xmin='8.775449276'
-    ymin='47.1892350573'
-    xmax='8.8901920319'
-    ymax='47.2413633153'
-    print 'Testing FileGDB Creation'
-    myfile=name_generator()
-    call_fgdb([xmin, ymin, xmax, ymax], myfile)
-    print 'FileGDB have been created in '+path+'/data/'+myfile
-    print 'Testing GPKG Creation'
-    myfile=name_generator()
-    call_gpkg([xmin, ymin, xmax, ymax], myfile)
-    print 'GPPKG have been created in '+path+'/data/'+myfile
-    print 'Testing ERSI ShapeFile Creation'
-    myfile=name_generator()
-    call_shp([xmin, ymin, xmax, ymax], myfile)
-    print 'ERSI ShapeFile have been created in '+path+'/data/'+myfile
-    print 'Testing Spatialite Creation'
-    myfile=name_generator()
-    call_shp([xmin, ymin, xmax, ymax], myfile)
-    print 'Spatialite have been created in '+path+'/data/'+myfile
-
-#call_test()
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f','--format',choices=['fgdb','shp','gpkg','spatialite'], default='fgdb', help= 'output formats')
@@ -87,20 +44,4 @@ if __name__ == '__main__':
     max_xy=to_mercator(args.xmax, args.ymax)
     name=name_generator()
     call_db([str(min_xy[0]), str(min_xy[1]), str(max_xy[0]), str(max_xy[1])], name, args.format)
-    print name+ 'have been completed in '+args.format+' format.'
-#    if args.format == 'fgdb':
-#   	name=name_generator()
-#    	call_fgdb([str(min_xy[0]), str(min_xy[1]), str(max_xy[0]), str(max_xy[1])], name)
-#    	print name+' have been completed in FileGDB format'
-#    elif args.format == 'shp':
-#    	name=name_generator()
-#    	call_shp([str(min_xy[0]), str(min_xy[1]), str(max_xy[0]), str(max_xy[1])], name)
-#    	print name+' have been completed in ESRI Shapefile format'
-#    elif args.format == 'gpkg':
-#    	name=name_generator()
-#    	call_gpkg([str(min_xy[0]), str(min_xy[1]), str(max_xy[0]), str(max_xy[1])], name)
-#    	print name+' have been completed in GPKG format'
-#    elif args.format == 'spatialite':
-#    	name=name_generator()
-#    	call_spatialite([str(min_xy[0]), str(min_xy[1]), str(max_xy[0]), str(max_xy[1])], name)
-#    	print name+' have been completed in Spatialite format'
+    print name+' have been completed in '+args.format+' format.'
