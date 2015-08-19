b2f3e835cef0742370fc8973fb41b2f33061deb4
Wasabideveloper <tobias.blaser@hsr.ch>
2015-08-19 16:48:29 +0200

    Update excerpt on map on selection of existing excerpt in list

--- osmaxx-py/osmaxx/excerptexport/static/excerptexport/scripts/map.js
+++ osmaxx-py/osmaxx/excerptexport/static/excerptexport/scripts/map.js
@@ -13,6 +13,9 @@ var inputElementWest = document.getElementById('new_excerpt_bounding_box_west');
 var inputElementEast = document.getElementById('new_excerpt_bounding_box_east');
 var inputElementSouth = document.getElementById('new_excerpt_bounding_box_south');
 
+var selectExistingExcerpts = document.getElementById('existing_excerpt.id');
+var formPartsSwitcher = document.getElementById('form-mode');
+
 
 var locationFilter = new L.LocationFilter({
     enable: true,
@@ -26,15 +29,28 @@ function updateBboxTextInputsWith(bounds) {
     inputElementSouth.value = bounds._southWest.lat;
 }
 
+/**
+ * if the shown excerpt on the map changes and the change was not made by selecting an existing excerpt in the list -> a user changed the excerpt, so change the form part the
+ */
+function showNewExcerptPart(locationFilterBounds) {
+    if(!(selectExistingExcerpts.value != "" &&
+            selectExistingExcerpts.querySelector('option[value="'+selectExistingExcerpts.value+'"]').getAttribute('data-north') == locationFilterBounds._northEast.lat &&
+            selectExistingExcerpts.querySelector('option[value="'+selectExistingExcerpts.value+'"]').getAttribute('data-west') == locationFilterBounds._southWest.lng &&
+            selectExistingExcerpts.querySelector('option[value="'+selectExistingExcerpts.value+'"]').getAttribute('data-east') == locationFilterBounds._northEast.lng &&
+            selectExistingExcerpts.querySelector('option[value="'+selectExistingExcerpts.value+'"]').getAttribute('data-south') == locationFilterBounds._southWest.lat)) {
+        formPartsSwitcher.value = 'new-excerpt';
+        formPartsSwitcher.dispatchEvent(new Event('change'));
+    }
+}
+
 
 // push initial values to text inputs
 updateBboxTextInputsWith(locationFilter.getBounds());
 
 // update text input values upon change on map
 locationFilter.on("change", function (e) {
-    console.log(locationFilter);
-    console.log(locationFilter.getBounds());
     updateBboxTextInputsWith(e.bounds);
+    showNewExcerptPart(e.bounds);
 });
 
 
@@ -54,3 +70,23 @@ inputElementNorth.addEventListener('change', updateMapBoundingBox);
 inputElementWest.addEventListener('change', updateMapBoundingBox);
 inputElementEast.addEventListener('change', updateMapBoundingBox);
 inputElementSouth.addEventListener('change', updateMapBoundingBox);
+
+selectExistingExcerpts.addEventListener('change', function(event) {
+    var excerptOption = event.explicitOriginalTarget;
+    if(excerptOption.getAttribute('data-geometry') == 'boundingbox') {
+        var bounds = locationFilter.getBounds();
+        bounds._northEast.lat = excerptOption.getAttribute('data-north');
+        bounds._southWest.lng = excerptOption.getAttribute('data-west');
+        bounds._northEast.lng = excerptOption.getAttribute('data-east');
+        bounds._southWest.lat = excerptOption.getAttribute('data-south');
+        locationFilter.setBounds(bounds);
+        locationFilter.enable();
+    } else {
+        locationFilter.disable();
+    }
+});
+
+formPartsSwitcher.addEventListener('change', function(event) {
+    locationFilter.enable();
+});
+
