6005b0a2cba2b51d3503a8fb2b9f8046a5cb5e7b
Wasabi Developer <wasabideveloper@users.noreply.github.com>
2015-08-24 11:45:06 +0200

    Fix indentation

--- osmaxx-py/osmaxx/excerptexport/static/excerptexport/scripts/map.js
+++ osmaxx-py/osmaxx/excerptexport/static/excerptexport/scripts/map.js
@@ -84,15 +84,15 @@
 
 
     var map = L.map('map').setView([0, 0], 2);
-        // add an OpenStreetMap tile layer
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+    // add an OpenStreetMap tile layer
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-        var locationFilter = new L.LocationFilter({
-            enable: true,
-            enableButton: false
-        }).addTo(map);
+    var locationFilter = new L.LocationFilter({
+        enable: true,
+        enableButton: false
+    }).addTo(map);
 
     var excerptManager = new ExcerptManager(
         locationFilter,
