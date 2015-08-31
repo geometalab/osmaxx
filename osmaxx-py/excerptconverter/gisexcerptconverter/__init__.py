61cccf3a65eb0704ffa8cd1d41a7a37778b5b61a
Raphael Das Gupta <github.com@raphael.dasgupta.ch>
2015-08-31 11:09:09 +0200

    remove commented-out import

--- osmaxx-py/excerptconverter/gisexcerptconverter/__init__.py
+++ osmaxx-py/excerptconverter/gisexcerptconverter/__init__.py
@@ -1,4 +1,3 @@
-#from .execute_converting_blackbox import PostGisExcerptConverter
 from excerptconverter.baseexcerptconverter import BaseExcerptConverter
 from .gis_excerpt_converter import GisExcerptConverter
 
