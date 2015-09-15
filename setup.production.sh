b795672a1b2849cf9376364aa9493f8c603ea109
Wasabideveloper <tobias.blaser@hsr.ch>
2015-09-15 10:13:06 +0200

    Update production setup

--- setup.production.sh
+++ setup.production.sh
@@ -21,4 +21,4 @@ docker run -v "$(pwd):/app" -v "/var/run/docker.sock:/var/run/docker.sock" -e "C
 sleep 3
 docker run -v "$(pwd):/app" -v "/var/run/docker.sock:/var/run/docker.sock" -e "COMPOSE_PROJECT_NAME=osmaxx" -it --rm "dduportal/docker-compose:$DOCKER_COMPOSE_TAG" \
     run --rm webapp /bin/bash -c "python3 manage.py migrate && python3 manage.py createsuperuser"
-docker run -v "$(pwd):/app" -v "/var/run/docker.sock:/var/run/docker.sock" -e "COMPOSE_PROJECT_NAME=osmaxx" --rm "dduportal/docker-compose:1.3.1" stop
+docker run -v "$(pwd):/app" -v "/var/run/docker.sock:/var/run/docker.sock" -e "COMPOSE_PROJECT_NAME=osmaxx" --rm "dduportal/docker-compose:$DOCKER_COMPOSE_TAG" stop
