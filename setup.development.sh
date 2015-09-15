62c2c99c6c588069c552d6b490a6344ef37f6c9e
Wasabideveloper <tobias.blaser@hsr.ch>
2015-09-15 10:13:17 +0200

    Add development setup

---
+++ setup.development.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Setup a docker compose container used to start the development application
+# Setup application & database
+
+# stop containers & cleanup
+docker-compose stop
+docker-compose rm -f
+
+# pull & build
+docker-compose pull
+docker-compose build
+
+# migrate
+docker-compose up -d databasedev
+sleep 3
+docker-compose run webappdev /bin/bash -c './manage.py migrate && ./manage.py createsuperuser'
+
