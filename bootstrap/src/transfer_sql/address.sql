e676aeef17c2c46059d4885582e4aa98c490cd16
IndianCurry <dsharma@hsr.ch>
2015-09-21 10:46:54 +0200

    changed parameterin address.sql

--- bootstrap/src/transfer_sql/address.sql
+++ bootstrap/src/transfer_sql/address.sql
@@ -53,7 +53,7 @@ INSERT INTO osmaxx.address_p
 	"addr:place" as city,
     "addr:country" as country
   FROM osm_point
-where building !='entrance' and (entrance !='yes' or entrance ='')
+where building !='entrance' and ("addr:street" !='' OR "addr:housenumber"!='' OR "addr:place"!='')
 
 -- Without the Entrance Node and the addresses are part of a way  --
 UNION
@@ -83,10 +83,10 @@ UNION
 	"addr:place" as city,
     "addr:country" as country
   FROM osm_polygon
-where building !='entrance';
+where building !='entrance' and ("addr:street" !='' OR "addr:housenumber"!='' OR "addr:place"!='');
 
 --------------
--- entrance --
+-- Entrance and Nodes--
 --------------
 
 -- With the entrance node --
@@ -95,37 +95,10 @@ INSERT INTO osmaxx.address_p
 	osm_timestamp as lastchange,
 	'N' AS geomtype, 	-- Node 
 	way AS geom,
-	'e' AS type,
-	name as name,
-	"name:en" as name_en, 
-	"name:fr" as name_fr, 
-	"name:es" as name_es, 
-	"name:de" as name_de, 
-	int_name as name_int,
-	transliterate(name) as label,
-	cast(tags as text) as tags,
-	case 
-	when "addr:street" is not null then "addr:street"
-	when "addr:place" is not null then "addr:place"
-	else NULL
-    end  as street,
-	"addr:housenumber" as housenumber,
-	"addr:postcode" as postcode,
-	"addr:place" as city,
-    "addr:country" as country
-  FROM osm_point
-  where (building='entrance' or entrance='yes') 
-
--- With the entrance node and the addresses are part of a way --
-  UNION
-  SELECT osm_id as osm_id,
-	osm_timestamp as lastchange,
-	CASE 
-	 WHEN osm_id<0 THEN 'R' -- R=Relation 
-	 ELSE 'W' 		-- W=Way
-	 END AS geomtype, 
-	ST_Centroid(way) AS geom,
-	'e' AS type,
+    case
+        when (building='entrance' or entrance='yes') then 'e'
+        else 'p'
+    end as type,
 	name as name,
 	"name:en" as name_en, 
 	"name:fr" as name_fr, 
@@ -143,38 +116,8 @@ INSERT INTO osmaxx.address_p
 	"addr:postcode" as postcode,
 	"addr:place" as city,
     "addr:country" as country
-  FROM osm_polygon
-  where (building='entrance') ;
-
-
--------------
---  place  --
--------------
-INSERT INTO osmaxx.address_p
-  SELECT osm_id as osm_id,
-	osm_timestamp as lastchange,
-	'N' AS geomtype, 
-	way AS geom,
-	'p' AS type,
-	name as name,
-	"name:en" as name_en, 
-	"name:fr" as name_fr, 
-	"name:es" as name_es, 
-	"name:de" as name_de, 
-	int_name as name_int,
-	transliterate(name) as label,
-	cast(tags as text) as tags,
-	case 
-		when "addr:street" is not null then "addr:street"
-		when "addr:place" is not null then "addr:place"
-		else NULL
-    end  as street,
-	"addr:housenumber" as housenumber,
-	"addr:postcode" as postcode,
-	"addr:place" as city,
-    "addr:country" as country
-  FROM osm_point;
-
+  FROM osm_point
+  where ("addr:street" !='' OR "addr:housenumber"!='' OR "addr:place"!='');
 
 -------------------
 -- interpolation --
