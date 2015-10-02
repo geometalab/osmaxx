eb6d97b28b800c12e48ad772c090b9e294f94e62
Nicola Jordan <njordan.hsr@gmail.com>
2015-10-02 16:47:59 +0200

    saner default logging setup for use in docker containers

--- osmaxx-py/config/settings/common.py
+++ osmaxx-py/config/settings/common.py
@@ -288,25 +288,17 @@ CACHES = {
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
     },
     'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
         },
-    }
+    },
 }
 
 # login url if param 'next' is not set
