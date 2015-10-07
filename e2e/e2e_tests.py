729f556e76c67cbba295049ba16ee2e05d8c658c
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-10-07 15:03:30 +0200

    rename method
    this is not about whether downloading has failed, but whether extraction has failed

--- e2e/e2e_tests.py
+++ e2e/e2e_tests.py
@@ -147,10 +147,10 @@ class EndToEndTests(unittest.TestCase):
     def _is_download_finished(self):
         self._reload_browser()
         status_element = self._get_status_element()
-        self.assertFalse(self._is_download_failed(status_element))
+        self.assertFalse(self._is_extraction_failed(status_element))
         return '✓' in status_element
 
-    def _is_download_failed(self, status_element):
+    def _is_extraction_failed(self, status_element):
         return '∅' in status_element
 
     def _reload_browser(self):
