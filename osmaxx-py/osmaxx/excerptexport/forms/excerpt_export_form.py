31e5661e3826934d6a0c5474f71a1944e59f2886
Nicola Jordan <njordan@hsr.ch>
2015-10-12 22:53:43 +0200

    form using helpers

--- osmaxx-py/osmaxx/excerptexport/forms/excerpt_export_form.py
+++ osmaxx-py/osmaxx/excerptexport/forms/excerpt_export_form.py
@@ -1,29 +1,36 @@
 from django import forms
+from django.http import Http404
 from django.utils.translation import ugettext as _
 
 from crispy_forms import helper as form_helper
 from crispy_forms import layout as form_layout
-from .temporary_form_helper import available_format_choices
+from excerptconverter import ConverterManager
+from osmaxx.excerptexport.models import BBoxBoundingGeometry, Excerpt, ExtractionOrder
+from osmaxx.utilities.dict_helpers import select_keys
+from .temporary_form_helper import available_format_choices, get_export_options
 
 
-class ExcerptOrderFormPartCoordinates(forms.Form):
-    excerpt_name = forms.CharField(
+class ExcerptOrderFormPartExcerptNameMixin(forms.Form):
+    name = forms.CharField(
         label=_('Excerpt name'),
         required=True
     )
-    excerpt_bounding_box_north = forms.FloatField(
+
+
+class ExcerptOrderFormPartCoordinatesMixin(ExcerptOrderFormPartExcerptNameMixin, forms.Form):
+    north = forms.FloatField(
         label=_('North'),
         required=True,
     )
-    excerpt_bounding_box_west = forms.FloatField(
+    west = forms.FloatField(
         label=_('West'),
         required=True,
     )
-    excerpt_bounding_box_east = forms.FloatField(
+    east = forms.FloatField(
         label=_('East'),
         required=True,
     )
-    excerpt_bounding_box_south = forms.FloatField(
+    south = forms.FloatField(
         label=_('South'),
         required=True,
     )
@@ -33,39 +40,38 @@ class ExcerptOrderFormPartCoordinates(forms.Form):
             form_layout.HTML('<h2>' + _('New excerpt') + '</h2>'),
             form_layout.Fieldset(
                 _('Create new excerpt'),
-                'excerpt_name',
+                'name',
             ),
             form_layout.Div(
                 form_layout.Fieldset(
                     _('Bounding box'),
                     form_layout.Div(
-                        form_layout.Field('excerpt_bounding_box_north', wrapper_class='column-stretch-4'),
+                        form_layout.Field('north', wrapper_class='column-stretch-4'),
                         css_class="box-column-container balanced",
                     ),
                     form_layout.Div(
-                        form_layout.Field('excerpt_bounding_box_west', wrapper_class='column-stretch-4'),
-                        form_layout.Field('excerpt_bounding_box_east', wrapper_class='column-stretch-4'),
+                        form_layout.Field('west', wrapper_class='column-stretch-4'),
+                        form_layout.Field('east', wrapper_class='column-stretch-4'),
                         css_class="box-column-container separated",
                     ),
                     form_layout.Div(
-                        form_layout.Field('excerpt_bounding_box_south', wrapper_class='column-stretch-4'),
+                        form_layout.Field('south', wrapper_class='column-stretch-4'),
                         css_class="box-column-container balanced",
                     ),
                 ),
                 css_class='form-group',
                 id='bbox-values',
             ),
-            id=FormMode.MODE_NEW,
+            id=FormModeMixin.MODE_NEW,
         )
 
 
-class ExcerptOrderFormCommonPart(forms.Form):
-    excerpt_is_public = forms.BooleanField(
+class ExcerptOrderFormCommonPartMixin(forms.Form):
+    is_public = forms.BooleanField(
         label=_('Public'),
         required=False,
         help_text=_("Others will see name and bounding box of this excerpt. "
                     "They won't see that you created it or when you did so."),
-        # widget=forms.CheckboxInput,
         initial=True,
     )
     formats = forms.MultipleChoiceField(
@@ -79,7 +85,7 @@ class ExcerptOrderFormCommonPart(forms.Form):
         return form_layout.Div(
             form_layout.Fieldset(
                 _('Visibility'),
-                'excerpt_is_public',
+                'is_public',
             ),
             form_layout.Fieldset(
                 _('GIS export formats'),
@@ -88,7 +94,7 @@ class ExcerptOrderFormCommonPart(forms.Form):
         )
 
 
-class ExcerptOrderFormPartExisting(forms.Form):
+class ExcerptOrderFormPartExistingMixin(forms.Form):
     existing_excerpts = forms.ChoiceField(
         label=_('Excerpt'),
         required=True,
@@ -97,15 +103,22 @@ class ExcerptOrderFormPartExisting(forms.Form):
     def form_layout(self):
         return form_layout.Div(
             form_layout.HTML('<h2>' + _('Existing excerpt') + '</h2>'),
+            form_layout.HTML('''
+                <div class="form-group has-feedback">
+                    <input id="excerptListFilterField" class="form-control" type="search" placeholder="Filter excerpts …" autocomplete="off"/>
+                    <span id="excerptListFilterFieldClearer" class="clearer glyphicon glyphicon-remove-circle form-control-feedback"></span>
+                </div>
+                '''  # noqa: line too long ignored
+             ),
             form_layout.Fieldset(
                 _('Existing excerpts'),
                 'existing_excerpts',
             ),
-            id=FormMode.MODE_EXISTING,
+            id=FormModeMixin.MODE_EXISTING,
         )
 
 
-class FormMode(forms.Form):
+class FormModeMixin(forms.Form):
     MODE_EXISTING = 'existing-excerpt'
     MODE_NEW = 'new-excerpt'
     FORM_MODE_CHOICES = (
@@ -120,23 +133,43 @@ class FormMode(forms.Form):
     )
 
     def form_layout(self):
-        return form_layout.Div(form_layout.Field(
-               'form_mode',
-                css_class='btn btn-default',
-            ))
+        return form_layout.HTML('''
+            <div class="form-group select-button-group">
+                <select class="btn-group" role="group" name="form_mode" id="id_form_mode" size="2" required="required" data-form-part-switcher>
+                    <option class="btn btn-default" value="{existing_value}" selected="selected" data-form-part-for="existing-excerpt">{existing_excerpt_string}</option>
+                    <option class="btn btn-default" value="{new_value}" data-form-part-for="new-excerpt">{new_excerpt_string}</option>
+                </select>
+            </div>
+            '''  # noqa: line too long ignored
+        .format(
+            existing_excerpt_string=self.FORM_MODE_CHOICES[0][1],
+            new_excerpt_string=self.FORM_MODE_CHOICES[1][1],
+            new_value=self.MODE_NEW,
+            existing_value=self.MODE_EXISTING,
+            )
+        )
 
 
-class ExcerptOrderForm(ExcerptOrderFormPartCoordinates, ExcerptOrderFormCommonPart, FormMode, ExcerptOrderFormPartExisting):
+class ExcerptOrderForm(ExcerptOrderFormPartCoordinatesMixin, ExcerptOrderFormCommonPartMixin, FormModeMixin,
+                       ExcerptOrderFormPartExistingMixin, forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = form_helper.FormHelper()
         self.helper.html5_required = True
         self.helper.form_tag = False
         self.helper.layout = form_layout.Layout(
-            FormMode(self).form_layout(),
-            ExcerptOrderFormPartExisting(self).form_layout(),
-            ExcerptOrderFormPartCoordinates(self).form_layout(),
-            ExcerptOrderFormCommonPart(self).form_layout(),
+            FormModeMixin(self).form_layout(),
+            form_layout.Div(
+                ExcerptOrderFormPartExistingMixin(self).form_layout(),
+                css_class="form-part",
+                data_form_part="existing-excerpt",
+            ),
+            form_layout.Div(
+                ExcerptOrderFormPartCoordinatesMixin(self).form_layout(),
+                css_class="form-part",
+                data_form_part="new-excerpt",
+            ),
+            ExcerptOrderFormCommonPartMixin(self).form_layout(),
         )
         self.helper.add_input(form_layout.Submit('submit', 'Submit'))
 
@@ -148,19 +181,54 @@ class ExcerptOrderForm(ExcerptOrderFormPartCoordinates, ExcerptOrderFormCommonPa
 
         form_mode = self.cleaned_data['form_mode']
 
-        if form_mode == FormMode.MODE_EXISTING:
-            self._remove_errors_for_non_participating_fields(fields=ExcerptOrderFormPartCoordinates().fields)
+        if form_mode == FormModeMixin.MODE_EXISTING:
+            self._remove_errors_for_non_participating_fields(fields=ExcerptOrderFormPartCoordinatesMixin().fields)
 
-        if form_mode == FormMode.MODE_NEW:
-            self._remove_errors_for_non_participating_fields(fields=ExcerptOrderFormPartExisting().fields)
+        if form_mode == FormModeMixin.MODE_NEW:
+            self._remove_errors_for_non_participating_fields(fields=ExcerptOrderFormPartExistingMixin().fields)
 
         return cleaned_data
 
+    def save(self, user):
+        extraction_order = ExtractionOrder(orderer=user)
+        extraction_order.extraction_configuration = self._generate_extraction_options()
+
+        if self.cleaned_data['form_mode'] == FormModeMixin.MODE_EXISTING:
+            existing_excerpt = Excerpt.objects.get(pk=int(self.cleaned_data['existing_excerpts']))
+            extraction_order.excerpt = existing_excerpt
+        elif self.cleaned_data['form_mode'] == FormModeMixin.MODE_NEW:
+            bbox_kwargs = select_keys(self.cleaned_data, ['north', 'east', 'south', 'west'])
+
+            bounding_geometry = BBoxBoundingGeometry(**bbox_kwargs)
+            bounding_geometry.save()
+
+            excerpt_dict = select_keys(self.cleaned_data, ['name', 'is_public'])
+            excerpt = Excerpt(
+                is_active=True,
+                bounding_geometry=bounding_geometry,
+                owner=user,
+                **excerpt_dict
+            )
+            excerpt.save()
+            extraction_order.excerpt = excerpt
+        else:
+            raise Http404()
+        extraction_order.save()
+        self._execute_converters(extraction_order)
+        return extraction_order
+
+    # helper methods
+    def _execute_converters(self, extraction_order):
+        converter_manager = ConverterManager(extraction_order)
+        converter_manager.execute_converters()
+
+    def _generate_extraction_options(self):
+        return get_export_options(self.cleaned_data['formats'])
+
     def _remove_errors_for_non_participating_fields(self, fields):
         not_to_be_validated_fields = fields
-        for key_name, _ in not_to_be_validated_fields.items():
+        for key_name, _ignored in not_to_be_validated_fields.items():
             try:
                 self._errors.pop(key_name)
             except KeyError:
-                    pass
-
+                pass
