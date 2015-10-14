eab7f24587006f450b6d0b47384be1a3482330d6
Nicola Jordan <njordan.hsr@gmail.com>
2015-10-14 16:02:16 +0200

    excerpt export api excerpt view

--- osmaxx-py/osmaxx/excerptexport/rest_api/views.py
+++ osmaxx-py/osmaxx/excerptexport/rest_api/views.py
@@ -1,8 +1,10 @@
 from django.db import models
 from rest_framework import generics
-from osmaxx.contrib.auth.frontend_permissions import AuthenticatedAndAccessPermission, HasBBoxAccessPermission
+from osmaxx.contrib.auth.frontend_permissions import AuthenticatedAndAccessPermission, HasBBoxAccessPermission, \
+    HasExcerptAccessPermission
+from osmaxx.excerptexport.models import Excerpt
 from osmaxx.excerptexport.models.bounding_geometry import BoundingGeometry
-from osmaxx.excerptexport.rest_api.serializers import BoundingGeometrySerializer
+from osmaxx.excerptexport.rest_api.serializers import BoundingGeometrySerializer, BoundingGeometryFromExcerptSerializer
 
 
 class BoundingGeometryMixin:
@@ -17,4 +19,19 @@ class BoundingGeometryList(BoundingGeometryMixin, generics.ListAPIView):
 
 
 class BoundingGeometryDetail(BoundingGeometryMixin, generics.RetrieveAPIView):
+    pass
+
+
+class BoundingGeometryFromExcerptMixin:
+    serializer_class = BoundingGeometryFromExcerptSerializer
+    permission_classes = (AuthenticatedAndAccessPermission, HasExcerptAccessPermission,)
+    queryset = Excerpt.objects.all()
+
+
+class BoundingGeometryFromExcerptList(BoundingGeometryFromExcerptMixin, generics.ListAPIView):
+    def get_queryset(self):
+        return self.queryset.filter(models.Q(owner=self.request.user) | models.Q(is_public=True))
+
+
+class BoundingGeometryFromExcerptDetail(BoundingGeometryFromExcerptMixin, generics.RetrieveAPIView):
     pass
