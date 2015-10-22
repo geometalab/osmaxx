52afa07bfa999ca403b8355d321e8a9ebdef5649
Nicola Jordan <njordan.hsr@gmail.com>
2015-10-22 16:03:15 +0200

    manager code: flakeit

--- runtests.py
+++ runtests.py
@@ -12,7 +12,7 @@ PYTEST_ARGS = {
     'fast': ['tests', '-q'],
 }
 
-FLAKE8_ARGS = ['osmaxx_conversion_service', 'tests', 'worker', 'converters', 'utils', '--ignore=E501']
+FLAKE8_ARGS = ['osmaxx_conversion_service', 'tests', 'manager', 'worker', 'converters', 'utils', '--ignore=E501']
 
 sys.path.append(os.path.dirname(__file__))
 
