35c1ae6726ad9adb7f52d3b4c8707fa3820966fb
Nicola Jordan <njordan@hsr.ch>
2015-10-27 20:58:55 +0100

    better use an 'as' statement then an assignement

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,5 @@
-# with statements are use as if they were function, so let's return it as if it was one
-from utils.directory_changer_helper import ChDirWith
-
-chg_dir_with = ChDirWith
+# with statements are used as if they were function, so let's return it as if it was one
+from utils.directory_changer_helper import ChDirWith as chg_dir_with
 
 __all__ = [
     'chg_dir_with',
