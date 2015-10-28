53404d5b130bbf58602c8854fc8fc06e41b47bbb
Raphael Das Gupta <github.com@raphael.dasgupta.ch>
2015-10-28 10:02:04 +0100

    rephrased comment

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
-# with statements are used as if they were function, so let's return it as if it was one
+# As `with`-statements are used as if they were functions, let's expose chg_dir_with under a name
+# following function naming conventions (snake_case) rather than class naming conventions (CamelCase):
 from utils.directory_changer_helper import ChDirWith as chg_dir_with
 
 __all__ = [
