5a734280839451f9b7e438e71300f4e5f1a3db87
Nicola Jordan <njordan.hsr@gmail.com>
2015-11-02 13:55:13 +0100

    lowercase class that is used as if it where a function

--- utils/directory_changer_helper.py
+++ utils/directory_changer_helper.py
@@ -2,7 +2,7 @@ import shutil
 import os
 
 
-class ChDirWith:
+class chg_dir_with:
     """
     Changes into a arbitrary directory, switching back to the directory after execution of the with statement.
 
