bd1d81a38311db76d565aeb38cb16d20ad4db6a1
Nicola Jordan <njordan.hsr@gmail.com>
2015-11-02 14:48:52 +0100

    cut pbf where we know the boundaries already

--- converters/boundaries.py
+++ converters/boundaries.py
@@ -1,3 +1,7 @@
+import subprocess
+from converters.converter_settings import OSMAXX_CONVERSION_SERVICE
+
+
 class BBox:
     """
     A pickleable Bounding Box object
@@ -10,6 +14,24 @@ class BBox:
     """
     def __init__(self, west, south, east, north):
         self.west, self.south, self.east, self.north = west, south, east, north
+
+    def cut_pbf(self, output_filename):
+        xapi_mirror = OSMAXX_CONVERSION_SERVICE.get('XAPI_MIRROR')
+        # Download the region map specified through the given coordinates
+        command = "wget -qO- {xapi_mirror}?map?bbox={west},{south},{east},{north}".format(
+            xapi_mirror=xapi_mirror,
+            west=self.west,
+            south=self.south,
+            east=self.east,
+            north=self.north,
+        )
+        p_wget = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+
+        command = "osmconvert --out-pbf -o={output_filename} -".format(
+            output_filename=output_filename,
+        )
+        subprocess.check_call(command.split(), stdin=p_wget.stdout)
+        return output_filename
 
 
 __all__ = [
