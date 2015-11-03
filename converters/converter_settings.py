361c91ecdaab6e1e79dd5a4c79072fafc6c78a04
Nicola Jordan <njordan.hsr@gmail.com>
2015-11-03 12:08:09 +0100

    add missing setting

--- converters/converter_settings.py
+++ converters/converter_settings.py
@@ -3,6 +3,7 @@ from django.conf import settings
 OSMAXX_CONVERSION_SERVICE = {
     'XAPI_MIRROR': 'http://www.overpass-api.de/api/xapi_meta',
     'WORKDIR': '/tmp/osm_converter',
+    'RESULTDIR': '/tmp/results',
     'RESULT_TTL': -1,  # never expire!
 }
 
