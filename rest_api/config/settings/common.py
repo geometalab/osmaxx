c6b413961be757e03e1f8d47d162b68224757917
Nicola Jordan <njordan.hsr@gmail.com>
2015-11-03 15:44:43 +0100

    add new app to settings

--- rest_api/config/settings/common.py
+++ rest_api/config/settings/common.py
@@ -41,6 +41,7 @@ THIRD_PARTY_APPS = (
 )
 # Apps specific for this project go here.
 LOCAL_APPS = (
+    'conversion_job',
     # 'version',
     # 'api.converters.core',
     # 'api.converters.gis',
