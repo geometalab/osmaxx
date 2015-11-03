e94e14ac5f10f500cba2941430ca9554a45efd88
Nicola Jordan <njordan.hsr@gmail.com>
2015-11-03 15:43:10 +0100

    move manager into rest_api

--- rest_api/manager/job_manager.py
+++ rest_api/manager/job_manager.py
@@ -1,8 +1,9 @@
 import random
 import string
+
 from converters import converter_settings
 from converters.boundaries import BBox
-from manager.rq_helper import rq_enqueue_with_settings
+from rest_api.manager.rq_helper import rq_enqueue_with_settings
 from worker.converter_job import convert
 
 
@@ -20,11 +21,12 @@ class ConversionJobManager:
         self.geometry = geometry if geometry else BBox(29.525547623634335, 40.77546776498174, 29.528980851173397, 40.77739734768811)
         self.format_options = format_options if format_options else dict(formats=['fgdb', 'spatialite', 'shp', 'gpkg'])
 
-    def start_conversion(self):
+    def start_conversion(self, callback_url):
         output_directory = self.result_directory + ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
         # todo: ensure job is cleaned up after files have been requested -> in rest_api
         return rq_enqueue_with_settings(
             convert,
+            callback_url=callback_url,
             geometry=self.geometry,
             format_options=self.format_options,
             output_directory=output_directory,
