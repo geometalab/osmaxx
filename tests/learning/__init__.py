4fb70d9900ff20687da68dce41d9bc37b433a119
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-11-04 16:07:14 +0100

    explain 'learning tests' in package docstring

---
+++ tests/learning/__init__.py
@@ -0,0 +1,5 @@
+"""
+Tests that don't test our own code, but show us (and let us explore) how foreign code can be used and how it (re)acts.
+
+These tests might still use some of our own code as utility to facilitate testing of foreign code.
+"""
