f9ca1439aa2daa2ebf8ffc17d20df0f6bd3f06a8
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-11-04 14:29:14 +0100

    rename test job function

--- tests/learning/rq_test.py
+++ tests/learning/rq_test.py
@@ -7,7 +7,7 @@ from manager.rq_helper import rq_enqueue_with_settings
 from tests.redis_test_helpers import perform_all_jobs_sync
 
 
-def hello():
+def store_helloworld_in_job():
     job = get_current_job(connection=get_connection())
     job.meta['world'] = 'hello'
     job.save()
@@ -15,12 +15,12 @@ def hello():
 
 class RQConnectionTests(django.test.TestCase):
     def test_meta_not_available_without_fetch(self, *args, **kwargs):
-        job = rq_enqueue_with_settings(hello)
+        job = rq_enqueue_with_settings(store_helloworld_in_job)
         perform_all_jobs_sync()
         self.assertDictEqual(job.meta, {})
 
     def test_meta_retrieval_with_fetch_succeeds(self, *args, **kwargs):
-        job = rq_enqueue_with_settings(hello)
+        job = rq_enqueue_with_settings(store_helloworld_in_job)
         perform_all_jobs_sync()
         job_fetched = Job.fetch(job.id, connection=get_connection())
         self.assertDictEqual(job_fetched.meta, {'world': 'hello'})
