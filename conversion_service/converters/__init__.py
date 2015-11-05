d96354f052dea29f0389cea25ec6692746151af1
Nicola Jordan <njordan.hsr@gmail.com>
2015-11-05 08:20:14 +0100

    define choices dict at one place with test

--- conversion_service/converters/__init__.py
+++ conversion_service/converters/__init__.py
@@ -2,6 +2,11 @@ from converters.gis_converter import options as gis_options
 
 converter_options = gis_options
 
+CONVERTER_CHOICES = dict(
+    output_formats=[(format, format) for format in converter_options.get_output_formats()],
+)
+
 __all__ = [
     'converter_options',
+    'CONVERTER_CHOICES',
 ]
--- tests/rest_api/output_format_test.py
+++ tests/rest_api/output_format_test.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from converters import converter_options
+from converters import converter_options, CONVERTER_CHOICES
 from rest_api.serializers import ConverterOptionsSerializer
 
 
@@ -8,3 +8,6 @@ class OutPutFormatTest(TestCase):
     def test_all_attributes_are_present_in_serializer(self):
         serialized = ConverterOptionsSerializer(converter_options)
         self.assertDictEqual(serialized.data, converter_options.__dict__)
+
+    def test_all_attributes_are_present_in_choices_dict(self):
+        self.assertEqual(converter_options.__dict__.keys(), CONVERTER_CHOICES.keys())
