30009a2ab9bc812febc2243a0c2b17eae5ec6617
Nicola Jordan <njordan.hsr@gmail.com>
2015-11-06 11:09:35 +0100

    clean ConversionManager from default arguments

--- conversion_service/manager/job_manager.py
+++ conversion_service/manager/job_manager.py
@@ -1,9 +1,7 @@
 import random
 import string
 
-from converters.boundaries import BBox
 from converters import converter_settings
-from converters.converter import Options
 from manager.rq_helper import rq_enqueue_with_settings
 from worker.converter_job import convert
 
@@ -17,10 +15,9 @@ class ConversionJobManager:
 
     result_directory = converter_settings.OSMAXX_CONVERSION_SERVICE['RESULTDIR']
 
-    # FIXME: do not allow None for required kwargs
-    def __init__(self, geometry=None, format_options=None):
-        self.geometry = geometry if geometry else BBox(29.525547623634335, 40.77546776498174, 29.528980851173397, 40.77739734768811)
-        self.format_options = format_options if format_options else Options(output_formats=['fgdb', 'spatialite', 'shp', 'gpkg'])
+    def __init__(self, geometry, format_options):
+        self.geometry = geometry  # BBox(29.525547623634335, 40.77546776498174, 29.528980851173397, 40.77739734768811)
+        self.format_options = format_options  # Options(output_formats=['fgdb', 'spatialite', 'shp', 'gpkg'])
 
     def start_conversion(self, callback_url):
         output_directory = self.result_directory + ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
