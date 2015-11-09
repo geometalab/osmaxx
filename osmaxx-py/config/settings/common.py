8aaf7472f7e8b4528334b385f14720201cf8f7cf
Wasabideveloper <tobias.blaser@hsr.ch>
2015-11-09 14:37:39 +0100

    Add content type description to download file and improve download file name

--- osmaxx-py/config/settings/common.py
+++ osmaxx-py/config/settings/common.py
@@ -390,7 +390,7 @@ MESSAGE_TAGS = {
 }
 
 OSMAXX = {
-    'download_file_name': '%(date)s-%(excerpt_name)s-%(content_type)s-%(id)s.%(file_extension)s',
+    'download_file_name': '%(date)s-%(excerpt_name)s-%(id)s.%(content_type)s.%(file_extension)s',
     'download_chunk_size': 8192,
     'orders_history_number_of_items': 100,
     # The email adress of this user will be used to generate the mailto link for users
