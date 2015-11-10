5e4d2a3b6085a8d1fa0f09e33beb9e1dcf5aa6e6
Nicola Jordan <njordan.hsr@gmail.com>
2015-11-10 09:45:01 +0100

    no test for cmd line call

--- conversion_service/converters/gis_converter/bootstrap/bootstrap.py
+++ conversion_service/converters/gis_converter/bootstrap/bootstrap.py
@@ -4,7 +4,7 @@ import subprocess
 from utils import chg_dir_with
 
 
-def boostrap(pbf_file_path):
+def boostrap(pbf_file_path):  # pragma: nocover
     with chg_dir_with(os.path.dirname(__file__)):
         boostrap_cmd = 'sh', 'main-bootstrap.sh', pbf_file_path
         subprocess.check_call(boostrap_cmd)
