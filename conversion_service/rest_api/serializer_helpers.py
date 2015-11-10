29606de4e63c8be39f9dd85652ff0bc7afae1896
Nicola Jordan <njordan.hsr@gmail.com>
2015-11-10 10:16:07 +0100

    no test for simple model validation mixin

--- conversion_service/rest_api/serializer_helpers.py
+++ conversion_service/rest_api/serializer_helpers.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 
-class ModelSideValidationMixin(object):
+class ModelSideValidationMixin(object):  # pragma: nocover
     def validate(self, data):
         try:
             self.Meta.model(**data).clean()
