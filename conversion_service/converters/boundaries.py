32deb58b8123884de95a700f2cf63c1c9162d761
Nicola Jordan <njordan.hsr@gmail.com>
2015-11-11 09:12:30 +0100

    refactoring: boundaries; added testfiles

--- conversion_service/converters/boundaries.py
+++ conversion_service/converters/boundaries.py
@@ -16,9 +16,9 @@ class BBox:
     def __init__(self, west, south, east, north):
         self.west, self.south, self.east, self.north = west, south, east, north
 
-    def cut_pbf(self, output_filename):  # pragma: nocover
+    def _get_cut_command(self, output_filename):
         pbf_file_path = OSMAXX_CONVERSION_SERVICE.get('PBF_WORLD_FILE_PATH')
-        command = "osmconvert --out-pbf -o={output_filename} -b={west},{south},{east},{north} {pbf_file_path}".format(
+        return "osmconvert --out-pbf -o={output_filename} -b={west},{south},{east},{north} {pbf_file_path}".format(
             output_filename=output_filename,
             pbf_file_path=pbf_file_path,
             west=self.west,
@@ -26,6 +26,9 @@ class BBox:
             east=self.east,
             north=self.north,
         )
+
+    def cut_pbf(self, output_filename):
+        command = self._get_cut_command(output_filename=output_filename)
         subprocess.check_call(command.split())
         return output_filename
 
--- tests/converters/boundaries_test.py
+++ tests/converters/boundaries_test.py
@@ -1,6 +1,10 @@
+from unittest.mock import patch
+
+from django.conf import settings
 from django.test import TestCase
 
 from converters.boundaries import BBox
+from tests.osm_test_helpers import BOUNDING_BOX_TEST_OSM
 
 
 class TestBBox(TestCase):
@@ -20,3 +24,28 @@ class TestBBox(TestCase):
     def test_initializing_works_if_all_given_parameters_are_set(self):
         # shouldn't raise an error
         BBox(west=0, south=0, east=0, north=0)
+
+    @patch('converters.boundaries.BBox._get_cut_command', return_value='true')
+    def test_cut_pbf_calls_correctly(self, cut_command_mock):
+        # tests are using sample data from monaco
+        bbox = BBox(**BOUNDING_BOX_TEST_OSM)
+        output_filename = 'cutted_filename.pbf'
+        bbox.cut_pbf(output_filename)
+        cut_command_mock.assertCalledWith(
+            output_filename=output_filename
+        )
+
+    def test_get_cut_command_returns_expected_command(self):
+        output_filename = 'cutted_filename.pbf'
+        pbf_file_path = settings.OSMAXX_CONVERSION_SERVICE.get('PBF_WORLD_FILE_PATH')
+        expected = "osmconvert --out-pbf -o={output_filename} -b={west},{south},{east},{north} {pbf_file_path}".format(
+            output_filename=output_filename,
+            pbf_file_path=pbf_file_path,
+            west=BOUNDING_BOX_TEST_OSM['west'],
+            south=BOUNDING_BOX_TEST_OSM['south'],
+            east=BOUNDING_BOX_TEST_OSM['east'],
+            north=BOUNDING_BOX_TEST_OSM['north'],
+        )
+        bbox = BBox(**BOUNDING_BOX_TEST_OSM)
+        actual = bbox._get_cut_command(output_filename=output_filename)
+        self.assertEqual(expected, actual)
