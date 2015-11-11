06c4de9f07bdf77f8d53dd33eaf85cf6cd4d0e6c
Nicola Jordan <njordan.hsr@gmail.com>
2015-11-11 16:23:11 +0100

    add estimate size for extent

--- tests/conftest.py
+++ tests/conftest.py
@@ -95,6 +95,7 @@ def pytest_configure():
         },
         OSMAXX_CONVERSION_SERVICE={
             'PBF_WORLD_FILE_PATH': os.path.join(test_data_dir, 'osm', 'monaco-latest.osm.pbf'),
+            'ESTIMATION_CSV_SOURCE_FILE': os.path.join(test_data_dir, 'estimation', 'planet-150202-stats.csv'),
         },
 
     )
--- tests/osm_test_helpers.py
+++ tests/osm_test_helpers.py
@@ -4,10 +4,10 @@ import os
 from tests.conftest import test_data_dir
 
 BOUNDING_BOX_TEST_OSM = {
-    'west': 7.400,
-    'east': 43.717,
-    'south': 7.439,
-    'north': 43.746,
+    'west': 7.400,  # (min lon)
+    'south': 43.717,  # (min lat)
+    'east': 7.439,  # (max lon)
+    'north': 43.746,  # (max lat)
 }
 
 # Monaco Polyfile
