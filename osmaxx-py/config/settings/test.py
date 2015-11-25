433ab03103796206acbe362f161190a08a5d13e1
Nicola Jordan <njordan.hsr@gmail.com>
2015-11-25 15:09:44 +0100

    use the linked service in tests

--- osmaxx-py/config/settings/test.py
+++ osmaxx-py/config/settings/test.py
@@ -4,7 +4,7 @@ DEBUG = False
 
 OSMAXX_TEST_SETTINGS = {
     'download_file_name': '%(excerpt_name)s-%(content_type)s-%(id)s.%(file_extension)s',
-    'CONVERSION_SERVICE_URL': 'http://localhost:8901/api/',
+    'CONVERSION_SERVICE_URL': 'http://coversion-service:8901/api/',
     'CONVERSION_SERVICE_USERNAME': 'osmaxxi',
     'CONVERSION_SERVICE_PASSWORD': '12345678',
 }
