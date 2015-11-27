861fcedd7dde44d3dde839772287bef3c0746872
Nicola Jordan <njordan.hsr@gmail.com>
2015-11-27 19:01:15 +0100

    use sqlite3 for testing database

--- osmaxx-py/config/settings/test.py
+++ osmaxx-py/config/settings/test.py
@@ -4,7 +4,7 @@ DEBUG = False
 
 OSMAXX_TEST_SETTINGS = {
     'download_file_name': '%(excerpt_name)s-%(content_type)s-%(id)s.%(file_extension)s',
-    'CONVERSION_SERVICE_URL': 'http://conversionservice:8901/api/',
+    'CONVERSION_SERVICE_URL': env.str('DJANGO_OSMAXX_CONVERSION_SERVICE_URL', default='http://localhost:8901/api/'),
     'CONVERSION_SERVICE_USERNAME': 'dev',
     'CONVERSION_SERVICE_PASSWORD': 'dev',
 }
@@ -28,4 +28,11 @@ LOGGING = {
             'level': 'ERROR',
         },
     },
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3'),
+    }
 }
