1c688de63bdec66de90caead5ce139f71bcbdb60
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-12-01 17:10:26 +0100

    update extraction order state from self-built status url

--- osmaxx-py/osmaxx/job_progress/tests.py
+++ osmaxx-py/osmaxx/job_progress/tests.py
@@ -1,5 +1,64 @@
-from django.test.testcases import TestCase
+import json
+from unittest.mock import patch
+
+import requests_mock
+from django.contrib.auth.models import User
+from osmaxx.excerptexport.models.bounding_geometry import BBoxBoundingGeometry
+from osmaxx.excerptexport.models.excerpt import Excerpt
+from osmaxx.excerptexport.models.extraction_order import ExtractionOrder, ExtractionOrderState
+from osmaxx.excerptexport.services.conversion_api_client import ConversionApiClient
+from osmaxx.job_progress import views
+from rest_framework.test import APITestCase
 
 
-class CallbackHandlingTest(TestCase):
-    pass
+class CallbackHandlingTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('user', 'user@example.com', 'pw')
+        self.bounding_box = BBoxBoundingGeometry.create_from_bounding_box_coordinates(
+            40.77739734768811, 29.528980851173397, 40.77546776498174, 29.525547623634335
+        )
+        self.excerpt = Excerpt.objects.create(
+            name='Neverland', is_active=True, is_public=True, owner=self.user, bounding_geometry=self.bounding_box
+        )
+        self.extraction_order = ExtractionOrder.objects.create(
+            excerpt=self.excerpt,
+            orderer=self.user,
+            process_id='53880847-faa9-43eb-ae84-dd92f3803a28',
+        )
+        self.extraction_order.extraction_configuration = {
+            'gis_formats': ['fgdb', 'spatialite'],
+            'gis_options': {
+                'coordinate_reference_system': 'WGS_84',
+                'detail_level': 1
+            }
+        }
+
+    @patch('osmaxx.excerptexport.services.conversion_api_client.ConversionApiClient.login', return_value=True)
+    @patch.object(ConversionApiClient, 'authorized_get', ConversionApiClient.get)
+    @requests_mock.Mocker(kw='requests')
+    def test_calling_tracker_when_status_query_indicates_started_updates_extraction_order_state(self, *args, **mocks):
+        requests_mock = mocks['requests']
+        requests_mock.get(
+            'http://localhost:8901/api/conversion_result/53880847-faa9-43eb-ae84-dd92f3803a28/',
+            text=json.dumps({
+                "rq_job_id": "53880847-faa9-43eb-ae84-dd92f3803a28",
+                "status": "started",
+                "progress": "started",
+                "gis_formats": [
+                    {
+                        "format": "fgdb",
+                        "progress": "started",
+                        "result_url": None
+                    },
+                    {
+                        "format": "spatialite",
+                        "progress": "queued",
+                        "result_url": None
+                    }
+                ]
+            })
+        )
+
+        views.tracker(request=None, order_id=self.extraction_order.id)
+        self.extraction_order.refresh_from_db()
+        self.assertEqual(self.extraction_order.state, ExtractionOrderState.PROCESSING)
--- osmaxx-py/osmaxx/job_progress/views.py
+++ osmaxx-py/osmaxx/job_progress/views.py
@@ -1,2 +1,8 @@
-def tracker(request):
-    pass
+from osmaxx.excerptexport.models import extraction_order
+from osmaxx.excerptexport.services import conversion_api_client
+
+
+def tracker(request, order_id):
+    order = extraction_order.ExtractionOrder.objects.get(pk=order_id)  # TODO: consider 404ing if not found
+    client = conversion_api_client.ConversionApiClient()
+    client.update_order_status(order)
