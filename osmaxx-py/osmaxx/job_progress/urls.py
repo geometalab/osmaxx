d4db26aa886f050aa3ed7fea8dc332aa342f118a
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-12-02 17:25:14 +0100

    end callback (job status notification) URL with slash

--- osmaxx-py/osmaxx/job_progress/urls.py
+++ osmaxx-py/osmaxx/job_progress/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from osmaxx.job_progress import views
 
 urlpatterns = [
-    url(r'^tracker/(?P<order_id>[0-9]+)$', views.tracker, name='tracker'),
+    url(r'^tracker/(?P<order_id>[0-9]+)/$', views.tracker, name='tracker'),
 ]
