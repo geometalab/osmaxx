494069e2451246159899dad03f3d7b1beb89819d
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-12-12 15:59:59 +0100

    test REST API view estimated_file_size

---
+++ osmaxx-py/osmaxx/excerptexport/tests/rest_api/test_views.py
@@ -0,0 +1,30 @@
+import json
+from unittest.mock import patch
+
+from django.test import TestCase
+from osmaxx.excerptexport.rest_api import views as api_views
+from rest_framework.test import APIRequestFactory
+
+
+class EstimatedFileSizeViewTests(TestCase):
+    def setUp(self):
+        p = patch('osmaxx.excerptexport.services.conversion_api_client.ConversionApiClient')
+        self.addCleanup(p.stop)
+        api_client_class_mock = p.start()
+
+        api_client_instance_mock = api_client_class_mock.return_value
+        self.estimated_file_size_api_client_mock = api_client_instance_mock.estimated_file_size
+        self.estimated_file_size_api_client_mock.return_value = 'estimated_size_value'
+
+        self.boundaries = {bound: bound + '_value' for bound in ['west', 'south', 'east', 'north']}
+
+        factory = APIRequestFactory()
+        self.request = factory.get('/estimated_file_size/', self.boundaries)
+
+    def test_estimated_file_size_view_calls_api_client_with_boundaries(self):
+        api_views.estimated_file_size(self.request)
+        self.estimated_file_size_api_client_mock.assert_called_with(**self.boundaries)
+
+    def test_estimated_file_size_view_returns_response_with_size_returned_by_api_client(self):
+        response = api_views.estimated_file_size(self.request)
+        self.assertContains(response, json.dumps(self.estimated_file_size_api_client_mock.return_value))
