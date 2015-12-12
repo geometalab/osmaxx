9c3bf4ca39c5c9663f6fc3de2b5935fdb6c8ee78
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-12-12 16:12:48 +0100

    rename variable

--- osmaxx-py/osmaxx/excerptexport/tests/rest_api/test_views.py
+++ osmaxx-py/osmaxx/excerptexport/tests/rest_api/test_views.py
@@ -16,14 +16,14 @@ class EstimatedFileSizeViewTests(TestCase):
         self.estimated_file_size_api_client_mock = api_client_instance_mock.estimated_file_size
         self.estimated_file_size_api_client_mock.return_value = 'estimated_size_value'
 
-        self.boundaries = {bound: bound + '_value' for bound in ['west', 'south', 'east', 'north']}
+        self.bbox_edges = {bound: bound + '_value' for bound in ['west', 'south', 'east', 'north']}
 
         factory = APIRequestFactory()
-        self.request = factory.get('/estimated_file_size/', self.boundaries)
+        self.request = factory.get('/estimated_file_size/', self.bbox_edges)
 
     def test_estimated_file_size_view_calls_api_client_with_boundaries(self):
         api_views.estimated_file_size(self.request)
-        self.estimated_file_size_api_client_mock.assert_called_with(**self.boundaries)
+        self.estimated_file_size_api_client_mock.assert_called_with(**self.bbox_edges)
 
     def test_estimated_file_size_view_returns_response_with_size_returned_by_api_client(self):
         response = api_views.estimated_file_size(self.request)
