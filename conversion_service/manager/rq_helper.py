dd4047189d0c61fd2d36633f0c64a437b67356b0
Nicola Jordan <njordan@hsr.ch>
2015-12-22 06:49:59 +0100

    worker should be allowed to work for 10 hours and the job not timeout for a day

--- conversion_service/manager/rq_helper.py
+++ conversion_service/manager/rq_helper.py
@@ -1,12 +1,16 @@
+from datetime import timedelta
+
 import django_rq
 
 from converters import converter_settings
 
 
 def rq_enqueue_with_settings(function, *args, **kwargs):
-        return django_rq.enqueue(
-            function,
-            result_ttl=converter_settings.OSMAXX_CONVERSION_SERVICE['RESULT_TTL'],
-            *args,
-            **kwargs
-        )
+    return django_rq.enqueue(
+        function,
+        result_ttl=converter_settings.OSMAXX_CONVERSION_SERVICE['RESULT_TTL'],
+        ttl=timedelta(days=1).seconds,  # max time in the queue, before it is considered lost
+        timeout=timedelta(hours=10).seconds,  # max time the worker may spend, before the job is cancelled
+        *args,
+        **kwargs
+    )
