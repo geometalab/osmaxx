fcb5ce25e37e4524a392427bbac3af9cf771966d
Wasabideveloper <tobias.blaser@hsr.ch>
2015-12-23 11:25:37 +0100

    Replace leaflet location filter by colorized square

--- web_frontend/osmaxx/excerptexport/static/excerptexport/scripts/maps/existing_excerpt_map.js
+++ web_frontend/osmaxx/excerptexport/static/excerptexport/scripts/maps/existing_excerpt_map.js
@@ -4,7 +4,7 @@
     var ExcerptViewer = function(mapElementID, selectElementExistingExcerpts, excerptApiUrl) {
         this.excerptApiUrl = excerptApiUrl;
         this.selectElementExistingExcerpts = selectElementExistingExcerpts;
-        this.currentCountryLayer = null;
+        this.currentLayer = null;
 
         this.map = L.map(mapElementID).setView([0, 0], 2);
         // add an OpenStreetMap tile layer
@@ -12,18 +12,12 @@
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this.map);
 
-        this.locationFilter = new L.LocationFilter({
-            enable: false,
-            enableButton: false
-        }).addTo(this.map);
-
-
         // update excerpt on map on selection of existing excerpt in list
         var getExcerptOptionAndUpdateFilter = function() {
             // we have single select, if it would happen to have two selected, ignore it.
             var excerptOption = this.selectElementExistingExcerpts.querySelector('option:checked');
             if(excerptOption) {
-                this._setLocationFilterFromExcerptID(excerptOption.value);
+                this._showExcerptOrCountryOnMap(excerptOption.value);
             }
         }.bind(this);
         this.selectElementExistingExcerpts.addEventListener('click', getExcerptOptionAndUpdateFilter);
@@ -34,39 +28,46 @@
             }
         });
 
-        this._handleCountry = function (country){
-            if (this.currentCountryLayer !== null) {
-                this.map.removeLayer(this.currentCountryLayer);
+        this._handleCountryOrBBox = function (layer){
+            if (this.currentLayer !== null) {
+                this.map.removeLayer(this.currentLayer);
             }
 
-            this.locationFilter.disable();
-            this.currentCountryLayer = country;
-            this.map.addLayer(country);
-            this.map.fitBounds(country.getBounds());
+            this.currentLayer = layer;
+            this.map.addLayer(layer);
         }.bind(this);
 
-        this._handleBBoxBoundingGeometry = function (geometry){
-            if (this.currentCountryLayer !== null) {
-                this.map.removeLayer(this.currentCountryLayer);
-            }
+        this._extendBounds = function(bounds, marginFactor) {
+            var latDiff = bounds._northEast.lat - bounds._southWest.lat;
+            var lonDiff = bounds._northEast.lon - bounds._southWest.lon;
+            bounds._northEast.lat += latDiff*marginFactor;
+            bounds._southWest.lat -= latDiff*marginFactor;
+            bounds._northEast.lon += lonDiff*marginFactor;
+            bounds._southWest.lon -= lonDiff*marginFactor;
+            return bounds;
+        };
 
-            // If location filter is enabled, it will collide with fitBounds and you will get some strange behaviour
-            this.locationFilter.disable();
-            this.locationFilter.setBounds(geometry.getBounds());
-            this.map.fitBounds(geometry.getBounds());
-            this.locationFilter.enable();
-        }.bind(this);
-
-        this._setLocationFilterFromExcerptID = function(ID) {
+        this._showExcerptOrCountryOnMap = function(ID) {
             L.geoJson.ajax(this.excerptApiUrl.replace('{ID}', ID)).on('data:loaded', function(event) {
                 // We are certain that there is only one layer on this feature, because our API provides it so.
                 var feature_type = event.target.getLayers()[0].feature.properties.type_of_geometry;
+                var layer = event.target;
+                this._handleCountryOrBBox(layer);
+                console.log(layer);
                 switch(feature_type) {
                     case 'BBoxBoundingGeometry':
-                        this._handleBBoxBoundingGeometry(event.target);
+                        layer.setStyle({
+                            color: 'black',
+                            fillOpacity: 0.15
+                        });
+                        this.map.fitBounds(this._extendBounds(layer.getBounds(), 0.2));
                         break;
                     case 'Country':
-                        this._handleCountry(event.target);
+                        layer.setStyle({
+                            color: 'navy',
+                            fillOpacity: 0.1
+                        });
+                        this.map.fitBounds(layer.getBounds());
                         break;
                     default:
                         break;
