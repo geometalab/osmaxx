e101fab3466d92671eb641ee8a4f9fa60c64c91e
Nicola Jordan <njordan.hsr@gmail.com>
2016-01-12 09:52:51 +0100

    use a separate instance for each event

--- web_frontend/osmaxx/excerptexport/static/excerptexport/scripts/maps/new_excerpt_map.js
+++ web_frontend/osmaxx/excerptexport/static/excerptexport/scripts/maps/new_excerpt_map.js
@@ -10,14 +10,13 @@
         this.updateInputElementsBoundingBox = function() {
             var locationFilterBounds = this.locationFilter.getBounds();
             this.inputElementsNewBoundingBox.inputElementNorth.value = locationFilterBounds._northEast.lat;
-            var ev = new CustomEvent("valueUpdate");
-            this.inputElementsNewBoundingBox.inputElementNorth.dispatchEvent(ev);
+            this.inputElementsNewBoundingBox.inputElementNorth.dispatchEvent(new CustomEvent("valueUpdate"));
             this.inputElementsNewBoundingBox.inputElementWest.value = locationFilterBounds._southWest.lng;
-            this.inputElementsNewBoundingBox.inputElementWest.dispatchEvent(ev);
+            this.inputElementsNewBoundingBox.inputElementWest.dispatchEvent(new CustomEvent("valueUpdate"));
             this.inputElementsNewBoundingBox.inputElementEast.value = locationFilterBounds._northEast.lng;
-            this.inputElementsNewBoundingBox.inputElementEast.dispatchEvent(ev);
+            this.inputElementsNewBoundingBox.inputElementEast.dispatchEvent(new CustomEvent("valueUpdate"));
             this.inputElementsNewBoundingBox.inputElementSouth.value = locationFilterBounds._southWest.lat;
-            this.inputElementsNewBoundingBox.inputElementSouth.dispatchEvent(ev);
+            this.inputElementsNewBoundingBox.inputElementSouth.dispatchEvent(new CustomEvent("valueUpdate"));
         };
         this.areAllBoxesSet = function(){
             return this.inputElementsNewBoundingBox.inputElementNorth.value &&
