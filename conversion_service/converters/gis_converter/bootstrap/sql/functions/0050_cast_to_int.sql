d5005da4a6a65abd29448c7a9fafe26a4ff32751
Nicola Jordan <njordan@hsr.ch>
2016-01-22 12:12:23 +0100

    correct function arguments

--- conversion_service/converters/gis_converter/bootstrap/sql/functions/0050_cast_to_int.sql
+++ conversion_service/converters/gis_converter/bootstrap/sql/functions/0050_cast_to_int.sql
@@ -1,9 +1,10 @@
 -- try casting to int and return NULL if failed
-create or replace function cast_to_int_null_if_failed(text, integer) returns integer as $$
+create or replace function cast_to_int_null_if_failed(v_input text) returns integer as $$
 begin
-    return cast($1 as int);
+    return cast(v_input as int);
 exception
     when invalid_text_representation then
+        RAISE NOTICE 'Invalid integer value: "%".  Returning NULL.', v_input;
         return NULL;
 end;
 $$ language plpgsql immutable;
