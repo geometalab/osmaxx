3e4b34be64114a125a826675c5d8ddd51e5b65e3
Nicola Jordan <njordan@hsr.ch>
2016-01-22 10:29:10 +0100

    splitted geoname sql into single statements

---
+++ conversion_service/converters/gis_converter/bootstrap/sql/filter/geoname/020_geoname_p.sql
@@ -0,0 +1,52 @@
+INSERT INTO osmaxx.geoname_p
+  SELECT osm_id as osm_id,
+	osm_timestamp as lastchange, 
+	'N' AS geomtype, -- Node
+	way AS geom,  
+-- Checks the data and fills value in case of NULL -- 
+	case 
+	 when place in ('city','town','village' ,'hamlet','suburb','island','farm','isolated_dwelling','locality', 'islet', 'neighbourhood','county','region','state','municipality') then place
+	 when area='yes' then 'named_place' 
+	 else 'place'
+	 end as type, 
+
+	name as name,
+	"name:en" as name_en, 
+	"name:fr" as name_fr, 
+	"name:es" as name_es, 
+	"name:de" as name_de, 
+	int_name as name_int, 
+	transliterate(name) as label,
+	cast(tags as text) as tags,
+	cast(nullif(population,'') as int) as population,
+	wikipedia as wikipedia
+  FROM osm_point
+  WHERE place is not null or (area='yes' and name is not null)
+UNION 
+  SELECT osm_id as osm_id,
+	osm_timestamp as lastchange, 
+	CASE 
+	 WHEN osm_id<0 THEN 'R' -- Relation
+	 ELSE 'W' 		-- Way
+	 END AS geomtype, 
+	ST_Centroid(way) AS geom,
+
+-- Checks the data and fills value in case of NULL --   
+	case 
+	 when place in ('city','town','village' ,'hamlet','suburb','island','farm','isolated_dwelling','locality', 'islet', 'neighbourhood','county','region','state','municipality') then place
+	 when area='yes' then 'named_place' 
+	 else 'place'
+	 end as type, 
+
+	name as name,
+	"name:en" as name_en, 
+	"name:fr" as name_fr, 
+	"name:es" as name_es, 
+	"name:de" as name_de, 
+	int_name as name_int, 
+	transliterate(name) as label,
+	cast(tags as text) as tags,
+	cast(nullif(population,'') as int) as population,
+	wikipedia as wikipedia
+  FROM osm_polygon
+  WHERE place is not null or (area='yes' and name is not null);
