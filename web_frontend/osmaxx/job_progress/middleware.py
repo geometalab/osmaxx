c5c8fa3582fbcc7fe17928c81ab80750fac39646
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-01-27 10:04:20 +0100

    refactoring: order code top-down

--- web_frontend/osmaxx/job_progress/middleware.py
+++ web_frontend/osmaxx/job_progress/middleware.py
@@ -3,14 +3,6 @@ from osmaxx.excerptexport.models.extraction_order import FINAL_STATES
 from osmaxx.excerptexport.services.shortcuts import get_authenticated_api_client
 
 
-def update_orders_of_request_user(request):
-    current_user = request.user
-    if current_user.is_anonymous():
-        return
-    for order in ExtractionOrder.objects.exclude(state__in=FINAL_STATES).filter(orderer=current_user):
-        update_order(order)
-
-
 class OrderUpdaterMiddleware(object):
     def process_request(self, request):
         assert hasattr(request, 'user'), (
@@ -20,6 +12,14 @@ class OrderUpdaterMiddleware(object):
             "'osmaxx.job_progress.middleware.OrderUpdaterMiddleware'."
         )
         update_orders_of_request_user(request)
+
+
+def update_orders_of_request_user(request):
+    current_user = request.user
+    if current_user.is_anonymous():
+        return
+    for order in ExtractionOrder.objects.exclude(state__in=FINAL_STATES).filter(orderer=current_user):
+        update_order(order)
 
 
 def update_order(extraction_order):
