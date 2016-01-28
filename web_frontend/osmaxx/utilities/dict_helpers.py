a685d87821a8bc45948e8cf3c10a71df6006389c
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-01-28 12:45:23 +0100

    make the keys argument of are_ll_keys_in keyword-only

--- web_frontend/osmaxx/utilities/dict_helpers.py
+++ web_frontend/osmaxx/utilities/dict_helpers.py
@@ -2,5 +2,5 @@ def select_keys(dictionary, key_list):
     return {key: dictionary[key] for key in key_list}
 
 
-def are_all_keys_in(dictionary, keys):
+def are_all_keys_in(dictionary, *, keys):
     return set(keys).issubset(dictionary)
