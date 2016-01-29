7ca08cbc9feaf631af9301e6c1d3f11c50c20c4f
Nicola Jordan <njordan.hsr@gmail.com>
2016-01-29 10:44:05 +0100

    polyfile creates valid polyfiles with holes

--- tests/clipping_polygon/to_polyfile_test.py
+++ tests/clipping_polygon/to_polyfile_test.py
@@ -14,17 +14,20 @@ class CreatePolyfileStringTest(TestCase):
         self.multi_polygon_1 = MultiPolygon(self.poly_1, self.poly_2)
         self.multi_polygon_2 = MultiPolygon([self.poly_1, self.poly_2])
 
+        self.holy_poly_3 = Polygon(((0, 0), (0, 1), (1, 1), (0, 0)), ((1, 1), (1, 2), (2, 2), (1, 1)))
+        self.multi_polygon_3 = MultiPolygon(self.holy_poly_3)
+
     def test_create_poly_file_string_when_valid_multipolygon_returns_correct_string(self):
         result = create_poly_file_string(self.multi_polygon_1)
         expected = os.linesep.join([
             'none',
-            '1',
+            '1-outer',
             '  0.000000E+00 0.000000E+00',
             '  0.000000E+00 1.000000E+00',
             '  1.000000E+00 1.000000E+00',
             '  0.000000E+00 0.000000E+00',
             'END',
-            '2',
+            '2-outer',
             '  1.000000E+00 1.000000E+00',
             '  1.000000E+00 2.000000E+00',
             '  2.000000E+00 2.000000E+00',
@@ -35,10 +38,10 @@ class CreatePolyfileStringTest(TestCase):
         ])
         self.assertMultiLineEqual(expected, result)
 
-    def test_create_poly_file_string_when_valid_polygon_returns_correct_string(self):
+    def test_create_poly_file_string_when_polygon_is_valid_returns_correct_string(self):
         expected = os.linesep.join([
             'none',
-            '1',
+            '1-outer',
             '  0.000000E+00 0.000000E+00',
             '  0.000000E+00 1.000000E+00',
             '  1.000000E+00 1.000000E+00',
@@ -50,6 +53,27 @@ class CreatePolyfileStringTest(TestCase):
         result = create_poly_file_string(self.poly_1)
         self.assertMultiLineEqual(expected, result)
 
+    def test_create_poly_file_string_when_valid_polygon_has_hole_returns_correct_string(self):
+        expected = os.linesep.join([
+            'none',
+            '1-outer',
+            '  0.000000E+00 0.000000E+00',
+            '  0.000000E+00 1.000000E+00',
+            '  1.000000E+00 1.000000E+00',
+            '  0.000000E+00 0.000000E+00',
+            'END',
+            '!1-inner-1',
+            '  1.000000E+00 1.000000E+00',
+            '  1.000000E+00 2.000000E+00',
+            '  2.000000E+00 2.000000E+00',
+            '  1.000000E+00 1.000000E+00',
+            'END',
+            'END',
+            '',
+        ])
+        result = create_poly_file_string(self.multi_polygon_3)
+        self.assertMultiLineEqual(expected, result)
+
     def test_create_poly_file_string_when_invalid_input_raises_type_error(self):
         no_multi_polygon_at_all = 'asda'
         self.assertRaises(TypeError, create_poly_file_string, no_multi_polygon_at_all)
@@ -59,8 +83,17 @@ class CreatePolyfileStringTest(TestCase):
         self.assertRaises(TypeError, create_poly_file_string, invalid_geometry)
 
     def test_create_poly_file_string_equals_the_multipolygon_it_was_constructed_from(self):
+        ext_coords = ((0, 0), (0, 1), (1, 1), (1, 0), (0, 0))
+        int_coords = ((0.4, 0.4), (0.4, 0.6), (0.6, 0.6), (0.6, 0.4), (0.4, 0.4))
+        complex_poly_3 = Polygon(ext_coords, int_coords)
+        complex_multi_polygon = MultiPolygon(complex_poly_3, self.poly_1, self.poly_2)
+
         create_poly_file_string(self.multi_polygon_1)
         self.assertEqual(
             self.multi_polygon_1,
             utils.parse_poly_string(create_poly_file_string(self.multi_polygon_1))
+        )
+        self.assertEqual(
+            complex_multi_polygon,
+            utils.parse_poly_string(create_poly_file_string(complex_multi_polygon))
         )
