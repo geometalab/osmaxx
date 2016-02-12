89b8bc8193b66077d9d6233504923e20109a2ffb
Nicola Jordan <njordan@hsr.ch>
2016-02-12 09:47:51 +0100

    rename fixture to singular

--- tests/conversion/view_test.py
+++ tests/conversion/view_test.py
@@ -9,21 +9,21 @@ authenticated_access_urls = [
 
 
 @pytest.fixture(params=authenticated_access_urls)
-def access_urls(request):
+def access_url(request):
     return request.param
 
 
-def test_access_for_unauthorized_user_denied(client, access_urls):
-    response = client.get(access_urls)
+def test_access_for_unauthorized_user_denied(client, access_url):
+    response = client.get(access_url)
     assert response.status_code == 403
 
 
 @pytest.mark.django_db()
-def test_access_for_authenticated_client_allowed(authenticated_client, access_urls):
-    response = authenticated_client.get(access_urls)
+def test_access_for_authenticated_client_allowed(authenticated_client, access_url):
+    response = authenticated_client.get(access_url)
     assert response.status_code == 200
 
 
-def test_access_for_admin_user_allowed(admin_client, access_urls):
-    response = admin_client.get(access_urls)
+def test_access_for_admin_user_allowed(admin_client, access_url):
+    response = admin_client.get(access_url)
     assert response.status_code == 200
