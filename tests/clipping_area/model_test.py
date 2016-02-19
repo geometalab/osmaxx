83810eb10bcfd8334ac45fbfe3d40c9acb4c3c91
Nicola Jordan <njordan.hsr@gmail.com>
2016-02-19 15:14:32 +0100

    refactor: assert where they belong

--- tests/clipping_area/model_test.py
+++ tests/clipping_area/model_test.py
@@ -3,11 +3,6 @@ import pytest
 
 @pytest.mark.django_db()
 def test_osmosis_polygon_file_string_property_returns_osmosis_polygon_file(valid_clipping_area):
-    assert valid_clipping_area.osmosis_polygon_file_string is not None
-    assert valid_clipping_area.osmosis_polygon_file_string != ''
-
-
-@pytest.mark.django_db()
-def test_representation(valid_clipping_area):
-    assert str(valid_clipping_area) is not None
-    assert str(valid_clipping_area) == "test (1)"
+    # this actually assures we are testing the asserts in valid_clipping_area, if
+    # at some point that fixture should be unused at other places
+    pass
