b8e363903d5df3d2025158798f4fe397e5d4db1c
Nicola Jordan <njordan.hsr@gmail.com>
2016-02-29 11:13:56 +0100

    clarify comment

--- web_frontend/osmaxx/countries/_settings.py
+++ web_frontend/osmaxx/countries/_settings.py
@@ -4,6 +4,6 @@ from django.conf import settings
 
 POLYFILE_LOCATION = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'polyfiles')
 
-# only for testing, not documented
+# only needed for testing
 if hasattr(settings, '_OSMAXX_POLYFILE_LOCATION'):
     POLYFILE_LOCATION = settings._OSMAXX_POLYFILE_LOCATION
