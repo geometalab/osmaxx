cab99a85779ce78024dac2272f11f37cda28605d
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-03-01 18:13:36 +0100

    misc_l barrier=*: For each MULTIPOLYGON, use ST_ExteriorRing()s of corresponding individual POLYGONs #97
    and ST_Collect() them to form a MULTILINESTRING

--- osmaxx/converters/gis_converter/bootstrap/sql/filter/misc/010_barrier.sql
+++ osmaxx/converters/gis_converter/bootstrap/sql/filter/misc/010_barrier.sql
@@ -26,13 +26,27 @@ INSERT INTO osmaxx.misc_l
   WHERE barrier is not null
 
 UNION
+
+  WITH osm_single_polygon AS (
+      SELECT osm_id, osm_timestamp, barrier, name, "name:en", "name:fr", "name:es", int_name, tags,
+          ST_Dump(way).geom AS way
+      FROM osm_polygon
+      WHERE ST_Geometrytype(way) = 'ST_MultiPolygon'
+
+    UNION
+
+      SELECT osm_id, osm_timestamp, barrier, name, "name:en", "name:fr", "name:es", int_name, tags,
+          way
+      FROM osm_polygon
+      WHERE ST_Geometrytype(way) != 'ST_MultiPolygon'
+  )
   SELECT osm_id as osm_id,
 	osm_timestamp as lastchange,
 	CASE
 	 WHEN osm_id<0 THEN 'R' -- Relation
 	 ELSE 'W' 		-- Way
 	 END AS geomtype,
-	ST_Multi(ST_ExteriorRing (way)) AS geom,
+	ST_Multi(ST_Collect(ST_ExteriorRing (way))) AS geom,
 	'barrier' as aggtype,
 -- Combining different tags into barrier tag --
 	case
@@ -48,5 +62,6 @@ UNION
 	int_name as name_int,
 	transliterate(name) as label,
 	cast(tags as text) as tags
-  FROM osm_polygon
-  WHERE  barrier is not null;
+  FROM osm_single_polygon
+  WHERE  barrier is not null
+  GROUP BY osm_id;
