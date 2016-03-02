b3986e24bf3316a517925d93e546a068b24f2ca2
Nicola Jordan <njordan.hsr@gmail.com>
2016-03-02 13:16:34 +0100

    support multipolygon as input type for road

--- osmaxx/converters/gis_converter/bootstrap/sql/filter/road/010_road.sql
+++ osmaxx/converters/gis_converter/bootstrap/sql/filter/road/010_road.sql
@@ -55,10 +55,20 @@ INSERT INTO osmaxx.road_l
  	FROM osm_line
  	WHERE highway not in ('abandon','construction','planned','disused') or junction not in ('roundabout')
 UNION
+(
+  WITH osm_single_polygon AS (
+      SELECT osm_id, osm_timestamp, highway, junction, tunnel, tracktype, bridge, oneway, ref, z_order, name, "name:en", "name:fr", "name:es", "name:de", int_name, tags,
+
+      CASE WHEN ST_Geometrytype(way) = 'ST_MultiPolygon'
+        THEN (ST_Dump(way)).geom
+        ELSE way
+      END AS way
+      FROM osm_polygon
+  )
   SELECT osm_id as osm_id,
 	osm_timestamp as lastchange,
 	'W' AS geomtype,
-	ST_Multi(ST_ExteriorRing (way)) AS geom,
+	ST_Multi(ST_Collect(ST_ExteriorRing (way))) AS geom,
 -- Creating tags for groups of roads --
 	case
 	 when highway in ('motorway','trunk','primary','secondary','tertiary') then 'major_road'
@@ -110,5 +120,7 @@ UNION
 	else FALSE
 	end as tunnel
 
- 	FROM osm_polygon
- 	WHERE highway not in ('abandon','construction','planned','disused') or junction not in ('roundabout');
+ 	FROM osm_single_polygon
+ 	WHERE highway not in ('abandon','construction','planned','disused') or junction not in ('roundabout')
+ 	GROUP BY osm_id, osm_timestamp, highway, junction, tunnel, tracktype, bridge, oneway, ref, z_order, name, "name:en", "name:fr", "name:es", "name:de", int_name, tags
+);
