80b0bc819e76de83b8ee94dd4bb16c8ead1339f4
Nicola Jordan <njordan.hsr@gmail.com>
2016-03-02 12:24:38 +0100

    support multipolygon as input type for traffic calming

--- osmaxx/converters/gis_converter/bootstrap/sql/filter/misc/030_traffic_calming.sql
+++ osmaxx/converters/gis_converter/bootstrap/sql/filter/misc/030_traffic_calming.sql
@@ -24,13 +24,23 @@ INSERT INTO osmaxx.misc_l
   FROM osm_line
   WHERE traffic_calming is not null
 UNION
+(
+  WITH osm_single_polygon AS (
+      SELECT osm_id, osm_timestamp, traffic_calming, name, "name:en", "name:fr", "name:es", "name:de", int_name, tags,
+
+      CASE WHEN ST_Geometrytype(way) = 'ST_MultiPolygon'
+        THEN (ST_Dump(way)).geom
+        ELSE way
+      END AS way
+      FROM osm_polygon
+  )
   SELECT osm_id as osm_id,
 	osm_timestamp as lastchange,
 	CASE
 	 WHEN osm_id<0 THEN 'R' -- R=Relation
 	 ELSE 'W' 		-- W=Way
 	 END AS geomtype,
-	ST_Multi(ST_ExteriorRing (way)) AS geom,
+	ST_Multi(ST_Collect(ST_ExteriorRing (way))) AS geom,
 	'traffic_calming' as aggtype,
 -- Combining different tags into traffic_calming tag --
 	case
@@ -46,5 +56,7 @@ UNION
 	int_name as name_int,
 	transliterate(name) as label,
 	cast(tags as text) as tags
-  FROM osm_polygon
-  WHERE traffic_calming is not null;
+  FROM osm_single_polygon
+  WHERE traffic_calming is not null
+  GROUP BY osm_id, osm_timestamp, traffic_calming, name, "name:en", "name:fr", "name:es", "name:de", int_name, tags
+);
