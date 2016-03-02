741bd9f8b1a268e14ef4c5aa9cf3c68f8e181ab3
Nicola Jordan <njordan.hsr@gmail.com>
2016-03-02 13:21:18 +0100

    support multipolygon as input type for junction

--- osmaxx/converters/gis_converter/bootstrap/sql/filter/road/020_junction.sql
+++ osmaxx/converters/gis_converter/bootstrap/sql/filter/road/020_junction.sql
@@ -48,10 +48,20 @@ INSERT INTO osmaxx.road_l
  	FROM osm_line
  	WHERE junction='roundabout'
 UNION
+(
+  WITH osm_single_polygon AS (
+      SELECT osm_id, osm_timestamp, junction, highway, bridge, tunnel, tracktype, ref, oneway, z_order, name, "name:en", "name:fr", "name:es", "name:de", int_name, tags,
+
+      CASE WHEN ST_Geometrytype(way) = 'ST_MultiPolygon'
+        THEN (ST_Dump(way)).geom
+        ELSE way
+      END AS way
+      FROM osm_polygon
+  )
   SELECT osm_id as osm_id,
 	osm_timestamp as lastchange,
 	'W' AS geomtype, 	-- Way
-	ST_Multi(ST_ExteriorRing (way)) AS geom,
+	ST_Multi(ST_Collect(ST_ExteriorRing (way))) AS geom,
 	'roundabout'as aggtype,
 -- Creating tags for groups of roads --
 	case
@@ -92,5 +102,7 @@ UNION
 	when tunnel in ('passage', 'culvert', 'noiseprotection galerie', 'gallery', 'building_passage', 'avalanche_protector','teilweise', 'viaduct', 'tunnel', 'yes') then TRUE
 	else FALSE
 	end as tunnel
- 	FROM osm_polygon
- 	WHERE junction='roundabout';
+ 	FROM osm_single_polygon
+ 	WHERE junction='roundabout'
+ 	GROUP BY osm_id, osm_timestamp, junction, highway, bridge, tunnel, tracktype, ref, oneway, z_order, name, "name:en", "name:fr", "name:es", "name:de", int_name, tags
+);
