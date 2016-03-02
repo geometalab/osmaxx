486bd86f10cfe744be0774839711f05785b48576
Nicola Jordan <njordan.hsr@gmail.com>
2016-03-02 11:31:15 +0100

    support multipolygon as input type for natural

--- osmaxx/converters/gis_converter/bootstrap/sql/filter/misc/020_natural.sql
+++ osmaxx/converters/gis_converter/bootstrap/sql/filter/misc/020_natural.sql
@@ -21,13 +21,23 @@ INSERT INTO osmaxx.misc_l
   FROM osm_line
   WHERE "natural"='cliff'
 UNION
+(
+  WITH osm_single_polygon AS (
+      SELECT osm_id, osm_timestamp, "natural", name, "name:en", "name:fr", "name:es", "name:de", int_name, tags,
+
+      CASE WHEN ST_Geometrytype(way) = 'ST_MultiPolygon'
+        THEN (ST_Dump(way)).geom
+        ELSE way
+      END AS way
+      FROM osm_polygon
+  )
   SELECT osm_id as osm_id,
 	osm_timestamp as lastchange,
 	CASE
 	 WHEN osm_id<0 THEN 'R' -- R=Relation
 	 ELSE 'W' 		-- W=Way
 	 END AS geomtype,
-	ST_Multi(ST_ExteriorRing (way)) AS geom,
+	ST_Multi(ST_Collect(ST_ExteriorRing (way))) AS geom,
 	'natural' as aggtype,
 	"natural" AS type,
 	name as name,
@@ -38,5 +48,7 @@ UNION
 	int_name as name_int,
 	transliterate(name) as label,
 	cast(tags as text) as tags
-  FROM osm_polygon
-  WHERE  "natural"='cliff';
+  FROM osm_single_polygon
+  WHERE  "natural"='cliff'
+  GROUP BY osm_id, osm_timestamp, name, "natural", "name:en", "name:fr", "name:es", "name:de", int_name, tags
+);
