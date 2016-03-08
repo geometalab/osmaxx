4411c7d104865c0469657f566230bc618f632097
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-03-08 12:14:37 +0100

    modify comment: Whether or not the result has been fetched doesn't matter

--- tests/inside_worker_test/burial_ground_sql_test.py
+++ tests/inside_worker_test/burial_ground_sql_test.py
@@ -31,7 +31,7 @@ def test_osmaxx_data_model_processing_puts_amenity_grave_yard_with_religion_into
         assert result.rowcount == 1
     finally:
         try:
-            del result  # The (unfetched) result would block the dropping of SCHEMA "osmaxx" in the following cleanup.
+            del result  # The result would block the dropping of SCHEMA "osmaxx" in the following cleanup.
         except NameError:
             pass
         cleanup_osmaxx_schemas(engine)
