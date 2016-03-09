cbe209e0cb261a313b0ec87c94731f87ad44762d
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-03-09 14:47:39 +0100

    refactoring: extract local variable for data to be inserted

--- tests/inside_worker_test/burial_ground_sql_test.py
+++ tests/inside_worker_test/burial_ground_sql_test.py
@@ -10,7 +10,8 @@ from tests.inside_worker_test.declarative_schema import osm_models
 
 @slow
 def test_osmaxx_data_model_processing_puts_amenity_grave_yard_with_religion_into_table_pow_a(data_import):
-    with data_import(dict(amenity='grave_yard', religion='any value will do, as long as one is present')) as engine:
+    data = dict(amenity='grave_yard', religion='any value will do, as long as one is present')
+    with data_import(data) as engine:
         t_pow_a = DbTable('pow_a', osm_models.metadata, schema='osmaxx')
         with closing(engine.execute(sqlalchemy.select([t_pow_a]))) as result:
             assert result.rowcount == 1
