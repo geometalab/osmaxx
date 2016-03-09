4f1d28f33541bd52e9ffc4d05f3369c674960703
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-03-09 14:49:12 +0100

    refactoring: formatting

--- tests/inside_worker_test/burial_ground_sql_test.py
+++ tests/inside_worker_test/burial_ground_sql_test.py
@@ -10,7 +10,10 @@ from tests.inside_worker_test.declarative_schema import osm_models
 
 @slow
 def test_osmaxx_data_model_processing_puts_amenity_grave_yard_with_religion_into_table_pow_a(data_import):
-    data = dict(amenity='grave_yard', religion='any value will do, as long as one is present')
+    data = dict(
+        amenity='grave_yard',
+        religion='any value will do, as long as one is present',
+    )
     with data_import(data) as engine:
         t_pow_a = DbTable('pow_a', osm_models.metadata, schema='osmaxx')
         with closing(engine.execute(sqlalchemy.select([t_pow_a]))) as result:
