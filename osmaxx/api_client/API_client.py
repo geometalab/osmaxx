c18780497c201fd93b85e7b440e4d00dd0e11cde
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-03-24 10:26:48 +0100

    refactoring: push up method login() to JWTClient

--- osmaxx/api_client/API_client.py
+++ osmaxx/api_client/API_client.py
@@ -62,6 +62,15 @@ class JWTClient(RESTApiClient):
         self.password = password
         self.token = None
 
+    def login(self, *args, **kwargs):
+        """
+        Logs in the api client by requesting an API token
+        """
+        if self.token:
+            # already logged in
+            return
+        self.auth(*args, **kwargs)
+
     def auth(self, username=None, password=None):
         if username is None:
             username = self.username
