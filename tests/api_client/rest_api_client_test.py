c9f8e1181f28e484c6fb6fae8f8b03b1aa0404cf
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-03-25 19:02:21 +0100

    add basic unit test for RESTApiJWTClient.post #343

--- tests/api_client/rest_api_client_test.py
+++ tests/api_client/rest_api_client_test.py
@@ -13,3 +13,20 @@ def test_get_performs_request(requests_mock):
     c = RESTApiJWTClient('http://example.com/service/uri_base/')
     response = c.get('get/example')
     assert response.json() == {'some response': 'you got it'}
+
+
+def test_post_performs_request(requests_mock):
+    def json_callback(request, context):
+        assert request.json() == {'pay': 'load'}
+        return {'some response': 'you posted it'}
+    requests_mock.post(
+        # Expected request:
+        'http://example.com/service/uri_base/post/example',
+        request_headers={'Content-Type': 'application/json; charset=UTF-8'},
+
+        # Response if request matched:
+        json=json_callback
+    )
+    c = RESTApiJWTClient('http://example.com/service/uri_base/')
+    response = c.post('post/example', json_data={'pay': 'load'})
+    assert response.json() == {'some response': 'you posted it'}
