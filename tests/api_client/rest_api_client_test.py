d062938741c3ec45765794451370e3f8466d5124
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-03-25 19:17:01 +0100

    more helpful message when requesting wrong URL #343
    show diff instead of causing requests_mock.exceptions.NoMockAddress

--- tests/api_client/rest_api_client_test.py
+++ tests/api_client/rest_api_client_test.py
@@ -1,10 +1,11 @@
 from osmaxx.api_client.API_client import RESTApiJWTClient
+from requests_mock import ANY
 
 
 def test_get_performs_request(requests_mock):
     requests_mock.get(
         # Expected request:
-        'http://example.com/service/uri_base/get/example',
+        ANY,
         request_headers={'Content-Type': 'application/json; charset=UTF-8'},
 
         # Response if request matched:
@@ -12,13 +13,14 @@ def test_get_performs_request(requests_mock):
     )
     c = RESTApiJWTClient('http://example.com/service/uri_base/')
     response = c.get('get/example')
+    assert response.request.url == 'http://example.com/service/uri_base/get/example'
     assert response.json() == {'some response': 'you got it'}
 
 
 def test_post_performs_request(requests_mock):
     requests_mock.post(
         # Expected request:
-        'http://example.com/service/uri_base/post/example',
+        ANY,
         request_headers={'Content-Type': 'application/json; charset=UTF-8'},
 
         # Response if request matched:
@@ -26,5 +28,6 @@ def test_post_performs_request(requests_mock):
     )
     c = RESTApiJWTClient('http://example.com/service/uri_base/')
     response = c.post('post/example', json_data={'pay': 'load'})
+    assert response.request.url == 'http://example.com/service/uri_base/post/example'
     assert response.request.json() == {'pay': 'load'}
     assert response.json() == {'some response': 'you posted it'}
