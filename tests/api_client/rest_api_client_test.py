2f2d9d31cb85cc35ba484dbef67301cdfa2184ec
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-03-25 17:29:37 +0100

    refactoring: spell out mock name

--- tests/api_client/rest_api_client_test.py
+++ tests/api_client/rest_api_client_test.py
@@ -4,8 +4,8 @@ from osmaxx.api_client.API_client import RESTApiJWTClient
 
 
 def test_get_performs_request():
-    with requests_mock_package.mock() as r_mock:
-        r_mock.get(
+    with requests_mock_package.mock() as requests_mock:
+        requests_mock.get(
             # Expected request:
             'http://example.com/service/uri_base/get/example',
             request_headers={'Content-Type': 'application/json; charset=UTF-8'},
