c006a39a62b3042c02b4597dd0f661450da5335f
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-03-25 18:37:46 +0100

    refactoring: extract request_mock context manager to yield fixture

--- tests/api_client/rest_api_client_test.py
+++ tests/api_client/rest_api_client_test.py
@@ -1,18 +1,24 @@
+import pytest
 import requests_mock as requests_mock_package
 
 from osmaxx.api_client.API_client import RESTApiJWTClient
 
 
-def test_get_performs_request():
-    with requests_mock_package.mock() as requests_mock:
-        requests_mock.get(
-            # Expected request:
-            'http://example.com/service/uri_base/get/example',
-            request_headers={'Content-Type': 'application/json; charset=UTF-8'},
+def test_get_performs_request(requests_mock):
+    requests_mock.get(
+        # Expected request:
+        'http://example.com/service/uri_base/get/example',
+        request_headers={'Content-Type': 'application/json; charset=UTF-8'},
 
-            # Response if request matched:
-            json={'some response': 'you got it'}
-        )
-        c = RESTApiJWTClient('http://example.com/service/uri_base/')
-        response = c.get('get/example')
-        assert response.json() == {'some response': 'you got it'}
+        # Response if request matched:
+        json={'some response': 'you got it'}
+    )
+    c = RESTApiJWTClient('http://example.com/service/uri_base/')
+    response = c.get('get/example')
+    assert response.json() == {'some response': 'you got it'}
+
+
+@pytest.yield_fixture
+def requests_mock():
+    with requests_mock_package.mock() as m:
+        yield m
