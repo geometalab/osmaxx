905fd71f997c6a543c598fe32af3e04fdd3ea5c5
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-03-25 23:34:46 +0100

    more helpful message when requesting wrong content type #343
    show diff instead of causing requests_mock.exceptions.NoMockAddress

--- tests/api_client/rest_api_client_test.py
+++ tests/api_client/rest_api_client_test.py
@@ -6,7 +6,6 @@ def test_get_performs_request(requests_mock):
     requests_mock.get(
         # Expected request:
         ANY,
-        request_headers={'Content-Type': 'application/json; charset=UTF-8'},
 
         # Response if request matched:
         json={'some response': 'you got it'}
@@ -14,6 +13,7 @@ def test_get_performs_request(requests_mock):
     c = RESTApiJWTClient('http://example.com/service/uri_base/')
     response = c.get('get/example')
     assert response.request.url == 'http://example.com/service/uri_base/get/example'
+    assert response.request.headers['Content-Type'] == 'application/json; charset=UTF-8'
     assert response.json() == {'some response': 'you got it'}
 
 
@@ -21,7 +21,6 @@ def test_post_performs_request(requests_mock):
     requests_mock.post(
         # Expected request:
         ANY,
-        request_headers={'Content-Type': 'application/json; charset=UTF-8'},
 
         # Response if request matched:
         json={'some response': 'you posted it'}
@@ -29,5 +28,6 @@ def test_post_performs_request(requests_mock):
     c = RESTApiJWTClient('http://example.com/service/uri_base/')
     response = c.post('post/example', json_data={'pay': 'load'})
     assert response.request.url == 'http://example.com/service/uri_base/post/example'
+    assert response.request.headers['Content-Type'] == 'application/json; charset=UTF-8'
     assert response.request.json() == {'pay': 'load'}
     assert response.json() == {'some response': 'you posted it'}
