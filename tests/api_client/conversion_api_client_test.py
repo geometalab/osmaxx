f0018e8e9c304539de8e1470f44d11996a409c3f
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-03-30 10:49:45 +0200

    refactoring: test call signature separately from call argument value #406

--- tests/api_client/conversion_api_client_test.py
+++ tests/api_client/conversion_api_client_test.py
@@ -5,6 +5,18 @@ from django.contrib.gis.gdal.geometries import MultiPolygon
 from osmaxx.api_client.conversion_api_client import ConversionApiClient
 
 
+def test_create_boundary_makes_authorized_post_with_json_payload(mocker):
+    c = ConversionApiClient()
+    geos_multipolygon = MultiPolygon(
+        'MULTIPOLYGON (((5 0, 5 1, 6 1, 5 0)),'
+        '((1 1, 4 0, -3 -3, 0 4, 1 1),'
+        '(0 0, 3 0, -2 -2, 0 3, 0 0)))'
+    )
+    mocker.patch.object(c, 'authorized_post', autospec=True)
+    c.create_boundary(geos_multipolygon)
+    c.authorized_post.assert_called_once_with(url=ANY, json_data=ANY)
+
+
 def test_create_boundary_posts_to_clipping_area_resource(mocker):
     c = ConversionApiClient()
     geos_multipolygon = MultiPolygon(
@@ -14,4 +26,5 @@ def test_create_boundary_posts_to_clipping_area_resource(mocker):
     )
     mocker.patch.object(c, 'authorized_post', autospec=True)
     c.create_boundary(geos_multipolygon)
-    c.authorized_post.assert_called_with(url='clipping_area/', json_data=ANY)
+    args, kwargs = c.authorized_post.call_args
+    assert kwargs['url'] == 'clipping_area/'
