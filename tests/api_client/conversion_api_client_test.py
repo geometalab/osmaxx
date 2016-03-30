a9f7fdfa515bfb1879425139f63fb67097ca6f06
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-03-30 14:18:31 +0200

    refactoring: explain what wtf we're doing with json.loads(json.dumps())

--- tests/api_client/conversion_api_client_test.py
+++ tests/api_client/conversion_api_client_test.py
@@ -31,7 +31,7 @@ def test_create_boundary_posts_geojson(mocker, geos_multipolygon):
         "name": "HSR Testcut",
         "clipping_multi_polygon": {
             "type": "MultiPolygon",
-            "coordinates": json.loads(json.dumps(geos_multipolygon.coords))
+            "coordinates": nested_list(geos_multipolygon.coords)
         }
     }
 
@@ -43,3 +43,22 @@ def geos_multipolygon():
         '((1 1, 4 0, -3 -3, 0 4, 1 1),'
         '(0 0, 3 0, -2 -2, 0 3, 0 0)))'
     )
+
+
+def nested_list(nested_collection):
+    """
+    Turn nested collection to nested list.
+
+    Might contain copies of leaf elements instead of references to the original ones.
+
+    Args:
+        nested_collection: a nested collection
+
+    Returns:
+        nested list with the same structure and content as nested_collection
+    """
+    # For alternatives, see http://stackoverflow.com/questions/1014352/
+
+    # (Ab)use the fact that json.loads always produces Python lists for JSON lists,
+    # while json.dumps turns any Python collection into a JSON list:
+    return json.loads(json.dumps(nested_collection))
