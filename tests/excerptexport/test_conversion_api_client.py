425c065fa446a4b344bb31b3850a3c0db021d126
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-04-05 12:57:22 +0200

    refactoring: make called-with-string-matching-regexp assertions more readable

--- tests/excerptexport/test_conversion_api_client.py
+++ tests/excerptexport/test_conversion_api_client.py
@@ -139,7 +139,7 @@ def test_create_job(api_client, extraction_order, job_progress_request):
     assert extraction_order.state == ExtractionOrderState.QUEUED
     assert extraction_order.process_id == response.json().get('rq_job_id')
     assert extraction_order.process_id is not None
-    job_progress_request.build_absolute_uri.assert_called_with(match_equality(matches_regexp('/job_progress/tracker/\d+/')))
+    job_progress_request.build_absolute_uri.assert_called_with(a_string_matching_regexp('/job_progress/tracker/\d+/'))
 
 
 @vcr.use_cassette('fixtures/vcr/conversion_api-test_create_job.yml')  # Intentionally same as for test_create_job()
@@ -153,7 +153,7 @@ def test_callback_url_of_created_job_resolves_to_job_updater(api_client, extract
 
     match = resolve(callback_path)
     assert match.func == tracker
-    job_progress_request.build_absolute_uri.assert_called_with(match_equality(matches_regexp('/job_progress/tracker/\d+/')))
+    job_progress_request.build_absolute_uri.assert_called_with(a_string_matching_regexp('/job_progress/tracker/\d+/'))
 
 
 @vcr.use_cassette('fixtures/vcr/conversion_api-test_create_job.yml')  # Intentionally same as for test_create_job()
@@ -167,7 +167,7 @@ def test_callback_url_of_created_job_refers_to_correct_extraction_order(api_clie
 
     match = resolve(callback_path)
     assert match.kwargs == {'order_id': str(extraction_order.id)}
-    job_progress_request.build_absolute_uri.assert_called_with(match_equality(matches_regexp('/job_progress/tracker/\d+/')))
+    job_progress_request.build_absolute_uri.assert_called_with(a_string_matching_regexp('/job_progress/tracker/\d+/'))
 
 
 @vcr.use_cassette('fixtures/vcr/conversion_api-test_create_job.yml')  # Intentionally same as for test_create_job()
@@ -180,7 +180,7 @@ def test_callback_url_would_reach_this_django_instance(api_client, extraction_or
     scheme, host, callback_path, params, *_ = urlparse(callback_url)
     assert scheme.startswith('http')  # also matches https
     assert host == 'the-host.example.com'
-    job_progress_request.build_absolute_uri.assert_called_with(match_equality(matches_regexp('/job_progress/tracker/\d+/')))
+    job_progress_request.build_absolute_uri.assert_called_with(a_string_matching_regexp('/job_progress/tracker/\d+/'))
 
 
 def test_download_files(api_client, extraction_order, job_progress_request):
@@ -212,7 +212,7 @@ def test_download_files(api_client, extraction_order, job_progress_request):
             len(extraction_order.output_files.order_by('id')[1].file.read()),
             is_close_to(368378, delta=10000)
         )
-    job_progress_request.build_absolute_uri.assert_called_with(match_equality(matches_regexp('/job_progress/tracker/\d+/')))
+    job_progress_request.build_absolute_uri.assert_called_with(a_string_matching_regexp('/job_progress/tracker/\d+/'))
 
 
 def test_order_status_processing(api_client, extraction_order, job_progress_request):
@@ -235,7 +235,7 @@ def test_order_status_processing(api_client, extraction_order, job_progress_requ
         assert extraction_order.state == ExtractionOrderState.PROCESSING
         assert extraction_order.state != ExtractionOrderState.INITIALIZED
         assert extraction_order.output_files.count() == 0
-    job_progress_request.build_absolute_uri.assert_called_with(match_equality(matches_regexp('/job_progress/tracker/\d+/')))
+    job_progress_request.build_absolute_uri.assert_called_with(a_string_matching_regexp('/job_progress/tracker/\d+/'))
 
 
 def test_order_status_done(api_client, extraction_order, job_progress_request):
@@ -257,4 +257,8 @@ def test_order_status_done(api_client, extraction_order, job_progress_request):
 
         api_client.update_order_status(extraction_order)
         assert extraction_order.state == ExtractionOrderState.FINISHED
-    job_progress_request.build_absolute_uri.assert_called_with(match_equality(matches_regexp('/job_progress/tracker/\d+/')))
+    job_progress_request.build_absolute_uri.assert_called_with(a_string_matching_regexp('/job_progress/tracker/\d+/'))
+
+
+def a_string_matching_regexp(pattern):
+    return match_equality(matches_regexp(pattern))
