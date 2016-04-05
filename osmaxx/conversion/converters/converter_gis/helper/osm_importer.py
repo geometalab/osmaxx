4b0d2a6987c07da2c5092410e07501026721a2e7
Nicola Jordan <nic@hixi.ch>
2016-04-05 15:35:11 +0200

    limit osm_ways and osm_nodes to extent as well

--- osmaxx/conversion/converters/converter_gis/helper/osm_importer.py
+++ osmaxx/conversion/converters/converter_gis/helper/osm_importer.py
@@ -1,7 +1,8 @@
+from itertools import chain
 from sqlalchemy import MetaData, Table, create_engine, func
 from sqlalchemy.engine.url import URL
 from sqlalchemy.sql import select, insert
-# import needed for reflection to work correctly with spacial types
+# import needed for reflection to work correctly with spatial types
 from geoalchemy2 import Geometry, Geography  # noqa
 
 
@@ -18,7 +19,7 @@ class OSMImporter:
             'host': 'world-database',
         }
         world_db_connection = URL('postgresql', **_world_db_connection_parameters)
-        self._world_db_engine = create_engine(world_db_connection)
+        self._world_db_engine = create_engine(world_db_connection, use_native_hstore=True)
         self._world_db_meta_data = MetaData()
 
         _local_db_connection_parameters = {
@@ -28,7 +29,7 @@ class OSMImporter:
             'database': 'osmaxx_db',
         }
         local_db_connection = URL('postgresql', **_local_db_connection_parameters)
-        self._local_db_engine = create_engine(local_db_connection)
+        self._local_db_engine = create_engine(local_db_connection, use_native_hstore=True)
         self._table_metas = {}
 
         for to_be_created_table in self._osm_derived_tables + self._osm_base_tables:
@@ -56,17 +57,39 @@ class OSMImporter:
             self._execute_and_insert_into_local_db(query, table_meta)
 
     def _load_base_tables(self):
-        for table_name in self._osm_base_tables:
-            table_meta = self._table_metas[table_name]
-            query = select([table_meta])
-            self._execute_and_insert_into_local_db(query, table_meta)
+        osm_line = self._table_metas['osm_line']
+        osm_ways = self._table_metas['osm_ways']
+        osm_nodes = self._table_metas['osm_nodes']
+
+        local_line_osm_ids_query = self._local_db_engine.execute(
+            select([osm_line.c.osm_id])
+        )
+        if local_line_osm_ids_query.rowcount > 0:
+            line_osm_ids = [q.osm_id for q in local_line_osm_ids_query.fetchall()]
+            osm_ways_query = select([osm_ways]).where(
+                osm_ways.c.id.in_(line_osm_ids)
+            )
+            rows_altered = self._execute_and_insert_into_local_db(osm_ways_query, osm_ways)
+            if rows_altered > 0:
+                local_osm_ways_query = self._local_db_engine.execute(
+                    select([osm_ways.c.nodes])
+                )
+                if local_osm_ways_query.rowcount > 0:
+                    array_rows = chain.from_iterable(local_osm_ways_query.fetchall())
+                    node_ids = chain.from_iterable(array_rows)
+                    osm_nodes_query = select([osm_nodes]).where(
+                        osm_nodes.c.id.in_(node_ids)
+                    )
+                    self._execute_and_insert_into_local_db(osm_nodes_query, osm_nodes)
 
     def _execute_and_insert_into_local_db(self, query, table_meta):
         query_result = self._world_db_engine.execute(query)
-        if query_result.rowcount > 0:
+        row_count = query_result.rowcount
+        if row_count > 0:
             results = query_result.fetchall()
             for result in results:
                 self._local_db_engine.execute(
                     insert(table_meta, values=result)
                 )
+        return row_count
 
