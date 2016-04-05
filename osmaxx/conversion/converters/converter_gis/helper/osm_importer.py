a333b6eb60abf45c66de318381d101f7e6104960
Nicola Jordan <nic@hixi.ch>
2016-04-05 12:28:52 +0200

    extract common functionality

--- osmaxx/conversion/converters/converter_gis/helper/osm_importer.py
+++ osmaxx/conversion/converters/converter_gis/helper/osm_importer.py
@@ -40,6 +40,11 @@ class OSMImporter:
                 postgresql_with_oids=True
             )
 
+    def load_area_specific_data(self, *, extent):
+        self._create_tables_on_local_db()
+        self._load_tables(extent)
+        self._load_base_tables()
+
     def _create_tables_on_local_db(self):
         self._world_db_meta_data.create_all(self._local_db_engine)
 
@@ -48,30 +53,20 @@ class OSMImporter:
             table_meta = self._table_metas[table_name]
             query = select([table_meta])
             query = query.where(func.ST_Intersects(table_meta.c.way, extent.ewkt))
-            query_result = self._world_db_engine.execute(query)
-
-            if query_result.rowcount > 0:
-                results = query_result.fetchall()
-                for result in results:
-                    self._local_db_engine.execute(
-                        insert(table_meta, values=result)
-                    )
+            self._execute_and_insert_into_local_db(query, table_meta)
 
     def _load_base_tables(self):
         for table_name in self._osm_base_tables:
             table_meta = self._table_metas[table_name]
             query = select([table_meta])
-            query_result = self._world_db_engine.execute(query)
+            self._execute_and_insert_into_local_db(query, table_meta)
 
-            if query_result.rowcount > 0:
-                results = query_result.fetchall()
-                for result in results:
-                    self._local_db_engine.execute(
-                        insert(table_meta, values=result)
-                    )
-
-    def load_area_specific_data(self, *, extent):
-        self._create_tables_on_local_db()
-        self._load_tables(extent)
-        self._load_base_tables()
+    def _execute_and_insert_into_local_db(self, query, table_meta):
+        query_result = self._world_db_engine.execute(query)
+        if query_result.rowcount > 0:
+            results = query_result.fetchall()
+            for result in results:
+                self._local_db_engine.execute(
+                    insert(table_meta, values=result)
+                )
 
