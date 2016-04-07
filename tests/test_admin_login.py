7c39085ad93e74d2bedfae0ca663216eb71f3650
beneditatan <benedita.tan@gmail.com>
2016-04-07 16:04:19 +0200

    1st test done

--- tests/test_admin_login.py
+++ tests/test_admin_login.py
@@ -3,16 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-@pytest.fixture
-def selenium(selenium):
-	selenium.implicitly_wait(10)
-	selenium.maximize_window()
-	return selenium
-
-
 @pytest.mark.nondestructive
 def test_admin_login(selenium):
     selenium.get('localhost:8000/admin/')
+    selenium.maximize_window()
     username = selenium.find_element_by_id('id_username')
     password = selenium.find_element_by_id('id_password')
     submit = selenium.find_element_by_class_name('submit-row')
