28610c1def249d4b339aa57b51b7f53170509a2b
Nicola Jordan <njordan.hsr@gmail.com>
2016-04-12 09:41:50 +0200

    added better waiting mecanism

--- docker_entrypoint/osmaxx/web_frontend/entrypoint.sh
+++ docker_entrypoint/osmaxx/web_frontend/entrypoint.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 set -e
 
-if test -z "${DATABASE_HOST}" -o -z "${DATABASE_PORT}"; then
-    echo "Missing 'database' container link!"
-    echo "You must link this container with a database container named 'database' (with postgis enabled) first"
-    exit 1
-fi
-
-# 'exec 6<>/dev/tcp/' is the equivalent of ping
-while ! exec 6<>/dev/tcp/${DATABASE_HOST}/${DATABASE_PORT}; do
-    echo "$(date) - still waiting for the database to come up"
-    sleep 1
-done
+# wait for at most 30s for the db to be up
+${HOME}/entrypoint/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} -t 30
 
 python3 web_frontend/manage.py migrate && \
 python3 web_frontend/manage.py collectstatic --noinput && \
