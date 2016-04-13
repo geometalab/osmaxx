d905e380a1ca616556315cbe558e68632949c9c6
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-04-13 17:37:39 +0200

    refactoring: move fixtures to conftest.py

--- tests/excerptexport/migrations/test_0019_one_export_per_file_format.py
+++ tests/excerptexport/migrations/test_0019_one_export_per_file_format.py
@@ -1,9 +1,6 @@
 import json
 
 import pytest
-from django.core.management import call_command
-from django.db.migrations.executor import MigrationExecutor
-from django.db import connection
 from hamcrest import assert_that, contains_inanyorder as contains_in_any_order
 
 
@@ -46,24 +43,3 @@ def test_0019_backward(migrate, user):
     ExtractionOrder = new_apps.get_model('excerptexport', 'ExtractionOrder')  # noqa
     extraction_order_after_migration = ExtractionOrder.objects.get(id=extraction_order_id)
     assert json.loads(extraction_order_after_migration._extraction_configuration) == extraction_configuration_with_formats
-
-
-@pytest.yield_fixture
-def migrate(_executor, transactional_db):
-    def _migrate(app_name, migration_name):
-        migration = [(app_name, migration_name)]
-        # For whatever reason, `executor.migrate(migration)` doesn't work in this context.
-        # So we use the manage.py command instead:
-        call_command('migrate', app_name, migration_name)
-        apps_at_state = _executor.loader.project_state(migration).apps
-        return apps_at_state
-
-    yield _migrate
-
-    # Back to latest state for following tests:
-    call_command('migrate')
-
-
-@pytest.fixture
-def _executor():
-    return MigrationExecutor(connection)
