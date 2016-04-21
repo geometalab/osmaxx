3c15dfcb4932b918e9eb13ce6784924e466685ae
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-04-21 16:00:00 +0200

    refactoring: extract message to local variable

--- osmaxx/job_progress/views.py
+++ osmaxx/job_progress/views.py
@@ -25,9 +25,8 @@ def _handle_new_status(export, new_status):
 def _handle_changed_status(export):
     emissary = Emissary(recipient=export.extraction_order.orderer)
     view_context = dict(export=export)
-    emissary.info(
-        render_to_string(
-            'job_progress/messages/export_status_changed.txt',
-            context=view_context,
-        ).strip()
-    )
+    message = render_to_string(
+        'job_progress/messages/export_status_changed.txt',
+        context=view_context,
+    ).strip()
+    emissary.info(message)
