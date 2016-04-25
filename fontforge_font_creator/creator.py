e7f34ac329e1d3f4f52ff86860ad2974b44c4631
Nicola Jordan <nic@hixi.ch>
2016-04-25 15:14:53 +0200

    use more speaking variable name

--- fontforge_font_creator/creator.py
+++ fontforge_font_creator/creator.py
@@ -14,31 +14,31 @@ class FontMaker:
     def __init__(self, font_config):
         self.config = self.yml_reader(font_config)
         self._base_path = '/home/fonts/osmaxx/svg'
-        self.n = fontforge.font()
-        self.n.encoding = 'Unicode'
+        self.fontforge_font = fontforge.font()
+        self.fontforge_font.encoding = 'Unicode'
 
     def yml_reader(self, font_config):
         return ruamel.yaml.load(open(font_config, 'r').read())
 
     def add_glyph(self, hex_position, svg):
-        glyph = self.n.createChar(hex_position)
+        glyph = self.fontforge_font.createChar(hex_position)
         glyph.importOutlines(os.path.join(self._base_path, svg))
 
     def create_font(self):
         for font, definition in self.config.items():
             print("creating {}".format(font))
-            self.n.fontname = definition['fontname']
-            self.n.familyname = definition['fontname']
-            self.n.fullname = definition['fontname']
-            self.n.comment = definition['fontname']
-            self.n.weight = 'normal'
+            self.fontforge_font.fontname = definition['fontname']
+            self.fontforge_font.familyname = definition['fontname']
+            self.fontforge_font.fullname = definition['fontname']
+            self.fontforge_font.comment = definition['fontname']
+            self.fontforge_font.weight = 'normal'
             for hex_position, glyph in definition['mappings'].items():
                 hex_value = int(hex_position, 16)
                 print(hex_value, glyph['filename'])
                 self.add_glyph(hex_value, glyph['filename'])
-            self.n.save('{}.sfd'.format(definition['fontname']))
+            self.fontforge_font.save('{}.sfd'.format(definition['fontname']))
             export_file_name = '{}'.format(definition['filename'])
-            self.n.generate(export_file_name)
+            self.fontforge_font.generate(export_file_name)
             print("generated: ", export_file_name)
             shutil.move(export_file_name, '/out/OSMaxx_v1.ttf')
 
