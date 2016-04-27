317b95dbfd6f7012097669bb171b775c5bf36c37
Nicola Jordan <nic@hixi.ch>
2016-04-27 15:36:27 +0200

    add about us in top navigation and extra downloads entry in footer

--- osmaxx/core/urls.py
+++ osmaxx/core/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^about/$', TemplateView.as_view(template_name="core/about_us.html"), name='about'),
-    url(r'^downloads/$', TemplateView.as_view(template_name="core/downloads.html"), name='downloads'),
+    url(r'^about/$', TemplateView.as_view(template_name="pages/about_us.html"), name='about'),
+    url(r'^downloads/$', TemplateView.as_view(template_name="pages/downloads.html"), name='downloads'),
 ]
--- web_frontend/config/urls.py
+++ web_frontend/config/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include('osmaxx.excerptexport.rest_api.urls', namespace='excerptexport_api')),
     url(r'^job_progress/', include('osmaxx.job_progress.urls', namespace='job_progress')),
-    url(r'^pages/', include('osmaxx.core.urls', namespace='core')),
+    url(r'^pages/', include('osmaxx.core.urls', namespace='pages')),
 ]
