de63f916982c9ef12bf01c904ecefe4fc4817043
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-04-27 14:37:57 +0200

    handle changed status only after persisting it
    so that code invoked for the handling will see a consistent and up-to-date DB state.
    
    Doesn't matter for the current implementation, but additional functionality will have to query the database.

--- osmaxx/excerptexport/models/export.py
+++ osmaxx/excerptexport/models/export.py
@@ -51,8 +51,8 @@ class Export(models.Model):
     def set_and_handle_new_status(self, new_status):
         if self.status != new_status:
             self.status = new_status
-            self._handle_changed_status()
             self.save()
+            self._handle_changed_status()
 
     def _handle_changed_status(self):
         from osmaxx.utilities.shortcuts import Emissary
