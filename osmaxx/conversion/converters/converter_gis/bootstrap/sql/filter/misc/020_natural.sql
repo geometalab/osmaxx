a19f2349f27306a405c052fbebe51ebce77faeaa
Nicola Jordan <nic@hixi.ch>
2016-04-28 10:32:49 +0200

    replaced all tabs with spaces

--- osmaxx/conversion/converters/converter_gis/bootstrap/sql/filter/misc/020_natural.sql
+++ osmaxx/conversion/converters/converter_gis/bootstrap/sql/filter/misc/020_natural.sql
@@ -2,30 +2,30 @@
 
 INSERT INTO osmaxx.misc_l
   SELECT osm_id as osm_id,
-	osm_timestamp as lastchange,
-	CASE
-	 WHEN osm_id<0 THEN 'R' -- R=Relation
-	 ELSE 'W' 		-- W=Way
-	 END AS geomtype,
-	ST_Multi(way) AS geom,
-	'natural' as aggtype,
-	"natural" AS type,
-	name as name,
-	"name:en" as name_en,
-	"name:fr" as name_fr,
-	"name:es" as name_es,
-	"name:de" as name_de,
-	int_name as name_int,
-	case
-		when name is not null AND name = transliterate(name) then name
-		when "name:en" is not null then "name:en"
-		when "name:fr" is not null then "name:fr"
-		when "name:es" is not null then "name:es"
-		when "name:de" is not null then "name:de"
-		when name is not null then transliterate(name)
-		else NULL
-	end as label, 
-	cast(tags as text) as tags
+    osm_timestamp as lastchange,
+    CASE
+     WHEN osm_id<0 THEN 'R' -- R=Relation
+     ELSE 'W'         -- W=Way
+     END AS geomtype,
+    ST_Multi(way) AS geom,
+    'natural' as aggtype,
+    "natural" AS type,
+    name as name,
+    "name:en" as name_en,
+    "name:fr" as name_fr,
+    "name:es" as name_es,
+    "name:de" as name_de,
+    int_name as name_int,
+    case
+        when name is not null AND name = transliterate(name) then name
+        when "name:en" is not null then "name:en"
+        when "name:fr" is not null then "name:fr"
+        when "name:es" is not null then "name:es"
+        when "name:de" is not null then "name:de"
+        when name is not null then transliterate(name)
+        else NULL
+    end as label,
+    cast(tags as text) as tags
   FROM osm_line
   WHERE "natural"='cliff'
 UNION
@@ -40,30 +40,30 @@ UNION
       FROM osm_polygon
   )
   SELECT osm_id as osm_id,
-	osm_timestamp as lastchange,
-	CASE
-	 WHEN osm_id<0 THEN 'R' -- R=Relation
-	 ELSE 'W' 		-- W=Way
-	 END AS geomtype,
-	ST_Multi(way) AS geom,
-	'natural' as aggtype,
-	"natural" AS type,
-	name as name,
-	"name:en" as name_en,
-	"name:fr" as name_fr,
-	"name:es" as name_es,
-	"name:de" as name_de,
-	int_name as name_int,
-	case
-		when name is not null AND name = transliterate(name) then name
-		when "name:en" is not null then "name:en"
-		when "name:fr" is not null then "name:fr"
-		when "name:es" is not null then "name:es"
-		when "name:de" is not null then "name:de"
-		when name is not null then transliterate(name)
-		else NULL
-	end as label, 
-	cast(tags as text) as tags
+    osm_timestamp as lastchange,
+    CASE
+     WHEN osm_id<0 THEN 'R' -- R=Relation
+     ELSE 'W'         -- W=Way
+     END AS geomtype,
+    ST_Multi(way) AS geom,
+    'natural' as aggtype,
+    "natural" AS type,
+    name as name,
+    "name:en" as name_en,
+    "name:fr" as name_fr,
+    "name:es" as name_es,
+    "name:de" as name_de,
+    int_name as name_int,
+    case
+        when name is not null AND name = transliterate(name) then name
+        when "name:en" is not null then "name:en"
+        when "name:fr" is not null then "name:fr"
+        when "name:es" is not null then "name:es"
+        when "name:de" is not null then "name:de"
+        when name is not null then transliterate(name)
+        else NULL
+    end as label,
+    cast(tags as text) as tags
   FROM osm_single_polygon
   WHERE  "natural"='cliff'
 );
--- osmaxx/conversion/converters/converter_gis/bootstrap/sql/filter/nonop/010_nonop.sql
+++ osmaxx/conversion/converters/converter_gis/bootstrap/sql/filter/nonop/010_nonop.sql
@@ -1,54 +1,54 @@
 INSERT INTO osmaxx.nonop_l
   SELECT osm_id as osm_id,
-	osm_timestamp as lastchange ,
+    osm_timestamp as lastchange ,
 -- R=Relation & W=Way --
-	CASE
-	 WHEN osm_id<0 THEN 'R' -- Relation
-	 ELSE 'W' 		-- Way
-	END AS geomtype,
+    CASE
+     WHEN osm_id<0 THEN 'R' -- Relation
+     ELSE 'W'         -- Way
+    END AS geomtype,
 
-	ST_Multi(way) AS geom,
+    ST_Multi(way) AS geom,
 -- Differentiating between Highway and Railway --
-	case
-	when highway is not null then 'highway'
-	when railway is not null then 'railway'
-	end as type,
-	name as name,
-	"name:en" as name_en,
-	"name:fr" as name_fr,
-	"name:es" as name_es,
-	"name:de" as name_de,
-	int_name as name_int,
-	case
-		when name is not null AND name = transliterate(name) then name
-		when "name:en" is not null then "name:en"
-		when "name:fr" is not null then "name:fr"
-		when "name:es" is not null then "name:es"
-		when "name:de" is not null then "name:de"
-		when name is not null then transliterate(name)
-		else NULL
-	end as label,
-	cast(tags as text) as tags,
-	ref as ref,
+    case
+    when highway is not null then 'highway'
+    when railway is not null then 'railway'
+    end as type,
+    name as name,
+    "name:en" as name_en,
+    "name:fr" as name_fr,
+    "name:es" as name_es,
+    "name:de" as name_de,
+    int_name as name_int,
+    case
+        when name is not null AND name = transliterate(name) then name
+        when "name:en" is not null then "name:en"
+        when "name:fr" is not null then "name:fr"
+        when "name:es" is not null then "name:es"
+        when "name:de" is not null then "name:de"
+        when name is not null then transliterate(name)
+        else NULL
+    end as label,
+    cast(tags as text) as tags,
+    ref as ref,
 -- Checking for bridges with different tags associated to bridges --
-	case
-	when bridge in ('split_log' , 'beam', 'culvert', 'low_water_crossing', 'yes', 'suspension', 'viaduct', 'aqueduct', 'covered') then TRUE
-	else FALSE
-	end as bridge,
+    case
+    when bridge in ('split_log' , 'beam', 'culvert', 'low_water_crossing', 'yes', 'suspension', 'viaduct', 'aqueduct', 'covered') then TRUE
+    else FALSE
+    end as bridge,
 -- Checking for tunnels with different tags associated to tunnels --
-	case
-	when tunnel in ('passage', 'culvert', 'noiseprotection galerie', 'gallery', 'building_passage', 'avalanche_protector','teilweise', 'viaduct', 'tunnel', 'yes') then TRUE
-	else FALSE
-	end as tunnel,
+    case
+    when tunnel in ('passage', 'culvert', 'noiseprotection galerie', 'gallery', 'building_passage', 'avalanche_protector','teilweise', 'viaduct', 'tunnel', 'yes') then TRUE
+    else FALSE
+    end as tunnel,
 
-	z_order as z_order,
+    z_order as z_order,
 -- Differentiating the different types of transport --
-	case
-	 when highway='planned' or railway='planned' then 'P'
-	 when highway='disused'  or railway='disused' then 'D'
-	 when highway='construction'  or railway='construction' then 'C'
-	 when highway='abandoned'  or railway='abandoned' then 'A'
-	 end as status
+    case
+     when highway='planned' or railway='planned' then 'P'
+     when highway='disused'  or railway='disused' then 'D'
+     when highway='construction'  or railway='construction' then 'C'
+     when highway='abandoned'  or railway='abandoned' then 'A'
+     end as status
  FROM osm_line
  WHERE highway='planned' or  highway='disused' or highway='construction' or highway='abandoned' or
-	railway='planned' or  railway='disused' or railway='construction' or railway='abandoned' ;
+    railway='planned' or  railway='disused' or railway='construction' or railway='abandoned' ;
