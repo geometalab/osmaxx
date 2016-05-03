c8d8c51171c5f1b197b507f38e6c5136d778a55a
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-05-03 10:43:54 +0200

    refactoring: extract local variable for QuerySet
    # Conflicts:
    #	osmaxx/job_progress/middleware.py

--- osmaxx/job_progress/middleware.py
+++ osmaxx/job_progress/middleware.py
@@ -24,7 +24,10 @@ def update_exports_of_request_user(request):
     current_user = request.user
     if current_user.is_anonymous():
         return
-    for export in Export.objects.exclude(status__in=FINAL_STATUSES).filter(extraction_order__orderer=current_user):
+    transmitted_but_unfinished_exports = Export.objects.\
+        exclude(status__in=FINAL_STATUSES).\
+        filter(extraction_order__orderer=current_user)
+    for export in transmitted_but_unfinished_exports:
         update_export_if_stale(export)
 
 
