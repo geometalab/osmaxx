3ad341d1164e8dbf2c3d63898a7be103bc494320
Nicola Jordan <nic@hixi.ch>
2016-05-03 13:51:03 +0200

    server static/media contents during development through django

--- web_frontend/config/urls.py
+++ web_frontend/config/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -12,3 +14,7 @@ urlpatterns = [
     url(r'^job_progress/', include('osmaxx.job_progress.urls', namespace='job_progress')),
     url(r'^pages/', include('osmaxx.core.urls', namespace='pages')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=True) +\
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, show_indexes=True)
