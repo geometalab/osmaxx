139d76bf35dbec9318270a51178daa6ccff82b0e
Nicola Jordan <nic@hixi.ch>
2016-05-04 10:53:15 +0200

    add bounding geometry to excerpt form

--- osmaxx/excerptexport/forms/excerpt_form.py
+++ osmaxx/excerptexport/forms/excerpt_form.py
@@ -1,3 +1,6 @@
+import json
+
+from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 from django import forms
 from django.core.urlresolvers import reverse
@@ -6,16 +9,20 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, Field, Submit
 
 from .order_options_mixin import OrderOptionsMixin, get_export_options
-from .bbox_bounding_geometry_mixin import BBoxBoundingGeometryMixin
-from osmaxx.excerptexport.models import Excerpt, ExtractionOrder, BBoxBoundingGeometry
+from osmaxx.excerptexport.models import Excerpt, ExtractionOrder
 from osmaxx.utilities.dict_helpers import select_keys
 
 
-class ExcerptForm(BBoxBoundingGeometryMixin, OrderOptionsMixin, forms.ModelForm):
+class ExcerptForm(OrderOptionsMixin, forms.ModelForm):
     name = forms.CharField(
         label=_('Name'),
         required=True,
         widget=forms.TextInput(attrs={'required': 'required'}),
+    )
+    bounding_geometry = forms.CharField(
+        label=_('Bounding Geometry (GeoJSON)'),
+        required=True,
+        widget=forms.Textarea(attrs={'required': 'required', 'rows': '10'}),
     )
 
     def __init__(self, *args, **kwargs):
@@ -30,30 +37,36 @@ class ExcerptForm(BBoxBoundingGeometryMixin, OrderOptionsMixin, forms.ModelForm)
         self.helper.layout = Layout(
             Fieldset(
                 _('Excerpt'),
-                Field('name'), 'is_public',
+                Field('name'),
+                Field('bounding_geometry'),
+                'is_public',
             ),
-            BBoxBoundingGeometryMixin(self).form_layout(),
             OrderOptionsMixin(self).form_layout(),
             Submit('submit', 'Submit'),
         )
 
+    def clean_bounding_geometry(self):
+        allowed_types = ['Polygon', 'Multipolygon']
+        data = self.cleaned_data['bounding_geometry']
+        data_dict = json.loads(data)
+        if data_dict.get('type') not in allowed_types:
+            raise ValidationError(_('Only {} are allowed as input.'.format(allowed_types)))
+        if data_dict.get('type') == 'Polygon':
+            data_dict['coordinates'] = [data_dict.get('coordinates')]
+            data_dict['type'] = 'Multipolygon'
+        return json.dumps(data_dict)
+
     class Meta:
         model = Excerpt
-        fields = ['name', 'is_public']
+        fields = ['name', 'is_public', 'bounding_geometry']
 
     def save(self, user):
         extraction_order = ExtractionOrder(orderer=user)
         extraction_order.extraction_configuration = get_export_options()
         extraction_order.extraction_formats = self.cleaned_data['formats']
-
-        bbox_kwargs = select_keys(self.cleaned_data, ['north', 'east', 'south', 'west'])
-        bounding_geometry = BBoxBoundingGeometry(**bbox_kwargs)
-        bounding_geometry.save()
-
-        excerpt_dict = select_keys(self.cleaned_data, ['name', 'is_public'])
+        excerpt_dict = select_keys(self.cleaned_data, ['name', 'is_public', 'bounding_geometry'])
         excerpt = Excerpt(
             is_active=True,
-            bounding_geometry_old=bounding_geometry,
             owner=user,
             **excerpt_dict
         )
