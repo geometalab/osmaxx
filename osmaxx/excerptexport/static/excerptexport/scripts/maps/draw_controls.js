8626f9d209fefed563d04e46d38bd67828768986
Nicola Jordan <nic@hixi.ch>
2016-05-06 16:23:03 +0200

    mak javascript more future reboust

--- osmaxx/excerptexport/static/excerptexport/scripts/maps/draw_controls.js
+++ osmaxx/excerptexport/static/excerptexport/scripts/maps/draw_controls.js
@@ -70,7 +70,7 @@ var draw_controls = function (map) {
                if(bytes == 0) return '0 Byte';
                var k = 1000;
                var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-               var i = Math.floor(Math.log(bytes) / Math.log(k));
+               var i = Math.max(Math.floor(Math.log(bytes) / Math.log(k)), sizes.length - 1);
                return parseFloat((bytes / Math.pow(k, i)).toFixed()) + ' ' + sizes[i];
             }
 
