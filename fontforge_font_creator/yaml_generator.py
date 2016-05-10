f6e45deff2bd4d3155776ca7065a9ec0dbf15f3b
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-05-10 17:39:29 +0200

    separate glyph per unique path in SVGs

--- fontforge_font_creator/yaml_generator.py
+++ fontforge_font_creator/yaml_generator.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 import os
 import unicodedata
+from xml.etree import ElementTree
 
+UNICODE_MAX = 0x10FFFF  # last chr of plane 16 (plane 0x10)
 
 class UnicodeRanger:
-    def __init__(self, start, stop):
+    def __init__(self, start, stop=UNICODE_MAX):
+        assert start >= 0
+        assert stop <= UNICODE_MAX
         self.current = start
         self.high = stop
         self.stop = stop
@@ -27,7 +31,7 @@ class UnicodeRanger:
         return self
 
     def __next__(self):
-        if self.current > self.stop:
+        if self.stop is not None and self.current > self.stop:
             raise StopIteration
         else:
             self.current += 1
@@ -42,9 +46,11 @@ svgs = [f for f in os.listdir(svg_dir) if f.lower().endswith('.svg')]
 svgs.sort()
 
 start_number = 0xE000
-r = UnicodeRanger(start_number, start_number + len(svgs))
+r = UnicodeRanger(start_number)
 outfile = os.path.join(os.path.dirname(__file__), 'osmaxx_v1_definition.yml')
 oufile_writer = open(outfile, 'w+')
+
+ns = dict(svg='http://www.w3.org/2000/svg')
 
 print('''---
 osmaxx_v1:
@@ -53,7 +59,15 @@ osmaxx_v1:
   mappings:''', file=oufile_writer)
 
 count = 0
+already_done = set()
 for svg in svgs:
-    print("    \"{:#06x}\":".format(r.__next__()), file=oufile_writer)
-    print("      filename: \"{}\"".format(svg), file=oufile_writer)
-    count += 1
+    tree = ElementTree.parse(os.path.join(svg_dir, svg))
+    for path_el in tree.findall('.//svg:path', ns):
+        h = hash(path_el.get('d'))
+        if h not in already_done:
+            el_id = path_el.get('id')
+            print("    \"{:#06x}\":".format(r.__next__()), file=oufile_writer)
+            print("      filename: \"{}\"".format(svg), file=oufile_writer)
+            print("      element: \"{}\"".format(el_id), file=oufile_writer)
+            already_done.add(h)
+            count += 1
