249b6b58d37c7b4a8de52003be52c9e11fafd363
Nicola Jordan <nic@hixi.ch>
2016-05-12 10:24:26 +0200

    merge part2: add new test settings

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true",
                      help="run slow tests")
+    parser.addoption("--selenium", action="store_true",
+                     help="run selenium tests")
