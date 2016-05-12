9a1f0a0d4bcd4e3079bcc251ed981302fb8e8378
Nicola Jordan <nic@hixi.ch>
2016-05-12 10:37:13 +0200

    remove obsolete config

--- conftest.py
+++ conftest.py
@@ -1,5 +1,3 @@
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true",
                      help="run slow tests")
-    parser.addoption("--selenium", action="store_true",
-                     help="run selenium tests")
