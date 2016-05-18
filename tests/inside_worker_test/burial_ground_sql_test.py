0b10001766af07b93f55603a244a0212594cee40
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-05-18 15:04:08 +0200

    add test for existing behavior: amenity=grave_yard with religion!=* will not appear in pow_a #477
    geometalab/osmaxx-conversion-service#124

--- tests/inside_worker_test/burial_ground_sql_test.py
+++ tests/inside_worker_test/burial_ground_sql_test.py
@@ -24,6 +24,19 @@ def test_osmaxx_data_model_processing_puts_amenity_grave_yard_with_religion_into
 
 
 @slow
+def test_osmaxx_data_model_processing_does_not_put_amenity_grave_yard_without_religion_into_table_pow_a(data_import):
+    data = {
+        osm_models.t_osm_polygon: dict(
+            amenity='grave_yard',
+        ),
+    }
+    with data_import(data) as engine:
+        t_pow_a = DbTable('pow_a', osm_models.metadata, schema='osmaxx')
+        with closing(engine.execute(sqlalchemy.select([t_pow_a]))) as result:
+            assert result.rowcount == 0
+
+
+@slow
 def test_osmaxx_data_model_processing_puts_amenity_grave_yard_with_religion_into_table_poi_a(data_import):
     data = {
         osm_models.t_osm_polygon: dict(
