ab4afdaf77ea2d7b7d0c86e77ddca62459f857ca
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-05-20 14:52:17 +0200

    refactoring: parametrize input OSM tables #477
    geometalab/osmaxx-conversion-service#124

--- tests/inside_worker_test/burial_ground_sql_test.py
+++ tests/inside_worker_test/burial_ground_sql_test.py
@@ -9,14 +9,9 @@ from tests.inside_worker_test.conftest import slow
 from tests.inside_worker_test.declarative_schema import osm_models
 
 
-@pytest.fixture
-def graveyard_polygon(osm_tags):
-    return {osm_models.t_osm_polygon: osm_tags}
-
-
-@pytest.fixture
-def graveyard_point(osm_tags):
-    return {osm_models.t_osm_point: osm_tags}
+@pytest.fixture(params=[osm_models.t_osm_polygon, osm_models.t_osm_point], ids=['osm_polygon', 'osm_point'])
+def graveyard_data(request, osm_tags):
+    return {request.param: osm_tags}
 
 
 @pytest.fixture(
@@ -38,51 +33,34 @@ def osm_tags(request):
 
 
 @pytest.fixture(params=['pow_a', 'landuse_a', 'pow_p'])
-def non_burial_ground_area_target_layer(request):
+def non_burial_ground_target_layer(request):
     return DbTable(request.param, osm_models.metadata, schema='osmaxx')
 
 
 @pytest.fixture(params=['poi_a', 'poi_p'])
-def burial_ground_area_target_layer(request):
+def burial_ground_target_layer(request):
     return DbTable(request.param, osm_models.metadata, schema='osmaxx')
 
 
 @slow
-def test_osmaxx_data_model_processing_does_not_put_burial_ground_into_non_burial_ground_area_target_layer(
-        graveyard_polygon, data_import, non_burial_ground_area_target_layer):
-    with data_import(graveyard_polygon) as engine:
-        with closing(engine.execute(sqlalchemy.select('*').select_from(non_burial_ground_area_target_layer))) as result:
+def test_osmaxx_data_model_processing_does_not_put_burial_ground_into_non_burial_ground_target_layer(
+        graveyard_data, data_import, non_burial_ground_target_layer):
+    if all(source_table is osm_models.t_osm_point for source_table in graveyard_data.keys()) \
+            and not non_burial_ground_target_layer.name.endswith('_p'):
+        pytest.skip()
+    with data_import(graveyard_data) as engine:
+        with closing(engine.execute(sqlalchemy.select('*').select_from(non_burial_ground_target_layer))) as result:
             assert result.rowcount == 0
 
 
 @slow
-def test_osmaxx_data_model_processing_puts_burial_ground_into_burial_ground_area_target_layer(
-        graveyard_polygon, data_import, osm_tags, burial_ground_area_target_layer):
-    with data_import(graveyard_polygon) as engine:
-        with closing(engine.execute(sqlalchemy.select('*').select_from(burial_ground_area_target_layer))) as result:
-            assert result.rowcount == 1
-            row = result.fetchone()
-            expected_type = osm_tags.get('amenity', None) or osm_tags['landuse']
-            assert expected_type in {'grave_yard', 'cemetery'}  # just a sanity check, not the test assertion
-            assert row['type'] == expected_type
-            assert row['aggtype'] == 'burial_ground'
-
-
-@slow
-def test_osmaxx_data_model_processing_does_not_put_burial_ground_point_into_table_pow_p(
-        graveyard_point, data_import):
-    t_pow_p = DbTable('pow_p', osm_models.metadata, schema='osmaxx')
-    with data_import(graveyard_point) as engine:
-        with closing(engine.execute(sqlalchemy.select('*').select_from(t_pow_p))) as result:
-            assert result.rowcount == 0
-
-
-@slow
-def test_osmaxx_data_model_processing_puts_burial_ground_point_into_table_poi_p(
-        graveyard_point, data_import, osm_tags):
-    t_poi_p = DbTable('poi_p', osm_models.metadata, schema='osmaxx')
-    with data_import(graveyard_point) as engine:
-        with closing(engine.execute(sqlalchemy.select('*').select_from(t_poi_p))) as result:
+def test_osmaxx_data_model_processing_puts_burial_ground_into_burial_ground_target_layer(
+        graveyard_data, data_import, osm_tags, burial_ground_target_layer):
+    if all(source_table is osm_models.t_osm_point for source_table in graveyard_data.keys()) \
+            and not burial_ground_target_layer.name.endswith('_p'):
+        pytest.skip()
+    with data_import(graveyard_data) as engine:
+        with closing(engine.execute(sqlalchemy.select('*').select_from(burial_ground_target_layer))) as result:
             assert result.rowcount == 1
             row = result.fetchone()
             expected_type = osm_tags.get('amenity', None) or osm_tags['landuse']
