fa798b642bebfc4f8b5fdc0919562575c019e18c
Nicola Jordan <nic@hixi.ch>
2016-05-20 13:28:22 +0200

    make path combining more readable

--- osmaxx/excerptexport/_settings.py
+++ osmaxx/excerptexport/_settings.py
@@ -2,7 +2,7 @@ import os
 
 from django.conf import settings
 
-POLYFILE_LOCATION = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'polyfiles')
+POLYFILE_LOCATION = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'polyfiles'))
 
 # only needed for testing
 if hasattr(settings, '_OSMAXX_POLYFILE_LOCATION'):
