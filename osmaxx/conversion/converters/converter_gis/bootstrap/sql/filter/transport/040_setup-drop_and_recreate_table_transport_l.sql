36778880c60401e3756e90f4abcd4bd8de316868
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-05-24 11:29:06 +0200

    re-format SQL code #422

--- osmaxx/conversion/converters/converter_gis/bootstrap/sql/filter/transport/040_setup-drop_and_recreate_table_transport_l.sql
+++ osmaxx/conversion/converters/converter_gis/bootstrap/sql/filter/transport/040_setup-drop_and_recreate_table_transport_l.sql
@@ -1,17 +1,17 @@
-DROP TABLE if exists osmaxx.transport_l;
-CREATE TABLE osmaxx.transport_l(
-    osm_id bigint,
-    lastchange timestamp without time zone,
-    geomtype char(1),
-    geom geometry(MULTILINESTRING, 4326),
-    aggtype text,
-    type text,
-    name text,
-    name_en text,
-    name_fr text,
-    name_es text,
-    name_de text,
-    int_name text,
-    label text,
-    tags text
+DROP TABLE IF EXISTS osmaxx.transport_l;
+CREATE TABLE osmaxx.transport_l (
+  osm_id     BIGINT,
+  lastchange TIMESTAMP WITHOUT TIME ZONE,
+  geomtype   CHAR(1),
+  geom       GEOMETRY(MULTILINESTRING, 4326),
+  aggtype    TEXT,
+  type       TEXT,
+  name       TEXT,
+  name_en    TEXT,
+  name_fr    TEXT,
+  name_es    TEXT,
+  name_de    TEXT,
+  int_name   TEXT,
+  label      TEXT,
+  tags       TEXT
 );
--- osmaxx/conversion/converters/converter_gis/bootstrap/sql/filter/transport/050_air_traffic.sql
+++ osmaxx/conversion/converters/converter_gis/bootstrap/sql/filter/transport/050_air_traffic.sql
@@ -1,68 +1,81 @@
 -- air_traffic --
 INSERT INTO osmaxx.transport_l
-  SELECT osm_id as osm_id,
-    osm_timestamp as lastchange,
+  SELECT
+    osm_id             AS osm_id,
+    osm_timestamp      AS lastchange,
     CASE
-     WHEN osm_id<0 THEN 'R' -- R=Relation
-     ELSE 'W'               -- W=Way
-     END AS geomtype,
-    ST_Multi(way) AS geom,
-    'air_traffic' as aggtype,
-    aeroway as type,
-    name as name,
-    "name:en" as name_en,
-    "name:fr" as name_fr,
-    "name:es" as name_es,
-    "name:de" as name_de,
-    int_name as name_int,
-    case
-        when name is not null AND name = transliterate(name) then name
-        when "name:en" is not null then "name:en"
-        when "name:fr" is not null then "name:fr"
-        when "name:es" is not null then "name:es"
-        when "name:de" is not null then "name:de"
-        when name is not null then transliterate(name)
-        else NULL
-    end as label,
-    cast(tags as text) as tags
+      WHEN osm_id < 0 THEN 'R' -- R=Relation
+      ELSE 'W'                 -- W=Way
+    END                AS geomtype,
+    ST_Multi(way)      AS geom,
+    'air_traffic'      AS aggtype,
+    aeroway            AS type,
+    name               AS name,
+    "name:en"          AS name_en,
+    "name:fr"          AS name_fr,
+    "name:es"          AS name_es,
+    "name:de"          AS name_de,
+    int_name           AS name_int,
+    CASE
+      WHEN name IS NOT NULL AND name = transliterate(name) THEN name
+      WHEN "name:en" IS NOT NULL THEN "name:en"
+      WHEN "name:fr" IS NOT NULL THEN "name:fr"
+      WHEN "name:es" IS NOT NULL THEN "name:es"
+      WHEN "name:de" IS NOT NULL THEN "name:de"
+      WHEN name IS NOT NULL THEN transliterate(name)
+      ELSE NULL
+    END                AS label,
+    cast(tags AS TEXT) AS tags
   FROM osm_line
-  WHERE aeroway in ('runway','taxiway','apron')
+  WHERE aeroway IN ('runway', 'taxiway', 'apron')
 UNION
-(
-  WITH osm_single_polygon AS (
-      SELECT osm_id, osm_timestamp, aeroway, name, "name:en", "name:fr", "name:es", "name:de", int_name, tags,
+  (
+    WITH osm_single_polygon AS (
+        SELECT
+          osm_id,
+          osm_timestamp,
+          aeroway,
+          name,
+          "name:en",
+          "name:fr",
+          "name:es",
+          "name:de",
+          int_name,
+          tags,
 
-      CASE WHEN ST_GeometryType(way) = ANY(array['ST_MultiPolygon', 'ST_Polygon'])
-        THEN ST_Boundary(way)
-        ELSE way
-      END AS way
-      FROM osm_polygon
-  )
-  SELECT osm_id as osm_id,
-    osm_timestamp as lastchange,
-    CASE
-     WHEN osm_id<0 THEN 'R' -- R=Relation
-     ELSE 'W'               -- W=Way
-     END AS geomtype,
-    ST_Multi(way) AS geom,
-    'air_traffic' as aggtype,
-    aeroway as type,
-    name as name,
-    "name:en" as name_en,
-    "name:fr" as name_fr,
-    "name:es" as name_es,
-    "name:de" as name_de,
-    int_name as name_int,
-    case
-        when name is not null AND name = transliterate(name) then name
-        when "name:en" is not null then "name:en"
-        when "name:fr" is not null then "name:fr"
-        when "name:es" is not null then "name:es"
-        when "name:de" is not null then "name:de"
-        when name is not null then transliterate(name)
-        else NULL
-    end as label,
-    cast(tags as text) as tags
-  FROM osm_single_polygon
-  WHERE aeroway in ('runway','taxiway','apron')
-);
+          CASE WHEN ST_GeometryType(way) = ANY (ARRAY ['ST_MultiPolygon', 'ST_Polygon'])
+            THEN ST_Boundary(way)
+          ELSE way
+          END AS way
+        FROM osm_polygon
+    )
+    SELECT
+      osm_id             AS osm_id,
+      osm_timestamp      AS lastchange,
+      CASE
+      WHEN osm_id < 0
+        THEN 'R' -- R=Relation
+      ELSE 'W' -- W=Way
+      END                AS geomtype,
+      ST_Multi(way)      AS geom,
+      'air_traffic'      AS aggtype,
+      aeroway            AS type,
+      name               AS name,
+      "name:en"          AS name_en,
+      "name:fr"          AS name_fr,
+      "name:es"          AS name_es,
+      "name:de"          AS name_de,
+      int_name           AS name_int,
+      CASE
+        WHEN name IS NOT NULL AND name = transliterate(name) THEN name
+        WHEN "name:en" IS NOT NULL THEN "name:en"
+        WHEN "name:fr" IS NOT NULL THEN "name:fr"
+        WHEN "name:es" IS NOT NULL THEN "name:es"
+        WHEN "name:de" IS NOT NULL THEN "name:de"
+        WHEN name IS NOT NULL THEN transliterate(name)
+        ELSE NULL
+      END                AS label,
+      cast(tags AS TEXT) AS tags
+    FROM osm_single_polygon
+    WHERE aeroway IN ('runway', 'taxiway', 'apron')
+  );
