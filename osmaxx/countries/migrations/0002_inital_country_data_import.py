e05fea6fe33625db4bf678687df731d20bbf0a83
Nicola Jordan <nic@hixi.ch>
2016-05-25 11:06:04 +0200

    make migrations run again on new installation where countries are not available

--- osmaxx/countries/migrations/0002_inital_country_data_import.py
+++ osmaxx/countries/migrations/0002_inital_country_data_import.py
@@ -3,23 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 
-from osmaxx.countries.utils import get_polyfile_name_to_file_mapping
-from osmaxx.utils.polyfile_helpers import polyfile_to_geos_geometry
-
 
 class Migration(migrations.Migration):
     def import_countries(apps, schema_editor):  # noqa
-        tolerance = 0.01  # in degrees
-        Country = apps.get_model("countries", "Country")  # noqa
-        for name, polyfile_path in get_polyfile_name_to_file_mapping().items():
-            geometry = polyfile_to_geos_geometry(polyfile_path)
-            simplified_geometry = polyfile_to_geos_geometry(polyfile_path, simplify_tolerance=tolerance)
-            Country.objects.create(
-                name=name,
-                polyfile=polyfile_path,
-                polygon=geometry,
-                simplified_polygon=simplified_geometry,
-            )
+        # not doing anything to let the migrations pass on new installations
+        pass
 
     def remove_countries(apps, schema_editor):  # noqa
         Country = apps.get_model("countries", "Country")  # noqa
