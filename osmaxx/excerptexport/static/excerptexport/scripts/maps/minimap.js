843c3b859f5a96d0d19f04fbc0252a876bb6abbe
Nicola Jordan <nic@hixi.ch>
2016-05-27 12:53:07 +0200

    move disable zoom to function

--- osmaxx/excerptexport/static/excerptexport/scripts/maps/minimap.js
+++ osmaxx/excerptexport/static/excerptexport/scripts/maps/minimap.js
@@ -4,12 +4,11 @@
     jQuery("div[id^='minimap']").each(function(discarded, elem){
         elem.innerHTML = '';
         var id = elem.id.replace('minimap-', '');
-        var disableZoom = true;
         var excerpt = new ExcerptViewer(
             elem.id,
-            "/api/bounding_geometry_from_excerpt/{ID}/",
-            disableZoom
+            "/api/bounding_geometry_from_excerpt/{ID}/"
         );
+        excerpt.disableZoom();
         excerpt.showExcerptOnMap(id);
     });
 })();
