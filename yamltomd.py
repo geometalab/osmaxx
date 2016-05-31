6151512faa3075b1eaf3c2d65c4cb1d9e240ab62
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-05-31 14:21:23 +0200

    fix indentation, remove trailing whitespace

--- yamltomd.py
+++ yamltomd.py
@@ -34,35 +34,35 @@ with open('templates/attribute_values.md.jinja2') as f:
 
 
 def yaml_to_md(layer_name, layer_definition, out):
-	out.write('## ' + layer_name + '\n\n')
-	
-	attributes = layer_definition['attributes']
-	attributes = OrderedDict(sorted(attributes.items()))
-	header = "|Attributes          |type                |Description                                                           |osm_tags            |"
-	header += "\n| ------------------ | ------------------ | -------------------------------------------------------------------- | ------------------ |"
+    out.write('## ' + layer_name + '\n\n')
 
-	out.write(LAYER_ATTRIBUTES_TEMPLATE.render(attributes=attributes, header=header, dicts_len=len(attributes.keys())))
+    attributes = layer_definition['attributes']
+    attributes = OrderedDict(sorted(attributes.items()))
+    header = "|Attributes          |type                |Description                                                           |osm_tags            |"
+    header += "\n| ------------------ | ------------------ | -------------------------------------------------------------------- | ------------------ |"
 
-	# values of layer attribute "type" (not to be confused with an attribute's type)
+    out.write(LAYER_ATTRIBUTES_TEMPLATE.render(attributes=attributes, header=header, dicts_len=len(attributes.keys())))
 
-	type_values = attributes["type"]['values']
-	type_values = OrderedDict(sorted(type_values.items()))
+    # values of layer attribute "type" (not to be confused with an attribute's type)
 
-	correlated_attributes = set()
-	for definition in type_values.values():
-	    for name, _ in definition.get('correlated_attributes', {}).items():
-	    	correlated_attributes.add(name)
+    type_values = attributes["type"]['values']
+    type_values = OrderedDict(sorted(type_values.items()))
 
-	out.write(ATTRIBUTE_VALUES_TEMPLATE.render(type_values=type_values, correlated_attributes=correlated_attributes))
-	out.write('\n\n')
+    correlated_attributes = set()
+    for definition in type_values.values():
+        for name, _ in definition.get('correlated_attributes', {}).items():
+            correlated_attributes.add(name)
+
+    out.write(ATTRIBUTE_VALUES_TEMPLATE.render(type_values=type_values, correlated_attributes=correlated_attributes))
+    out.write('\n\n')
 
 
 with open("osmaxx_schema.yaml", 'r') as in_file:
     data = yaml.load(in_file)
 layers = data['layers']
 with open('header.md', 'r') as h:
-	header_doc = h.read()
+    header_doc = h.read()
 with open("documentation.md", 'w') as out_file:
-	out_file.write(header_doc)
-	for layer_name in LAYERS_TO_BE_DOCUMENTED:
-	    yaml_to_md(layer_name, layers[layer_name], out=out_file)
+    out_file.write(header_doc)
+    for layer_name in LAYERS_TO_BE_DOCUMENTED:
+        yaml_to_md(layer_name, layers[layer_name], out=out_file)
