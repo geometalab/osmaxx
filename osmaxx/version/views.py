04d86c61369cbb82ebc0fa5ca7dc441ebdf133f6
Nicola Jordan <nic@hixi.ch>
2016-06-01 11:30:34 +0200

    add more info to version view

--- osmaxx/version/views.py
+++ osmaxx/version/views.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from django.conf import settings
 from django.shortcuts import render_to_response
 
@@ -6,5 +8,6 @@ def show_version_number(request):
     from osmaxx import __version__
     return render_to_response(template_name='version/show_version.html', context={
         'version_number': __version__,
+        'actual_version': subprocess.check_output(["git", "describe", "--dirty"]).strip().decode(),
         'DEBUG': settings.DEBUG,
     })
