759349b38defaab592a6ed9b0782b9b277ff52d3
Nicola Jordan <nic@hixi.ch>
2016-06-10 11:58:08 +0200

    separate filter and view creation script order testing

--- tests/conversion/converters/bootstrap_test.py
+++ tests/conversion/converters/bootstrap_test.py
@@ -6,14 +6,31 @@ from tests.conftest import area_polyfile_string
 from osmaxx.conversion.converters.converter_gis.bootstrap import bootstrap
 
 
-def test_scripts_are_executed_in_correct_order(sql_scripts_filter, sql_scripts_create_view):
+def test_filter_scripts_are_executed_in_correct_order(sql_scripts_filter):
     bootstrapper = bootstrap.BootStrapper(area_polyfile_string=area_polyfile_string())
     with mock.patch.object(bootstrapper, '_postgres') as postgres_mock:
         bootstrapper._filter_data()
+
+        base_path_to_bootstrap = os.path.dirname(bootstrap.__file__)
+        expected_script_order = sql_scripts_filter
+        expected_calls = [
+            mock.call(
+                os.path.join(
+                    base_path_to_bootstrap,
+                    relative_script_path
+                )
+            ) for relative_script_path in expected_script_order
+        ]
+        assert expected_calls == postgres_mock.execute_sql_file.mock_calls
+
+
+def test_create_views_scripts_are_executed_in_correct_order(sql_scripts_create_view):
+    bootstrapper = bootstrap.BootStrapper(area_polyfile_string=area_polyfile_string())
+    with mock.patch.object(bootstrapper, '_postgres') as postgres_mock:
         bootstrapper._create_views()
 
         base_path_to_bootstrap = os.path.dirname(bootstrap.__file__)
-        expected_script_order = sql_scripts_filter + sql_scripts_create_view
+        expected_script_order = sql_scripts_create_view
         expected_calls = [
             mock.call(
                 os.path.join(
