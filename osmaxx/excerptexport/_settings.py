17fc489e8dd7aa4b67ba466d99b7ef50a5d4b2cc
Nicola Jordan <nic@hixi.ch>
2016-06-14 14:21:26 +0200

    sanitize settings

--- osmaxx/excerptexport/_settings.py
+++ osmaxx/excerptexport/_settings.py
@@ -4,14 +4,14 @@ from datetime import timedelta
 from django.conf import settings
 
 POLYFILE_LOCATION = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'polyfiles'))
+OLD_RESULT_FILES_REMOVAL_CHECK_INTERVAL_HOURS = timedelta(hours=1)  # default every hour
+PURGE_OLD_RESULT_FILES_AFTER = timedelta(14)  # default to two weeks
 
-OLD_RESULT_FILES_REMOVAL_CHECK_INTERVAL_HOURS = timedelta(
-    hours=settings.OSMAXX.get('OLD_RESULT_FILES_REMOVAL_CHECK_INTERVAL_HOURS', 1)  # default every hour
-)
-PURGE_OLD_RESULT_FILES_AFTER = timedelta(
-    days=settings.OSMAXX.get('OLD_RESULT_FILES_REMOVAL_DAYS', 14)  # default to two weeks
-)
-
+if hasattr(settings, 'OSMAXX'):
+    if hasattr(settings.OSMAXX, 'OLD_RESULT_FILES_REMOVAL_CHECK_INTERVAL_HOURS'):
+        OLD_RESULT_FILES_REMOVAL_CHECK_INTERVAL_HOURS = settings.OSMAXX['OLD_RESULT_FILES_REMOVAL_CHECK_INTERVAL_HOURS']
+    if hasattr(settings.OSMAXX, 'OLD_RESULT_FILES_REMOVAL_DAYS'):
+        PURGE_OLD_RESULT_FILES_AFTER = timedelta(settings.OSMAXX.get('OLD_RESULT_FILES_REMOVAL_DAYS'))
 
 # only needed for testing
 if hasattr(settings, '_OSMAXX_POLYFILE_LOCATION'):
