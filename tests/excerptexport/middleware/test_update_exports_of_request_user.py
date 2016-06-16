c3b899748f2b934c6802a9c416ba5d5e9eaa458a
Nicola Jordan <nic@hixi.ch>
2016-06-16 13:33:22 +0200

    also test other exception case

--- tests/excerptexport/middleware/test_update_exports_of_request_user.py
+++ tests/excerptexport/middleware/test_update_exports_of_request_user.py
@@ -30,3 +30,22 @@ def test_update_exports_of_request_user_sets_export_to_failed_if_not_available_o
             update_exports_of_request_user(http_request_mock)
             export_with_job.refresh_from_db()
             assert export_with_job.status == export_with_job.FAILED
+
+
+def test_update_exports_of_request_user_does_not_change_status_when_exception_occurs(rf, mocker, export_with_job, user):
+    def side_effect_function(*args, **kwargs):
+        raise Exception()
+
+    with mocker.patch.object(ConversionApiClient, 'authorized_get'):
+
+        with mocker.patch('osmaxx.job_progress.middleware.update_export_if_stale', side_effect=side_effect_function):
+            from osmaxx.job_progress.middleware import update_exports_of_request_user
+            assert export_with_job.status == export_with_job.INITIAL
+
+            http_request_mock = rf.get('/')
+            http_request_mock.user = user
+
+            update_exports_of_request_user(http_request_mock)
+            export_with_job.refresh_from_db()
+
+            assert export_with_job.status == export_with_job.INITIAL
