20435bedb1dc873cca7a08886d74862abe537022
Nicola Jordan <nic@hixi.ch>
2016-06-16 13:31:42 +0200

    extract changed export to fixture

--- tests/excerptexport/middleware/test_update_exports_of_request_user.py
+++ tests/excerptexport/middleware/test_update_exports_of_request_user.py
@@ -1,9 +1,17 @@
+import pytest as pytest
 import requests
 
 from osmaxx.api_client import ConversionApiClient
 
 
-def test_update_exports_of_request_user_sets_export_to_failed_if_not_available_on_mediator(rf, mocker, export, user):
+@pytest.fixture
+def export_with_job(export):
+    export.conversion_service_job_id = 10
+    export.save()
+    return export
+
+
+def test_update_exports_of_request_user_sets_export_to_failed_if_not_available_on_mediator(rf, mocker, export_with_job, user):
     def side_effect_function(*args, **kwargs):
         response = mocker.Mock()
         setattr(response, 'status_code', requests.codes['not_found'])
@@ -12,16 +20,13 @@ def test_update_exports_of_request_user_sets_export_to_failed_if_not_available_o
         raise http_error
 
     with mocker.patch.object(ConversionApiClient, 'authorized_get'):
-        export.conversion_service_job_id = 10
-        export.save()
-
         with mocker.patch('osmaxx.job_progress.middleware.update_export_if_stale', side_effect=side_effect_function):
             from osmaxx.job_progress.middleware import update_exports_of_request_user
-            assert export.status == export.INITIAL
+            assert export_with_job.status == export_with_job.INITIAL
 
             http_request_mock = rf.get('/')
             http_request_mock.user = user
 
             update_exports_of_request_user(http_request_mock)
-            export.refresh_from_db()
-            assert export.status == export.FAILED
+            export_with_job.refresh_from_db()
+            assert export_with_job.status == export_with_job.FAILED
