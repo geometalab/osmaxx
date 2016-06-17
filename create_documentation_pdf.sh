433222e7a74c2df165060e11ced0b6bf3ec68ee0
Nicola Jordan <nic@hixi.ch>
2016-06-17 15:40:03 +0200

    small omission, big impact

--- create_documentation_pdf.sh
+++ create_documentation_pdf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -it -v $(pwd):/pandoc geometalab/pandoc bash -c pandoc -t html5 documentation.md -o documentation.html; wkhtmltopdf --encoding 'utf-8' toc documentation.html documentation.pdf
+docker run --rm -it -v $(pwd):/pandoc geometalab/pandoc bash -c "pandoc -t html5 documentation.md -o documentation.html; wkhtmltopdf --encoding 'utf-8' toc documentation.html documentation.pdf"
 rm documentation.html
