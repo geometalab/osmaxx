bc0a5f31f85b179f92961a910aceca2abf888fe9
Nicola Jordan <hixi@users.noreply.github.com>
2016-06-20 09:20:47 +0200

    rename file accordingly to change of input markdown

--- create_documentation_pdf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run --rm -it -v $(pwd):/pandoc geometalab/pandoc bash -c "pandoc -t html5 documentation.md -o documentation.html && wkhtmltopdf --encoding 'utf-8' toc documentation.html documentation.pdf"
-rm documentation.html
