f0e0ca5643b9cefc12eebe86f64b644f31f3484b
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-06-21 09:27:26 +0200

    move create_osmaxx_data_schema_pdf.sh and make it independant of caller's working dir

--- create_osmaxx_data_schema_pdf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run --rm -it -v $(pwd):/pandoc geometalab/pandoc bash -c "pandoc -t html5 osmaxx_data_schema.md -o osmaxx_data_schema.html && wkhtmltopdf --encoding 'utf-8' toc osmaxx_data_schema.html osmaxx_data_schema.pdf"
-rm osmaxx_data_schema.html
