38a7b7de4549e7a2de6ec858dcf04696915ce5b6
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-06-29 13:43:45 +0200

    remove spurious quote in area_polyfile_string fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -299,7 +299,7 @@ def geos_geometry_can_be_created_from_geojson_string():
 
 @pytest.fixture
 def area_polyfile_string():
-    return ''''none
+    return '''none
 polygon-1
     7.495679855346679 43.75782881091782
     7.38581657409668 43.75782881091782
