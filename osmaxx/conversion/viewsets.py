160b893615c486c8e5996588b168e6d3f79b4a86
Nicola Jordan <nic@hixi.ch>
2016-07-01 13:26:19 +0200

    expose format and detail level dependent size estamation on mediator api

--- osmaxx/conversion/viewsets.py
+++ osmaxx/conversion/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework import permissions, viewsets
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
 
 from .models import Job, Parametrization
-from .serializers import JobSerializer, ParametrizationSerializer
+from .serializers import JobSerializer, ParametrizationSerializer, FormatSizeEstimationSerializer
 
 
 class JobViewSet(viewsets.ModelViewSet):
@@ -22,3 +24,22 @@ class ParametrizationViewSet(viewsets.ModelViewSet):
     permission_classes = (
         permissions.IsAuthenticated,
     )
+
+
+class FormatSizeEstimationView(viewsets.ViewSet):
+    """
+    Returns the estimated file size for osm data of the given extent
+    """
+    serializer_class = FormatSizeEstimationSerializer
+
+    def get_success_headers(self, data):
+        try:
+            return {'Location': data[api_settings.URL_FIELD_NAME]}
+        except (TypeError, KeyError):
+            return {}
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, headers=headers)
