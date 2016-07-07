ef316d8c7967c8701fd354fb27a3a2be217182f7
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-07-07 10:04:27 +0200

    refactoring: extract modulus (modulo base) to local variable

--- osmaxx/geodesy/coordinate_reference_system.py
+++ osmaxx/geodesy/coordinate_reference_system.py
@@ -86,6 +86,7 @@ def utm_zones_for_representing(geom):
 
 
 def wrap_longitude_degrees(longitude_degrees):
-    wrapped_longitude_degrees = (longitude_degrees - -180) % 360 + -180
+    modulus = 360
+    wrapped_longitude_degrees = (longitude_degrees - -180) % modulus + -180
     assert -180 <= wrapped_longitude_degrees <= 180
     return wrapped_longitude_degrees
