43c521e2d1b4d62bc00bb80ffb980c56b87db732
Nicola Jordan <nic@hixi.ch>
2016-07-14 10:02:09 +0200

    use consistent casing

--- osmaxx/conversion/converters/converter_gis/bootstrap/sql/functions/0060_cast_to_float_or_null.sql
+++ osmaxx/conversion/converters/converter_gis/bootstrap/sql/functions/0060_cast_to_float_or_null.sql
@@ -1,10 +1,10 @@
 -- try casting to float and return NULL if failed
-create or replace function cast_to_float_null_if_failed(v_input text) returns float as $$
-begin
-    return cast(v_input as float);
-exception
-    when invalid_text_representation then
+CREATE OR REPLACE FUNCTION cast_to_float_null_if_failed(v_input text) RETURNS FLOAT AS $$
+BEGIN
+    RETURN CAST(v_input AS FLOAT);
+EXCEPTION
+    WHEN invalid_text_representation THEN
         RAISE NOTICE 'Invalid float value: "%".  Returning NULL.', v_input;
-        return NULL;
-end;
-$$ language plpgsql immutable;
+        RETURN NULL;
+END;
+$$ LANGUAGE PLPGSQL IMMUTABLE;
