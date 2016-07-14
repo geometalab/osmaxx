63536846caf109337b556d91ff39b31f63c0fb10
Nicola Jordan <nic@hixi.ch>
2016-07-14 09:15:56 +0200

    use email settings prefix from settings, not hardcoded

--- osmaxx/utilities/shortcuts.py
+++ osmaxx/utilities/shortcuts.py
@@ -32,7 +32,7 @@ class Emissary:
         try:
             email_address = self.recipient.email
             mail.send_mail(
-                '[OSMaxx] ' + subject,
+                settings.EMAIL_SUBJECT_PREFIX + subject,
                 mail_body,
                 settings.DEFAULT_FROM_EMAIL,
                 [email_address]
