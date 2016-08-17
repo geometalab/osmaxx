885d7111516f310d36fa69d6a47f5f325f379c54
Nicola Jordan <njordan.hsr@gmail.com>
2016-08-17 09:41:21 +0200

    add a token creation time to profile

--- osmaxx/profile/models.py
+++ osmaxx/profile/models.py
@@ -6,3 +6,4 @@ from django.utils.translation import ugettext_lazy as _
 class Profile(models.Model):
     associated_user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name=_('user'))
     unverified_email = models.EmailField(verbose_name=_('unverified email'), max_length=200, null=True)
+    token_creation_time = models.DateTimeField(verbose_name=_('token timestamp'))
