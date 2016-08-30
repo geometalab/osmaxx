5843ed694970176b0cc673feca1700166c00271c
Nicola Jordan <njordan.hsr@gmail.com>
2016-08-30 17:20:19 +0200

    use the utm-zone-info in javascript

--- osmaxx/excerptexport/static/excerptexport/scripts/utm_zone_filter.js
+++ osmaxx/excerptexport/static/excerptexport/scripts/utm_zone_filter.js
@@ -1,26 +1,46 @@
-window.filterUTMZones = function (leafletGeometry) {
-    var getUTMZones = function (leafletGeometry) {
-        console.log(leafletGeometry.getLayers()[0].feature.geometry);
-        // console.log(leafletGeometry.getLayers()[0].geometry.toGeoJSON());
-        var csrftoken = Cookies.get('csrftoken');
+jQuery(document).ready(function(){
+    var utm_optgroup_html_element = '#id_coordinate_reference_system optgroup[label="UTM zones"]';
+    var utm_zone_optgroup_original = jQuery(utm_optgroup_html_element).clone();
+
+    window.filterUTMZones = function (leafletGeometry) {
+        var utm_zone_optgroup = jQuery(utm_optgroup_html_element);
+
+        if (utm_zone_optgroup !== null) {
+            utm_zone_optgroup.html("");
+            getUTMZones(leafletGeometry).done(function(data){
+                var srids = data["utm_zone_srids"].sort();
+
+                var options_html = '';
+                srids.forEach(function(srid){
+                    var original_text = utm_zone_optgroup_original.find('option[value=' + srid + ']').text();
+                    options_html += _optionHTML(srid, original_text);
+                });
+                utm_zone_optgroup.html(options_html);
+            }).error(function(data, other, other2, other3){
+                console.log(data, other, other2, other3);
+            });
+        }
+
+        function getUTMZones(leafletGeometry) {
+            var geometry = leafletGeometry.toGeoJSON()["geometry"],
+                srid = 4326;
+            var csrftoken = Cookies.get('csrftoken');
             return jQuery.ajax({
-                url: '/api/geodesy/utm-zones/',
+                url: '/api/utm-zone-info/',
                 contentType: "application/json; charset=utf-8",
                 dataType: 'json',
                 type: 'POST',
-                data: JSON.stringify({"geometry": leafletGeometry.getLayers()[0].feature.geometry}),
-                beforeSend: function(xhr, settings) {
+                data: JSON.stringify({"geom": geometry, "srid": srid}),
+                beforeSend: function(xhr) {
                     xhr.setRequestHeader("X-CSRFToken", csrftoken);
                 }
-                // success: function() {
-                //     panel_container.remove();
-                // }
             });
+        }
+
+        function _optionHTML(srid, name) {
+            return '<option value="' + srid + '">' + name + '</option>';
+
+        }
     };
 
-    getUTMZones(leafletGeometry).done(function(data, other, other2, other3){
-        console.log(data, other, other2, other3);
-    }).error(function(data, other, other2, other3){
-            console.log(data, other, other2, other3);
-        });
-};
+});
