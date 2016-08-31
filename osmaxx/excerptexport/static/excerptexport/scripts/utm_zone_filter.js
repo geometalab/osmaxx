f0bfd9e3dc34377ebb2f1d8782163e1c678d667a
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-08-31 10:43:22 +0200

    do not repeat `var` in JS

--- osmaxx/excerptexport/static/excerptexport/scripts/utm_zone_filter.js
+++ osmaxx/excerptexport/static/excerptexport/scripts/utm_zone_filter.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
-    var utm_optgroup_html_element = '#id_coordinate_reference_system optgroup[label="UTM zones"]';
-    var utm_zone_optgroup_original = jQuery(utm_optgroup_html_element).clone();
+    var utm_optgroup_html_element = '#id_coordinate_reference_system optgroup[label="UTM zones"]',
+        utm_zone_optgroup_original = jQuery(utm_optgroup_html_element).clone();
 
     window.filterUTMZones = function (leafletGeometry) {
         var utm_zone_optgroup = jQuery(utm_optgroup_html_element);
@@ -8,9 +8,8 @@ jQuery(document).ready(function(){
         if (utm_zone_optgroup !== null) {
             utm_zone_optgroup.html("");
             getUTMZones(leafletGeometry).done(function(data){
-                var srids = data["utm_zone_srids"].sort();
-
-                var options_html = '';
+                var srids = data["utm_zone_srids"].sort(),
+                    options_html = '';
                 srids.forEach(function(srid){
                     var original_text = utm_zone_optgroup_original.find('option[value=' + srid + ']').text();
                     options_html += _optionHTML(srid, original_text);
@@ -23,8 +22,8 @@ jQuery(document).ready(function(){
 
         function getUTMZones(leafletGeometry) {
             var geometry = leafletGeometry.toGeoJSON()["geometry"],
-                srid = 4326;
-            var csrftoken = Cookies.get('csrftoken');
+                srid = 4326,
+                csrftoken = Cookies.get('csrftoken');
             return jQuery.ajax({
                 url: '/api/utm-zone-info/',
                 contentType: "application/json; charset=utf-8",
