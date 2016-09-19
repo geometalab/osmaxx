86469137dff7ab1a3e1194de0c4de08ea62cba3f
Nicola Jordan <njordan.hsr@gmail.com>
2016-09-19 16:51:51 +0200

    add user monkey-patch to display user group

--- osmaxx/contrib/auth/__init__.py
+++ osmaxx/contrib/auth/__init__.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
+from django.contrib.auth.models import User, Group
+
+
+def is_exclusive_member(self: User):
+    return Group.objects.get(name=settings.OSMAXX['EXCLUSIVE_USER_GROUP']) in self.groups.all()
+User.is_exclusive_member = is_exclusive_member
