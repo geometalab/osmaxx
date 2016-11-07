02c7b8f2b9df3e133274ccc7d369acb8ca609e31
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-11-07 11:53:29 +0100

    remove attribute value encoding parametrization from QGIS project template #708
    as attribute value data is now UTF-8 for all GIS formats exported

--- osmaxx/conversion_api/formats.py
+++ osmaxx/conversion_api/formats.py
@@ -9,7 +9,7 @@ FGDB, SHAPEFILE, GPKG, SPATIALITE, GARMIN = 'fgdb', 'shapefile', 'gpkg', 'spatia
 class OutputFormat:
     def __init__(
             self, *, long_identifier, verbose_name, archive_file_name_identifier, abbreviations, is_white_box,
-            layer_filename_extension=None, attribute_value_encoding='UTF-8'
+            layer_filename_extension=None
     ):
         self._long_identifier = long_identifier
         self._verbose_name = verbose_name
@@ -17,7 +17,6 @@ class OutputFormat:
         self._abbreviations = abbreviations
         self._is_white_box = is_white_box
         self._layer_filename_extension = layer_filename_extension
-        self._attribute_value_encoding = attribute_value_encoding
 
     @property
     def long_identifier(self):
@@ -46,10 +45,6 @@ class OutputFormat:
         file's ``<datasource>`` element referring to data in this format.
         """
         return '/' if self._layer_filename_extension is not None else '|layername='
-
-    @property
-    def attribute_value_encoding(self):
-        return self._attribute_value_encoding
 
     def unique_archive_name(self):
         return "{}_{}.zip".format(uuid.uuid4(), self.archive_file_name_identifier)
