82b142cabdafaff3b036aa45d27f9d33d0b9a6bf
Nicola Jordan <njordan.hsr@gmail.com>
2017-01-11 11:23:12 +0100

    enable dates in detail view of export

--- osmaxx/excerptexport/admin.py
+++ osmaxx/excerptexport/admin.py
@@ -38,3 +38,5 @@ class ExportAdmin(admin.ModelAdmin):
     list_display = ('id', 'file_format', 'conversion_service_job_id', 'status', 'finished_at')
     list_display_links = ('id', )
     list_filter = ['finished_at', 'created_at', 'updated_at']
+    fields = ('file_format', 'conversion_service_job_id', 'status', 'finished_at', 'created_at', 'updated_at')
+    readonly_fields = ('finished_at', 'created_at', 'updated_at')
