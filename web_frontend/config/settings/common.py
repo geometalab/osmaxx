cb6f0b259cb8243bbbe42fa45e4d77b43858a9ff
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2017-01-17 13:12:03 +0100

    use Django 1.10
    and thereby drop compatibility to DRF 3.3

--- web_frontend/config/settings/common.py
+++ web_frontend/config/settings/common.py
@@ -2,10 +2,10 @@
 Django settings for osmaxx project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/topics/settings/
+https://docs.djangoproject.com/en/1.10/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.9/ref/settings/
+https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 # flake8: noqa
 import os
