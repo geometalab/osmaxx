73544a9df88c876059d852827977b204aadd77ad
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2017-01-30 11:26:58 +0100

    Revert "merge loops"
    This reverts commit 384c56238869b681326ed5fd1d02af32820db1ce.

--- osmaxx/conversion/management/commands/result_harvester.py
+++ osmaxx/conversion/management/commands/result_harvester.py
@@ -137,6 +137,9 @@ def remove_if_final(job):
 
 
 def cleanup_old_jobs():
-    for queue_name in [django_rq.get_failed_queue()] + settings.RQ_QUEUE_NAMES:
+    for job in django_rq.get_failed_queue():
+        remove_if_final(job)
+
+    for queue_name in settings.RQ_QUEUE_NAMES:
         for job in django_rq.get_queue(name=queue_name):
             remove_if_final(job)
