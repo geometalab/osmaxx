be7a19c6f5aa69e889029c2df422d3ab03381081
Nicola Jordan <njordan.hsr@gmail.com>
2017-01-30 14:15:30 +0100

    inline function

--- osmaxx/conversion/management/commands/result_harvester.py
+++ osmaxx/conversion/management/commands/result_harvester.py
@@ -131,14 +131,10 @@ def fetch_job(rq_job_id, from_queues):
     return None
 
 
-def remove_if_final(job):
-    if job.status in FINAL_STATUSES:
-        job.delete()
-
-
 def cleanup_old_jobs():
     queues = [django_rq.get_queue(name=queue_name) for queue_name in settings.RQ_QUEUE_NAMES]
     queues.append(django_rq.get_failed_queue())
     for queue in queues:
         for job in queue.get_jobs():
-            remove_if_final(job)
+            if job.status in FINAL_STATUSES:
+                job.delete()
