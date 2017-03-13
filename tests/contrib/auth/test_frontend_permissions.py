5053c04a9f54ddee7c838be04b45a4445edbd01b
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2017-03-13 16:46:30 +0100

    #819 don't consider falsy email address to be confirmed
    as that would be either empty ('') or None, thus not a usable email address

--- tests/contrib/auth/test_frontend_permissions.py
+++ tests/contrib/auth/test_frontend_permissions.py
@@ -1,3 +1,4 @@
+import pytest
 from django.contrib.auth.models import User
 from osmaxx.contrib.auth.frontend_permissions import _user_has_validated_email
 from osmaxx.profile.models import Profile
@@ -18,4 +19,10 @@ def test_new_superuser_has_no_validated_email_address(db):
     an_admin = User.objects.create_superuser('A. D. Min', 'admin@example.com', 'password')
     assert not _user_has_validated_email(an_admin)
 
-# TODO: neither email set -> _user_has_validated_email(...) is false
+
+@pytest.mark.parametrize('user_email', ['', None])
+@pytest.mark.parametrize('profile_email', ['', None])
+def test_user_with_empty_email_addresses(db, user_email, profile_email):
+    a_user = User.objects.create_user('U. Ser', user_email, 'password')
+    Profile.objects.create(associated_user=a_user, unverified_email=profile_email)
+    assert not _user_has_validated_email(a_user)
