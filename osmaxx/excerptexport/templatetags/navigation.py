e03c305cc3915076afc71ed3f2310ee64f6f42d7
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2017-03-16 17:08:59 +0100

    refactoring: change filter argument name and docstring
    and remove reference to original to which this isn't too similar anymore

--- osmaxx/excerptexport/templatetags/navigation.py
+++ osmaxx/excerptexport/templatetags/navigation.py
@@ -1,4 +1,3 @@
-# Adopted from https://djangosnippets.org/snippets/2783/
 from django import template
 from django.conf import settings
 
@@ -6,12 +5,18 @@ register = template.Library()
 
 
 @register.filter
-def siteabsoluteurl(absoluteurl, request):
+def siteabsoluteurl(relative_or_absolute_url, request):
     """
-    receives an argument in the form of an absolute url <absoluteurl>
-    Returns the same url with all the necessary path information to be used offsite
+    Produce URL suitable for offsite use.
+
+    Args:
+        relative_or_absolute_url: A relative, scheme-relative or absolute URL
+        request: A Django request object, from which to take the missing parts
+
+    Returns:
+        An absolute URL, incl. scheme, host, etc.
     """
-    absolute_url = request.build_absolute_uri(absoluteurl)
+    absolute_url = request.build_absolute_uri(relative_or_absolute_url)
     if settings.OSMAXX.get('SECURED_PROXY', False) and not absolute_url.startswith('https'):
         if absolute_url.startswith('http'):
             absolute_url = absolute_url[4:]
