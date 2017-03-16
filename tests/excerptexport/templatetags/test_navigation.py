c93e3cde4188115fcadf3a0e1acaa6ee2d8ec415
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2017-03-16 18:22:36 +0100

    add tests for non-http(s) URLs (1 failing)

--- tests/excerptexport/templatetags/test_navigation.py
+++ tests/excerptexport/templatetags/test_navigation.py
@@ -1,3 +1,4 @@
+import pytest
 from django.test import override_settings
 
 from osmaxx.excerptexport.templatetags.navigation import siteabsoluteurl
@@ -55,6 +56,16 @@ def test_siteabsoluteurl_without_secured_proxy_returns_absolute_https_urls_uncha
 
 @override_settings(
     OSMAXX=dict(
+    )
+)
+def test_siteabsoluteurl_without_secured_proxy_returns_absolute_nonhttp_urls_unchanged(rf):
+    absolute_url = 'ftp://example.com/foo/bar'
+    request = rf.get('/another/path')
+    assert siteabsoluteurl(absolute_url, request) == 'ftp://example.com/foo/bar'
+
+
+@override_settings(
+    OSMAXX=dict(
         SECURED_PROXY=True,
     )
 )
@@ -106,3 +117,15 @@ def test_siteabsoluteurl_when_secured_proxy_in_use_returns_absolute_https_urls_u
     absolute_url = 'https://example.com/foo/bar'
     request = rf.get('/another/path')
     assert siteabsoluteurl(absolute_url, request) == 'https://example.com/foo/bar'
+
+
+@pytest.mark.xfail
+@override_settings(
+    OSMAXX=dict(
+        SECURED_PROXY=True,
+    )
+)
+def test_siteabsoluteurl_when_secured_proxy_in_use_returns_absolute_nonhttp_urls_unchanged(rf):
+    absolute_url = 'ftp://example.com/foo/bar'
+    request = rf.get('/another/path')
+    assert siteabsoluteurl(absolute_url, request) == 'ftp://example.com/foo/bar'
