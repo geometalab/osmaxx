d48497f843572bdd4aad5ffdbb576a77ca4692ed
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2017-03-20 11:40:26 +0100

    refactoring: rename variable for warning mock

--- tests/core/templatetags/test_navigation.py
+++ tests/core/templatetags/test_navigation.py
@@ -12,9 +12,9 @@ def test_siteabsoluteurl_without_secured_proxy_adds_scheme_and_netloc_and_path_p
     relative_url = 'foo/bar'
     request = rf.get('/another/path')
     logger.setLevel(logging.WARNING)
-    w = mocker.patch.object(logger, 'warning')
+    log_warning_mock = mocker.patch.object(logger, 'warning')
     assert siteabsoluteurl(relative_url, request) == 'http://testserver/another/foo/bar'
-    w.assert_not_called()
+    log_warning_mock.assert_not_called()
 
 
 @override_settings(
@@ -25,9 +25,9 @@ def test_siteabsoluteurl_without_secured_proxy_adds_scheme_and_netloc(rf, mocker
     netloc_relative_url = '/foo/bar'
     request = rf.get('/another/path')
     logger.setLevel(logging.WARNING)
-    w = mocker.patch.object(logger, 'warning')
+    log_warning_mock = mocker.patch.object(logger, 'warning')
     assert siteabsoluteurl(netloc_relative_url, request) == 'http://testserver/foo/bar'
-    w.assert_not_called()
+    log_warning_mock.assert_not_called()
 
 
 @override_settings(
@@ -38,9 +38,9 @@ def test_siteabsoluteurl_without_secured_proxy_adds_scheme(rf, mocker):
     scheme_relative_url = '//example.com/foo/bar'
     request = rf.get('/another/path')
     logger.setLevel(logging.WARNING)
-    w = mocker.patch.object(logger, 'warning')
+    log_warning_mock = mocker.patch.object(logger, 'warning')
     assert siteabsoluteurl(scheme_relative_url, request) == 'http://example.com/foo/bar'
-    w.assert_not_called()
+    log_warning_mock.assert_not_called()
 
 
 @override_settings(
@@ -51,9 +51,9 @@ def test_siteabsoluteurl_without_secured_proxy_returns_absolute_http_urls_unchan
     absolute_url = 'http://example.com/foo/bar'
     request = rf.get('/another/path')
     logger.setLevel(logging.WARNING)
-    w = mocker.patch.object(logger, 'warning')
+    log_warning_mock = mocker.patch.object(logger, 'warning')
     assert siteabsoluteurl(absolute_url, request) == 'http://example.com/foo/bar'
-    w.assert_not_called()
+    log_warning_mock.assert_not_called()
 
 
 @override_settings(
@@ -64,9 +64,9 @@ def test_siteabsoluteurl_without_secured_proxy_returns_absolute_https_urls_uncha
     absolute_url = 'https://example.com/foo/bar'
     request = rf.get('/another/path')
     logger.setLevel(logging.WARNING)
-    w = mocker.patch.object(logger, 'warning')
+    log_warning_mock = mocker.patch.object(logger, 'warning')
     assert siteabsoluteurl(absolute_url, request) == 'https://example.com/foo/bar'
-    w.assert_not_called()
+    log_warning_mock.assert_not_called()
 
 
 @override_settings(
@@ -77,9 +77,9 @@ def test_siteabsoluteurl_without_secured_proxy_returns_absolute_nonhttp_urls_unc
     absolute_url = 'ftp://example.com/foo/bar'
     request = rf.get('/another/path')
     logger.setLevel(logging.WARNING)
-    w = mocker.patch.object(logger, 'warning')
+    log_warning_mock = mocker.patch.object(logger, 'warning')
     assert siteabsoluteurl(absolute_url, request) == 'ftp://example.com/foo/bar'
-    w.assert_not_called()
+    log_warning_mock.assert_not_called()
 
 
 @override_settings(
@@ -91,9 +91,9 @@ def test_siteabsoluteurl_when_secured_proxy_in_use_adds_https_and_netloc_and_pat
     relative_url = 'foo/bar'
     request = rf.get('/another/path')
     logger.setLevel(logging.WARNING)
-    w = mocker.patch.object(logger, 'warning')
+    log_warning_mock = mocker.patch.object(logger, 'warning')
     assert siteabsoluteurl(relative_url, request) == 'https://testserver/another/foo/bar'
-    w.assert_not_called()
+    log_warning_mock.assert_not_called()
 
 
 @override_settings(
@@ -105,9 +105,9 @@ def test_siteabsoluteurl_when_secured_proxy_in_use_adds_https_and_netloc(rf, moc
     netloc_relative_url = '/foo/bar'
     request = rf.get('/another/path')
     logger.setLevel(logging.WARNING)
-    w = mocker.patch.object(logger, 'warning')
+    log_warning_mock = mocker.patch.object(logger, 'warning')
     assert siteabsoluteurl(netloc_relative_url, request) == 'https://testserver/foo/bar'
-    w.assert_not_called()
+    log_warning_mock.assert_not_called()
 
 
 @override_settings(
@@ -119,9 +119,9 @@ def test_siteabsoluteurl_when_secured_proxy_in_use_adds_https(rf, mocker):
     scheme_relative_url = '//example.com/foo/bar'
     request = rf.get('/another/path')
     logger.setLevel(logging.WARNING)
-    w = mocker.patch.object(logger, 'warning')
+    log_warning_mock = mocker.patch.object(logger, 'warning')
     assert siteabsoluteurl(scheme_relative_url, request) == 'https://example.com/foo/bar'
-    w.assert_not_called()
+    log_warning_mock.assert_not_called()
 
 
 @override_settings(
@@ -133,9 +133,9 @@ def test_siteabsoluteurl_when_secured_proxy_in_use_returns_absolute_http_urls_co
     absolute_url = 'http://example.com/foo/bar'
     request = rf.get('/another/path')
     logger.setLevel(logging.WARNING)
-    w = mocker.patch.object(logger, 'warning')
+    log_warning_mock = mocker.patch.object(logger, 'warning')
     assert siteabsoluteurl(absolute_url, request) == 'https://example.com/foo/bar'
-    w.assert_not_called()
+    log_warning_mock.assert_not_called()
 
 
 @override_settings(
@@ -147,9 +147,9 @@ def test_siteabsoluteurl_when_secured_proxy_in_use_returns_absolute_https_urls_u
     absolute_url = 'https://example.com/foo/bar'
     request = rf.get('/another/path')
     logger.setLevel(logging.WARNING)
-    w = mocker.patch.object(logger, 'warning')
+    log_warning_mock = mocker.patch.object(logger, 'warning')
     assert siteabsoluteurl(absolute_url, request) == 'https://example.com/foo/bar'
-    w.assert_not_called()
+    log_warning_mock.assert_not_called()
 
 
 @override_settings(
@@ -161,6 +161,8 @@ def test_siteabsoluteurl_when_secured_proxy_in_use_returns_absolute_nonhttp_urls
     absolute_url = 'ftp://example.com/foo/bar'
     request = rf.get('/another/path')
     logger.setLevel(logging.WARNING)
-    w = mocker.patch.object(logger, 'warning')
+    log_warning_mock = mocker.patch.object(logger, 'warning')
     assert siteabsoluteurl(absolute_url, request) == 'ftp://example.com/foo/bar'
-    w.assert_called_with("ftp://example.com/foo/bar has not been converted to HTTPS, because it isn't an HTTP URL.")
+    log_warning_mock.assert_called_with(
+        "ftp://example.com/foo/bar has not been converted to HTTPS, because it isn't an HTTP URL."
+    )
