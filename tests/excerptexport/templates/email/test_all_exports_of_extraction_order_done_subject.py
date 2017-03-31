23771a7597b287b232101aa3fbc65a3ec85b32a0
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2017-03-31 14:19:48 +0200

    correct singular pronoun if one failed export

--- tests/excerptexport/templates/email/test_all_exports_of_extraction_order_done_subject.py
+++ tests/excerptexport/templates/email/test_all_exports_of_extraction_order_done_subject.py
@@ -44,3 +44,48 @@ def test_some_success_some_failed(rf, extraction_order, exports):
         order_id=extraction_order.id,
     )
     assert email_body == expected_body
+
+
+def test_some_success_1_failed(rf, extraction_order, exports):
+    successful_exports = exports[:-1]
+    failed_exports = exports[-1:]
+
+    view_context = dict(
+        extraction_order=extraction_order,
+        successful_exports=successful_exports,
+        failed_exports=failed_exports,
+        request=rf.get('/foo/bar'),
+    )
+    email_body = render_to_string(
+        'excerptexport/email/all_exports_of_extraction_order_done_body.txt',
+        context=view_context,
+    ).strip()
+    expected_body = '\n'.join(
+        [
+            'This is an automated email from testserver',
+            '',
+            'The extraction order #{order_id} "Neverland" has been processed and is available for download:',
+            '- Esri File Geodatabase',
+            '- Esri Shapefile',
+            '- GeoPackage',
+            '- SpatiaLite',
+            '- Garmin navigation &amp; map data',
+            '',
+            'Unfortunately, the following export has failed:',
+            '- OSM Protocolbuffer Binary Format',
+            '',
+            'Please order it anew if you need it. '
+            'If there are repeated failures, '
+            'please report them on https://github.com/geometalab/osmaxx/issues '
+            'unless the problem is already known there.',
+            '',
+            'View the complete order at http://testserver/exports/ (login required)',
+            '',
+            'Thank you for using OSMaxx.',
+            'The team at Geometa Lab HSR',
+            'geometalab@hsr.ch',
+        ]
+    ).format(
+        order_id=extraction_order.id,
+    )
+    assert email_body == expected_body
