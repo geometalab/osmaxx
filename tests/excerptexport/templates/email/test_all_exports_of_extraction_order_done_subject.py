32a47147ff17d7e60980eec00188e9a7645964b3
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2017-03-31 14:38:48 +0200

    #815: add fullstop and blank line
    The bug #815 itself ("processedUnfortunately" glued together) was inadvertently already fixed in cec923b21002e63ccd1ea94e3769fd9a77da2ea7 and/or 74948a84cf2fbaf20d9e2ee47cccbf537bbd01a5

--- tests/excerptexport/templates/email/test_all_exports_of_extraction_order_done_subject.py
+++ tests/excerptexport/templates/email/test_all_exports_of_extraction_order_done_subject.py
@@ -89,3 +89,48 @@ def test_some_success_1_failed(rf, extraction_order, exports):
         order_id=extraction_order.id,
     )
     assert email_body == expected_body
+
+
+def test_no_success_1_failed(rf, extraction_order, exports):
+    successful_exports = tuple()
+    failed_exports = exports
+
+    view_context = dict(
+        extraction_order=extraction_order,
+        successful_exports=successful_exports,
+        failed_exports=failed_exports,
+        request=rf.get('/foo/bar'),
+    )
+    email_body = render_to_string(
+        'excerptexport/email/all_exports_of_extraction_order_done_body.txt',
+        context=view_context,
+    ).strip()
+    expected_body = '\n'.join(
+        [
+            'This is an automated email from testserver',
+            '',
+            'The extraction order #{order_id} "Neverland" has been processed.',
+            '',
+            'Unfortunately, the following exports have failed:',
+            '- Esri File Geodatabase',
+            '- Esri Shapefile',
+            '- GeoPackage',
+            '- SpatiaLite',
+            '- Garmin navigation &amp; map data',
+            '- OSM Protocolbuffer Binary Format',
+            '',
+            'Please order them anew if you need them. '
+            'If there are repeated failures, '
+            'please report them on https://github.com/geometalab/osmaxx/issues '
+            'unless the problem is already known there.',
+            '',
+            'View the complete order at http://testserver/exports/ (login required)',
+            '',
+            'Thank you for using OSMaxx.',
+            'The team at Geometa Lab HSR',
+            'geometalab@hsr.ch',
+        ]
+    ).format(
+        order_id=extraction_order.id,
+    )
+    assert email_body == expected_body
