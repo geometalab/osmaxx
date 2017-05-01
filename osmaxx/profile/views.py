a1b17c1e3c4ad96b80e1045644f67ca676eb6f00
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2017-05-01 16:59:38 +0200

    refactoring: trigger confirmation email via profile

--- osmaxx/profile/views.py
+++ osmaxx/profile/views.py
@@ -7,7 +7,6 @@ from django.shortcuts import redirect
 from django.utils.translation import ugettext as _
 from django.views import generic
 
-from osmaxx.profile.email_confirmation import send_email_confirmation
 from osmaxx.profile.forms import ProfileForm
 from osmaxx.profile.models import Profile
 
@@ -35,7 +34,7 @@ class ProfileView(LoginRequiredMixin, generic.UpdateView):
             )
         if self.is_new_user():
             self._move_email_from_user_to_profile(user, profile)
-            send_email_confirmation(self.request, profile)
+            profile.request_email_address_confirmation(self.request)
         else:
             self._ensure_profile_has_email(profile, user)
         user.refresh_from_db()
@@ -52,7 +51,7 @@ class ProfileView(LoginRequiredMixin, generic.UpdateView):
         if isinstance(response, HttpResponseRedirect):  # successful form validation
             profile = Profile.objects.get(associated_user=self.request.user)
             if not profile.has_validated_email():
-                send_email_confirmation(self.request, profile=profile)
+                profile.request_email_address_confirmation(self.request)
         return response
 
     def _ensure_profile_has_email(self, profile, user):
@@ -101,7 +100,7 @@ class ResendVerificationEmail(LoginRequiredMixin, generic.RedirectView):
 
     def get(self, request, *args, **kwargs):
         profile = Profile.objects.get(associated_user=self.request.user)
-        send_email_confirmation(self.request, profile=profile)
+        profile.request_email_address_confirmation(self.request)
         return super().get(request, *args, **kwargs)
 
 
