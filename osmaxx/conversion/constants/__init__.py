da357fffb6f3c8b259125f1daa9dfd979a360483
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2017-05-08 16:46:36 +0200

    #845 remove docstring for conversion.constants

--- osmaxx/conversion/constants/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Interface between OSMaxx frontend (Django-based web client) and the OSMaxx conversion service (mediator and worker).
-
-This package holds shared code. All OSMaxx components (frontend & conversion service) may depend on it.
-No code in this package may depend on other OSMaxx Django apps, especially not the component-specific ones.
-"""
