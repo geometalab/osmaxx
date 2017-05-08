fdfa8a2f3c3f856566ad9ea1e1de854da9d3e588
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2017-05-08 15:47:37 +0200

    #845 remove conversion_api from INSTALLED_APPS
    as it only contains plain old python code and thus doesn't need to be a Django app

--- osmaxx/conversion_api/__init__.py
+++ osmaxx/conversion_api/__init__.py
@@ -1,9 +1,9 @@
 """
 Interface between OSMaxx frontend (Django-based web client) and the OSMaxx conversion service (mediator and worker).
 
-This Django app holds shared code. All OSMaxx components (frontend & conversion service) may depend on it.
-No code in this app may depend on other OSMaxx Django apps, especially not the component-specific ones.
+This package holds shared code. All OSMaxx components (frontend & conversion service) may depend on it.
+No code in this package may depend on other OSMaxx Django apps, especially not the component-specific ones.
 
 The OSMaxx conversion service implementation (provided by the mediator) is located in Django app `conversion`,
-not here in app `conversion_api`.
+not here in the package `conversion_api`.
 """
